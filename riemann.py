1bf8366d6f5b48e796d0ad5db95a9a09bf32d2f1
Weicheng Pei <weicheng.pei@icloud.com>
2019-01-07 22:52:48 +0800

    Implement Riemann solvers for scalar conservation laws.

---
+++ riemann.py
@@ -0,0 +1,80 @@
+import abc
+
+import numpy as np
+
+import equation
+
+
+class RiemannSolver(abc.ABC):
+
+  def set_initial(self, U_L, U_R):
+    self._U_L = U_L
+    self._U_R = U_R
+
+  @abc.abstractmethod
+  def U(self, x, t):
+    pass
+
+  def F(self, U):
+    return self._equation.F(U)
+
+  def F_on_t_axis(self, U_L, U_R):
+    # F is always unique on t-axis
+    self.set_initial(U_L=U_L, U_R=U_R)
+    U_on_t_axis = self.U(x=0, t=1)
+    return self._equation.F(U_on_t_axis)
+
+
+class LinearAdvection(RiemannSolver):
+
+  def __init__(self, a_const):
+    self._equation = equation.LinearAdvection(a_const)
+
+  def U(self, x, t):
+    U = 0.0
+    if x < t * self._equation.A(U):
+      U = self._U_L
+    else:
+      U = self._U_R
+    return U
+
+
+class InviscidBurgers(RiemannSolver):
+
+  def __init__(self):
+    self._equation = equation.InviscidBurgers()
+
+  def U(self, x, t):
+    if self._U_L < self._U_R:
+      return self._rarefaction(x, t)
+    else:
+      return self._shock(x, t)
+
+  def _rarefaction(self, x, t):
+    U = 0.0
+    if t == 0.0:
+      if x < 0:
+        U = self._U_L
+      else:
+        U = self._U_R
+    else:
+      slope = x / t
+      if  slope < self._U_L:
+        U = self._U_L
+      elif slope > self._U_R:
+        U = self._U_R
+      else:
+        U = slope
+    return U
+
+  def _shock(self, x, t):
+    U = (self._U_L + self._U_R) / 2
+    if x < t * U:
+      U = self._U_L
+    else:
+      U = self._U_R
+    return U
+
+
+if __name__ == '__main__':
+  pass
---
+++ test_riemann.py
@@ -0,0 +1,126 @@
+import unittest
+
+import numpy as np
+from numpy.random import rand
+
+import riemann
+
+
+class TestLinearAdvection(unittest.TestCase):
+
+  def test_U(self):
+    U_L, U_R = rand(), rand()  # for any U_L, U_R
+    solver = riemann.LinearAdvection(a_const=1)
+    solver.set_initial(U_L, U_R)
+    # x / t < a
+    self.assertEqual(solver.U(x=-1, t=1), U_L)
+    self.assertEqual(solver.U(x=0, t=1), U_L)
+    # x / t > a
+    self.assertEqual(solver.U(x=2, t=1), U_R)
+    # on x-axis, where t == 0
+    self.assertIn(solver.U(x=0, t=0), (U_L, U_R))
+    self.assertEqual(solver.U(x=-1, t=0), U_L)
+    self.assertEqual(solver.U(x=+1, t=0), U_R)
+
+  def test_F(self):
+    U_L, U_R = rand(), rand()  # for any U_L, U_R
+    # right running wave
+    solver = riemann.LinearAdvection(a_const=+1)
+    self.assertEqual(solver.F_on_t_axis(U_L, U_R), solver.F(U_L))
+    # left running wave
+    solver = riemann.LinearAdvection(a_const=-1)
+    self.assertEqual(solver.F_on_t_axis(U_L, U_R), solver.F(U_R))
+    # standing wave
+    solver = riemann.LinearAdvection(a_const=0)
+    self.assertEqual(solver.F_on_t_axis(U_L, U_R), solver.F(U_L))
+    self.assertEqual(solver.F_on_t_axis(U_L, U_R), solver.F(U_R))
+
+
+class TestInviscidBurgers(unittest.TestCase):
+
+  def test_right_running_shock(self):
+    U_L, U_R = 2, 0
+    solver = riemann.InviscidBurgers()
+    solver.set_initial(U_L, U_R)
+    self.assertEqual(solver.U(x=-1, t=2), U_L)
+    self.assertEqual(solver.U(x=+1, t=2), U_L)
+    self.assertEqual(solver.U(x=+2, t=1), U_R)
+    # on x-axis, where t == 0
+    self.assertEqual(solver.U(x=-1, t=0), U_L)
+    self.assertEqual(solver.U(x=+1, t=0), U_R)
+    self.assertIn(solver.U(x=0, t=0), (U_L, U_R))
+    # on shock, where x/t == +1
+    self.assertIn(solver.U(x=1, t=1), (U_L, U_R))
+    self.assertIn(solver.U(x=2, t=2), (U_L, U_R))
+    # on t-axis
+    self.assertEqual(solver.F_on_t_axis(U_L, U_R), solver.F(U_L))
+
+  def test_left_running_shock(self):
+    U_L, U_R = 0, -2
+    solver = riemann.InviscidBurgers()
+    solver.set_initial(U_L, U_R)
+    self.assertEqual(solver.U(x=+1, t=2), U_R)
+    self.assertEqual(solver.U(x=-1, t=2), U_R)
+    self.assertEqual(solver.U(x=-2, t=1), U_L)
+    # on x-axis, where t == 0
+    self.assertEqual(solver.U(x=-1, t=0), U_L)
+    self.assertEqual(solver.U(x=+1, t=0), U_R)
+    self.assertIn(solver.U(x=0, t=0), (U_L, U_R))
+    # on shock, where x/t == -1
+    self.assertIn(solver.U(x=-1, t=1), (U_L, U_R))
+    self.assertIn(solver.U(x=-2, t=2), (U_L, U_R))
+    # on t-axis
+    self.assertEqual(solver.F_on_t_axis(U_L, U_R), solver.F(U_R))
+
+  def test_standing_shock(self):
+    U_L, U_R = +1, -1
+    solver = riemann.InviscidBurgers()
+    solver.set_initial(U_L, U_R)
+    self.assertEqual(solver.U(x=-2, t=1), U_L)
+    self.assertEqual(solver.U(x=-1, t=2), U_L)
+    self.assertEqual(solver.U(x=+1, t=2), U_R)
+    self.assertEqual(solver.U(x=+2, t=1), U_R)
+    # on x-axis, where t == 0
+    self.assertEqual(solver.U(x=-1, t=0), U_L)
+    self.assertEqual(solver.U(x=+1, t=0), U_R)
+    self.assertIn(solver.U(x=0, t=0), (U_L, U_R))
+    # on shock, where x == 0
+    self.assertIn(solver.U(x=0, t=1), (U_L, U_R))
+    self.assertIn(solver.U(x=0, t=2), (U_L, U_R))
+    self.assertEqual(solver.F_on_t_axis(U_L, U_R), solver.F(U_L))
+    self.assertEqual(solver.F_on_t_axis(U_L, U_R), solver.F(U_R))
+
+  def test_rarefaction(self):
+    U_L, U_R = -1, +1
+    solver = riemann.InviscidBurgers()
+    solver.set_initial(U_L, U_R)
+    # in left const region, where x/t < -1
+    self.assertEqual(solver.U(x=-2, t=1), U_L)
+    # in right const region, where x/t > +1
+    self.assertEqual(solver.U(x=2, t=1), U_R)
+    # on x-axis, where t == 0
+    self.assertEqual(solver.U(x=-1, t=0), U_L)
+    self.assertEqual(solver.U(x=+1, t=0), U_R)
+    self.assertIn(solver.U(x=0, t=0), (U_L, U_R))
+    # inside rarefaction, where -1 <= x/t= < +1
+    self.assertEqual(solver.U(x=-1, t=1), U_L)
+    self.assertEqual(solver.U(x=+1, t=1), U_R)
+    x, t = rand(), 1
+    self.assertEqual(solver.U(x, t), x/t)
+    self.assertEqual(solver.F_on_t_axis(U_L, U_R), 0)
+
+  def test_smooth_initial_condition(self):
+    U_L = rand()
+    U_R = U_L
+    solver = riemann.InviscidBurgers()
+    self.assertEqual(solver.F_on_t_axis(U_L, U_R), solver.F(U_L))
+    solver.set_initial(U_L, U_R)
+    self.assertEqual(solver.U(x=-1, t=1), U_L)
+    self.assertEqual(solver.U(x=+1, t=1), U_R)
+    # on x-axis, where t == 0
+    self.assertEqual(solver.U(x=-1, t=0), U_L)
+    self.assertEqual(solver.U(x=+1, t=0), U_R)
+    self.assertIn(solver.U(x=0, t=0), (U_L, U_R))
+
+if __name__ == '__main__':
+    unittest.main()
