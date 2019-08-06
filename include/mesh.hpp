fa8d09a35476f0714a68c51b074b3acbcf3eb1e2
Weicheng Pei <weicheng.pei@icloud.com>
2019-08-06 19:36:18 +0800

    Initialize Mesh's header.

---
+++ include/mesh.hpp
@@ -0,0 +1,24 @@
+#ifndef PVC_CFD_MESH_HPP_
+#define PVC_CFD_MESH_HPP_
+namespace pvc {
+namespace cfd {
+
+class Node {
+
+};
+
+class Edge {
+
+};
+
+class Cell {
+
+};
+
+class Mesh {
+
+};
+
+}  // namespace cfd
+}  // namespace pvc
+#endif
