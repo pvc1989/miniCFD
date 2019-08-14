fdee1eb84aac113ef4eaf13c2abf8d82a5519645
buaaymh <yangminghao@buaa.edu.cn>
2019-08-14 15:45:07 +0800

    Implement Point::operator, Vector::Dot and Vector::Cross.

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -53,10 +53,28 @@ class Geometry<Real, kDim>::Point {
     return point;
   }
   Vector operator-(const Point& that) const {
+    auto point = Point(xyz_.begin(), xyz_.end());
+    if (this != &that) {
+      for (auto i = 0; i != kDim; ++i) {
+        point.xyz_[i] -= that.xyz_[i];
+      }
+    }
+    return point;
   }
   Point operator*(const Real& scalar) const {
+    auto point = Point(xyz_.begin(), xyz_.end());
+    for (auto i = 0; i != kDim; ++i) {
+      point.xyz_[i] *= scalar;
+    }
+    return point;
   }
   Point operator/(const Real& scalar) const {
+    assert(scalar != 0);
+    auto point = Point(xyz_.begin(), xyz_.end());
+    for (auto i = 0; i != kDim; ++i) {
+      point.xyz_[i] /= scalar;
+    }
+    return point;
   }
  private:
   std::array<Real, kDim> xyz_;
@@ -70,8 +88,27 @@ class Geometry<Real, kDim>::Vector : public Geometry<Real, kDim>::Point {
   Vector(T&&... t) : Point(std::forward<T...>(t...)) {}
   // Operators:
   Real Dot(const Vector& that) {
+    Real dot = 0.0;
+    for (auto i = 0; i != kDim; ++i) {
+      dot += xyz_[i] * that.xyz_[i];
+    }
+    return dot;
   }
   Vector Cross(const Vector& that) {
+    assert(kDim >= 2);
+    std::array<Real, 3> xyz;
+    if (kDim == 2) {
+      xyz[0] = 0.0;
+      xyz[1] = 0.0;
+      xyz[2] = xyz_[0] * that.xyz_[1] - xyz_[1] * that.xyz_[0];
+    }
+    else if (kDim == 3) {
+      xyz[0] = xyz_[1] * that.xyz_[2] - xyz_[2] * that.xyz_[1];
+      xyz[1] = xyz_[2] * that.xyz_[0] - xyz_[0] * that.xyz_[2];
+      xyz[2] = xyz_[0] * that.xyz_[1] - xyz_[1] * that.xyz_[0];
+    }
+    auto cross = Vector(xyz.begin(), xyz.end());
+    return cross;
   }
 };
 
