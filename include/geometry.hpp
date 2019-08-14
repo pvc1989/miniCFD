8b9835084138baa4e97d9115c0212d6f0b56ffa6
Weicheng Pei <weicheng.pei@icloud.com>
2019-08-14 16:05:21 +0800

    Fix logical errors.

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -45,19 +45,15 @@ class Geometry<Real, kDim>::Point {
   }
   Point operator+(const Point& that) const {
     auto point = Point(xyz_.begin(), xyz_.end());
-    if (this != &that) {
-      for (auto i = 0; i != kDim; ++i) {
-        point.xyz_[i] += that.xyz_[i];
-      }
+    for (auto i = 0; i != kDim; ++i) {
+      point.xyz_[i] += that.xyz_[i];
     }
     return point;
   }
   Vector operator-(const Point& that) const {
     auto point = Point(xyz_.begin(), xyz_.end());
-    if (this != &that) {
-      for (auto i = 0; i != kDim; ++i) {
-        point.xyz_[i] -= that.xyz_[i];
-      }
+    for (auto i = 0; i != kDim; ++i) {
+      point.xyz_[i] -= that.xyz_[i];
     }
     return point;
   }
@@ -76,7 +72,7 @@ class Geometry<Real, kDim>::Point {
     }
     return point;
   }
- private:
+ protected:
   std::array<Real, kDim> xyz_;
 };
 
@@ -90,7 +86,7 @@ class Geometry<Real, kDim>::Vector : public Geometry<Real, kDim>::Point {
   Real Dot(const Vector& that) {
     Real dot = 0.0;
     for (auto i = 0; i != kDim; ++i) {
-      dot += xyz_[i] * that.xyz_[i];
+      dot += this->xyz_[i] * that.xyz_[i];
     }
     return dot;
   }
@@ -100,12 +96,12 @@ class Geometry<Real, kDim>::Vector : public Geometry<Real, kDim>::Point {
     if (kDim == 2) {
       xyz[0] = 0.0;
       xyz[1] = 0.0;
-      xyz[2] = xyz_[0] * that.xyz_[1] - xyz_[1] * that.xyz_[0];
+      xyz[2] = this->xyz_[0] * that.xyz_[1] - this->xyz_[1] * that.xyz_[0];
     }
     else if (kDim == 3) {
-      xyz[0] = xyz_[1] * that.xyz_[2] - xyz_[2] * that.xyz_[1];
-      xyz[1] = xyz_[2] * that.xyz_[0] - xyz_[0] * that.xyz_[2];
-      xyz[2] = xyz_[0] * that.xyz_[1] - xyz_[1] * that.xyz_[0];
+      xyz[0] = this->xyz_[1] * that.xyz_[2] - this->xyz_[2] * that.xyz_[1];
+      xyz[1] = this->xyz_[2] * that.xyz_[0] - this->xyz_[0] * that.xyz_[2];
+      xyz[2] = this->xyz_[0] * that.xyz_[1] - this->xyz_[1] * that.xyz_[0];
     }
     auto cross = Vector(xyz.begin(), xyz.end());
     return cross;
