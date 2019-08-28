a4dc1e0d3b5cf23067482227ba89314940afa6b9
Weicheng Pei <weicheng.pei@icloud.com>
2019-08-28 16:11:14 +0800

    Test `VtkReader` on medium grids.

--- test/reader.cpp
+++ test/reader.cpp
@@ -38,6 +38,22 @@ TEST_F(VtkReaderTest, GetMesh) {
   mesh->ForEachDomain(visitor);
   EXPECT_EQ(area, 2.0);
 }
+TEST_F(VtkReaderTest, MediumMesh) {
+  for (auto suffix : {".vtk", ".vtu"}) {
+    reader.ReadFile(test_data_dir_ + "medium" + suffix);
+    auto mesh = reader.GetMesh();
+    ASSERT_TRUE(mesh);
+    EXPECT_EQ(mesh->CountNodes(), 920);
+    auto n_lines = (918*3 + 400*4 + 12*10) / 2;
+    EXPECT_EQ(mesh->CountBoundaries(), n_lines);
+    EXPECT_EQ(mesh->CountDomains(), 918/* Triangles */ + 400/* Rectangles */);
+    // sum of each face's area
+    double area = 0.0;
+    auto visitor = [&area](const Domain& d) { area += d.Measure(); };
+    mesh->ForEachDomain(visitor);
+    EXPECT_NEAR(area, 8.0, 1e-6);
+  }
+}
 
 }  // namespace mesh
 }  // namespace cfd
