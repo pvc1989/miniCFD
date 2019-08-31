3b0d9db8e789926481d2a740d0ebc90f1eedef6c
buaaymh <yangminghao@buaa.edu.cn>
2019-08-31 16:37:52 +0800

    Implement `vtk::Writer`

--- include/mini/mesh/vtk.hpp
+++ include/mini/mesh/vtk.hpp
@@ -18,6 +18,7 @@
 // Helps:
 #include <vtkCellTypes.h>
 #include <vtkCell.h>
+#include <vtkFloatArray.h>
 #include <vtkTriangle.h>
 #include <vtkQuad.h>
 #include <vtkSmartPointer.h>
@@ -130,43 +131,12 @@ class VtkWriter : public Writer<Mesh> {
  public:
   void SetMesh(Mesh* mesh) override {
     assert(mesh);
+    mesh_ = mesh;
     vtk_data_set = vtkSmartPointer<vtkUnstructuredGrid>::New();
-    //read Points
-    auto vtk_points = vtkSmartPointer<vtkPoints>::New();
-    vtk_points->SetNumberOfPoints(mesh->CountNodes());
-    auto insert_point = [&](Node const& node) {
-      vtk_points->InsertPoint(node.I(), node.X(), node.Y(), 0.0);
-    };
-    mesh->ForEachNode(insert_point);
-    vtk_data_set->SetPoints(vtk_points);
-    //read Cells
-    auto insert_cell = [&](Domain const& domain) {
-      vtkIdList* id_list{nullptr};
-      switch (domain.CountVertices()) {
-      case 3: {
-        auto vtk_cell = vtkSmartPointer<vtkTriangle>::New();
-        id_list = vtk_cell->GetPointIds();
-        id_list->SetId(0, domain.GetNode(0)->I());
-        id_list->SetId(1, domain.GetNode(1)->I());
-        id_list->SetId(2, domain.GetNode(2)->I());
-        vtk_data_set->InsertNextCell(vtk_cell->GetCellType(), id_list);
-        break;
-      }
-      case 4: {
-        auto vtk_cell = vtkSmartPointer<vtkQuad>::New();
-        id_list = vtk_cell->GetPointIds();
-        id_list->SetId(0, domain.GetNode(0)->I());
-        id_list->SetId(1, domain.GetNode(1)->I());
-        id_list->SetId(2, domain.GetNode(2)->I());
-        id_list->SetId(3, domain.GetNode(3)->I());
-        vtk_data_set->InsertNextCell(vtk_cell->GetCellType(), id_list);
-        break;
-      }
-      default:
-        std::cerr << "Unknown cell type! " << std::endl;
-      }
-    };
-    mesh->ForEachDomain(insert_cell);
+    //write Points
+    WritePoints();
+    //write Cells
+    WriteCells();
   }
   bool WriteFile(const std::string& file_name) override {
     if (vtk_data_set == nullptr) return false;
@@ -195,7 +165,83 @@ class VtkWriter : public Writer<Mesh> {
   }
 
  private:
+  Mesh* mesh_;
   vtkSmartPointer<vtkUnstructuredGrid> vtk_data_set;
+  void WritePoints() {
+    auto vectors = vtkSmartPointer<vtkFloatArray>::New();
+    vectors->SetName("Node's Position");
+    vectors->SetNumberOfComponents(3);
+    vectors->SetNumberOfTuples(mesh_->CountNodes());
+    auto values = vtkSmartPointer<vtkFloatArray>::New();
+    values->SetName("Average of XYZ");
+    values->SetNumberOfTuples(mesh_->CountNodes());
+    
+    auto vtk_points = vtkSmartPointer<vtkPoints>::New();
+    vtk_points->SetNumberOfPoints(mesh_->CountNodes());
+    auto insert_point = [&](Node const& node) {
+      vtk_points->InsertPoint(node.I(), node.X(), node.Y(), 0.0);
+      vectors->SetTuple3(node.I(), node.X(), node.Y(), 0.0);
+      values->SetValue(node.I(), (node.X() + node.Y()) / 3);
+    };
+    mesh_->ForEachNode(insert_point);
+    vtk_data_set->SetPoints(vtk_points);
+    auto point_data = vtk_data_set->GetPointData();
+    point_data->SetVectors(vectors);
+    point_data->SetScalars(values);
+  }
+  void WriteCells() {
+    auto vectors = vtkSmartPointer<vtkFloatArray>::New();
+    vectors->SetName("Cell's Position");
+    vectors->SetNumberOfComponents(3);
+    vectors->SetNumberOfTuples(mesh_->CountDomains());
+    auto values = vtkSmartPointer<vtkFloatArray>::New();
+    values->SetName("Cell's X+Y+Z");
+    values->SetNumberOfTuples(mesh_->CountDomains());
+    auto i_cell = 0;
+    auto insert_cell = [&](Domain const& domain) {
+      vtkIdList* id_list{nullptr};
+      switch (domain.CountVertices()) {
+      case 3: {
+        auto vtk_cell = vtkSmartPointer<vtkTriangle>::New();
+        id_list = vtk_cell->GetPointIds();
+        id_list->SetId(0, domain.GetNode(0)->I());
+        id_list->SetId(1, domain.GetNode(1)->I());
+        id_list->SetId(2, domain.GetNode(2)->I());
+        auto x = (domain.GetNode(0)->X() + domain.GetNode(1)->X() +
+                  domain.GetNode(2)->X()) / 3;
+        auto y = (domain.GetNode(0)->Y() + domain.GetNode(1)->Y() +
+                  domain.GetNode(2)->Y()) / 3;
+        vectors->SetTuple3(i_cell, x, y, 0.0);
+        values->SetValue(i_cell, x + y);
+        vtk_data_set->InsertNextCell(vtk_cell->GetCellType(), id_list);
+        break;
+      }
+      case 4: {
+        auto vtk_cell = vtkSmartPointer<vtkQuad>::New();
+        id_list = vtk_cell->GetPointIds();
+        id_list->SetId(0, domain.GetNode(0)->I());
+        id_list->SetId(1, domain.GetNode(1)->I());
+        id_list->SetId(2, domain.GetNode(2)->I());
+        id_list->SetId(3, domain.GetNode(3)->I());
+        auto x = (domain.GetNode(0)->X() + domain.GetNode(1)->X() +
+                  domain.GetNode(2)->X() + domain.GetNode(3)->X()) / 4;
+        auto y = (domain.GetNode(0)->Y() + domain.GetNode(1)->Y() +
+                  domain.GetNode(2)->Y() + domain.GetNode(3)->Y()) / 4;
+        vectors->SetTuple3(i_cell, x, y, 0.0);
+        values->SetValue(i_cell, x + y);
+        vtk_data_set->InsertNextCell(vtk_cell->GetCellType(), id_list);
+        break;
+      }
+      default:
+        std::cerr << "Unknown cell type! " << std::endl;
+      }
+      ++i_cell;
+    };
+    mesh_->ForEachDomain(insert_cell);
+    auto cell_data = vtk_data_set->GetCellData();
+    cell_data->SetVectors(vectors);
+    cell_data->SetScalars(values);
+  }
 };
 
 }  // namespace mesh
