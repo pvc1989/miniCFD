e7f6fd8fdad029da4508b4fa973183632f97931f
Weicheng Pei <weicheng.pei@icloud.com>
2019-09-11 03:09:52 +0800

    Redefine tests for `Burgers`.

--- test/riemann.cpp
+++ test/riemann.cpp
@@ -34,39 +34,48 @@ class BurgersTest : public :: testing::Test {
  protected:
   using Solver = Burgers;
   using State = Solver::State;
-  using Flux = Solver::Flux;
 };
-TEST_F(BurgersTest, TestFlux) {
-  auto solver = Solver(1.0);
-  // right running shock wave
-  State u_l{2.0}, u_r{1.0};
-  EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_l));
-  // left running shock wave
-  u_l = -1.0;
-  u_r = -2.0;
-  EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_r));
-  // standing shock wave
-  u_l = +1.0;
-  u_r = -1.0;
-  EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_l));
-  EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_r));
-  // right running rare wave
-  u_l = 1.0;
-  u_r = 2.0;
-  EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_l));
-  // left running rare wave
-  u_l = -2.0;
-  u_r = -1.0;
-  EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_r));
-  // inside rare wave
-  u_l = -2.0;
-  u_r = +2.0;
-  EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(0));
-  // smooth
-  u_l = +1.54;
-  u_r = +1.54;
-  EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_l));
-  EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_r));
+TEST_F(BurgersTest, TestNonZeroK) {
+  for (auto k : {+2.0, -2.0}) {
+    auto solver = Solver(k);
+    // smooth
+    State u_l{1.54 / k};
+    State u_r{u_l};
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_l));
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_r));
+    u_l = -1.54 / k;
+    u_r = -1.54 / k;
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_l));
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_r));
+    // right running shock
+    u_l = 2.0 / k;
+    u_r = 1.0 / k;
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_l));
+    // left running shock
+    u_l = -1.0 / k;
+    u_r = -2.0 / k;
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_r));
+    // standing shock
+    u_l = +1.0 / k;
+    u_r = -1.0 / k;
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_l));
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_r));
+    // right running expansion
+    u_l = +1.0 / k;
+    u_r = +2.0 / k;
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_l));
+    // left running expansion
+    u_l = -2.0 / k;
+    u_r = -1.0 / k;
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(u_r));
+    // inside expansion
+    u_l = -1.0 / k;
+    u_r = +2.0 / k;
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(0.0));
+    u_l = -2.0 / k;
+    u_r = +1.0 / k;
+    EXPECT_EQ(solver.GetFluxOnTimeAxis(u_l, u_r), solver.GetFlux(0.0));
+  }
 }
 
 class MultiWaveTest : public :: testing::Test {
