22f19f38eccd832f91d5499f4261fef13602bce4
Weicheng Pei <weicheng.pei@icloud.com>
2019-12-10 21:24:24 +0800

    Initialize files for line projection.

---
+++ include/mini/polynomial/legendre.hpp
@@ -0,0 +1,11 @@
+//  Copyright 2019 Weicheng Pei and Minghao Yang
+#ifndef MINI_POLYNOMIAL_LEGENDRE_HPP_
+#define MINI_POLYNOMIAL_LEGENDRE_HPP_
+
+namespace mini {
+namespace polynomial {
+
+}  // namespace polynomial
+}  // namespace mini
+
+#endif  // MINI_POLYNOMIAL_LEGENDRE_HPP_
