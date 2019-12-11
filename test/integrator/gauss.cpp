02e905f857ffe7529630a66c9873dc70bb9573b7
Weicheng Pei <weicheng.pei@icloud.com>
2019-12-11 15:13:51 +0800

    Define a generic test for vector functions.

--- test/integrator/gauss.cpp
+++ test/integrator/gauss.cpp
@@ -1,5 +1,6 @@
 // Copyright 2019 Weicheng Pei and Minghao Yang
 #include "mini/integrator/gauss.hpp"
+#include "mini/algebra/column.hpp"
 
 #include <cmath>
 
@@ -15,6 +16,20 @@ class GaussTest : public ::testing::Test {
     // \int_{-1}^{+1} x^{p} dx
     return p % 2 ? 0.0 : 2.0 / (p + 1);
   }
+  template <int kPoints>
+  void TestVectorFunction() {
+    auto integrand = [&](double x) {
+      auto result = algebra::Column<double, 2 * kPoints>();
+      for (int p = 0; p != 2 * kPoints; ++p) {
+        result[p] = std::pow(x, p);
+      }
+      return result;
+    };
+    auto result = Gauss<kPoints>::Integrate(integrand);
+    for (int p = 0; p != 2 * kPoints; ++p) {
+      EXPECT_DOUBLE_EQ(result[p], MonomialIntegral(p));
+    }
+  }
 };
 TEST_F(GaussTest, OnePoint) {
   constexpr int kDegree = 1;
@@ -22,6 +37,7 @@ TEST_F(GaussTest, OnePoint) {
     auto integrand = [&](double x) { return std::pow(x, p); };
     EXPECT_DOUBLE_EQ(Gauss<kDegree>::Integrate(integrand), MonomialIntegral(p));
   }
+  TestVectorFunction<1>();
 }
 
 }  // namespace integrator
