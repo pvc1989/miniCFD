a70b1b1770ecbdef97eb8e70add04d868e472df4
Weicheng Pei <weicheng.pei@icloud.com>
2019-12-11 18:14:11 +0800

    Define API and tests for `projector::Line`.

--- include/mini/projector/line.hpp
+++ include/mini/projector/line.hpp
@@ -2,11 +2,23 @@
 #ifndef MINI_PROJECTOR_LINE_HPP_
 #define MINI_PROJECTOR_LINE_HPP_
 
+#include "mini/algebra/column.hpp"
 #include "mini/integrator/gauss.hpp"
 #include "mini/polynomial/legendre.hpp"
 
 namespace mini {
 namespace projector {
+
+template <int kDegree>
+class Line {
+ public:
+  Line(Point const& head, Point const& tail) {
+  }
+  using Vector = algebra::Column<double, kDegree+1>;
+  template <class Function>
+  Vector GetCoefficients(Function&& function) const {
+  }
+};
 
 }  // namespace projector
 }  // namespace mini
---
+++ test/projector/line.cpp
@@ -0,0 +1,29 @@
+// Copyright 2019 Weicheng Pei and Minghao Yang
+#include "mini/geometry/dim0.hpp"
+
+#include <cmath>
+
+#include "gtest/gtest.h"
+
+
+namespace mini {
+namespace projector {
+
+class Line1dTest : public ::testing::Test {
+ protected:
+  static constexpr int kDegree = 4;
+  using Point = geometry::Point<double, 1>;
+};
+TEST_F(Line1dTest, SecondOrder) {
+  Point head{-1}, tail{+1};
+  auto line = Line<3>(head, tail);
+  auto function = [](double x){ return 0.5 * (x*x - 3); };
+  auto coefficients = line.GetCoefficients(function);
+  EXPECT_DOUBLE_EQ(coefficients[0], 0);
+  EXPECT_DOUBLE_EQ(coefficients[1], 0);
+  EXPECT_DOUBLE_EQ(coefficients[2], 1);
+  EXPECT_DOUBLE_EQ(coefficients[3], 0);
+}
+
+}  // namespace projector
+}  // namespace mini
