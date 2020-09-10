9112a118b60d4939639b1f575e46b34ba93a08d0
Weicheng Pei <weicheng.pei@icloud.com>
2020-09-11 00:25:34 +0800

    Sketch `cgns::reader`.

--- include/mini/mesh/cgns/reader.hpp
+++ include/mini/mesh/cgns/reader.hpp
@@ -3,9 +3,19 @@
 #ifndef MINI_MESH_CGNS_READER_HPP_
 #define MINI_MESH_CGNS_READER_HPP_
 
+#include <memory>
+#include <string>
+
 namespace mini {
 namespace mesh {
 namespace cgns {
+
+template <class Mesh>
+class Reader {
+ public:
+  bool ReadFromFile(const std::string& file_name);
+  std::unique_ptr<Mesh> GetMesh();
+};
 
 }  // namespace cgns
 }  // namespace mesh
