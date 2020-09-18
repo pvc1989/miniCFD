fb3b4a99f40884bbf3c2fc1a26d8eb46251391d0
Weicheng Pei <weicheng.pei@icloud.com>
2020-09-18 22:09:18 +0800

    Add weights of elements.

--- test/mesh/metis.cpp
+++ test/mesh/metis.cpp
@@ -8,7 +8,7 @@
 #include "metis.h"
 #include "gtest/gtest.h"
 
-#include "mini/data/path.hpp"  // defines TEST_DATA_DIR
+#include "mini/data/path.hpp"  // defines PROJECT_BINARY_DIR
 
 namespace mini {
 namespace mesh {
@@ -16,14 +16,14 @@ namespace metis {
 
 class Partitioner : public ::testing::Test {
  protected:
-  std::string const test_data_dir_{TEST_DATA_DIR};
+  std::string const project_binary_dir_{PROJECT_BINARY_DIR};
 };
 TEST_F(Partitioner, PartMeshDual) {
   /*
     Partition a mesh into k parts based on a partitioning of its dual graph.
    */
   // Build a simple mesh:
-  idx_t n_elems_x{30}, n_elems_y{30};
+  idx_t n_elems_x{100}, n_elems_y{40};
   idx_t n_elems = n_elems_x * n_elems_y;
   idx_t n_nodes_x{n_elems_x + 1}, n_nodes_y{n_elems_y + 1};
   idx_t n_nodes = n_nodes_x * n_nodes_y;
@@ -32,8 +32,8 @@ TEST_F(Partitioner, PartMeshDual) {
   auto curr_node = elem_nodes.begin();
   auto curr_elem = elem_range.begin();
   *curr_elem = 0;
-  for (int i = 0; i != n_elems_x; ++i) {
-    for (int j = 0; j != n_elems_y; ++j) {
+  for (int j = 0; j != n_elems_y; ++j) {
+    for (int i = 0; i != n_elems_x; ++i) {
       auto next = 4 + *curr_elem;
       *++curr_elem = next;
       auto k_node = i + j * n_nodes_x;  // node index starts from 0 in C code
@@ -48,14 +48,20 @@ TEST_F(Partitioner, PartMeshDual) {
   EXPECT_EQ(curr_elem, elem_range.end());
   EXPECT_EQ(curr_node, elem_nodes.end());
   // Partition the mesh:
+  auto elem_weights = std::vector<idx_t>(n_elems, 1);
+  for (int j = 0; j != n_elems_y; ++j) {
+    for (int i = 0; i != n_elems_x/4; ++i) {
+      elem_weights[i + j * n_elems_x] = 4;
+    }
+  }
   auto elem_parts = std::vector<idx_t>(n_elems);
   auto node_parts = std::vector<idx_t>(n_nodes);
-  idx_t n_parts{5}, n_common_nodes{2}, edge_cut{0};
+  idx_t n_parts{8}, n_common_nodes{2}, edge_cut{0};
   // idx_t options[METIS_NOPTIONS];
   // options[METIS_OPTION_NUMBERING] = 0;
   auto result = METIS_PartMeshDual(
     &n_elems, &n_nodes, elem_range.data(), elem_nodes.data(), 
-    NULL/* idx_t t *vwgt */,
+    elem_weights.data(),
     NULL/* idx_t t *vsize */,
     &n_common_nodes, &n_parts,
     NULL/* real t *tpwgts */,
@@ -90,7 +96,7 @@ TEST_F(Partitioner, PartMeshDual) {
   std::cout << std::endl;
    */
   // Write partitioned mesh:
-  auto output = test_data_dir_ + "partitioned_mesh.vtk";
+  auto output = project_binary_dir_ + "/test/mesh/partitioned_mesh.vtk";
   auto* file = fopen(output.c_str(), "w");
   fprintf(file, "# vtk DataFile Version 2.0\n");  // Version and Identifier
   fprintf(file, "An unstructed mesh partitioned by METIS.\n");  // Header
@@ -98,14 +104,14 @@ TEST_F(Partitioner, PartMeshDual) {
   // DataSet Structure
   fprintf(file, "DATASET UNSTRUCTURED_GRID\n");
   fprintf(file, "POINTS %d float\n", n_nodes);
-  for (int i = 0; i != n_nodes_x; ++i) {
-    for (int j = 0; j != n_nodes_y; ++j) {
+  for (int j = 0; j != n_nodes_y; ++j) {
+    for (int i = 0; i != n_nodes_x; ++i) {
       fprintf(file, "%f %f 0.0\n", (float) i, (float) j);
     }
   }
   fprintf(file, "CELLS %d %d\n", n_elems, n_elems * 5);
   curr_node = elem_nodes.begin();
-  for (int i = 0; i != n_elems; ++i) {
+  while (curr_node != elem_nodes.end()) {
     fprintf(file, "4 %d %d %d %d\n",
             curr_node[0], curr_node[1], curr_node[2], curr_node[3]);
     curr_node += 4;
@@ -115,9 +121,20 @@ TEST_F(Partitioner, PartMeshDual) {
     fprintf(file, "9\n");  // VTK_QUAD = 9
   }
   fprintf(file, "CELL_DATA %d\n", n_elems);
-  fprintf(file, "SCALARS PartID float 1\n");
+  fprintf(file, "SCALARS CellPartID float 1\n");
   fprintf(file, "LOOKUP_TABLE elem_parts\n");
   for (auto x : elem_parts) {
+    fprintf(file, "%f\n", (float) x);
+  }
+  fprintf(file, "SCALARS CellWeight float 1\n");
+  fprintf(file, "LOOKUP_TABLE elem_weights\n");
+  for (auto x : elem_weights) {
+    fprintf(file, "%f\n", (float) x);
+  }
+  fprintf(file, "POINT_DATA %d\n", n_nodes);
+  fprintf(file, "SCALARS NodePartID float 1\n");
+  fprintf(file, "LOOKUP_TABLE node_parts\n");
+  for (auto x : node_parts) {
     fprintf(file, "%f\n", (float) x);
   }
   fclose(file);
