f393788871a1bdb1575d6b1ebdaf76bfaeb708bb
buaaymh <yangminghao@buaa.edu.cn>
2020-10-26 21:52:33 +0800

    Implement 'Shuffler' class and 'Shuffle' functions.

--- include/mini/mesh/cgns/shuffler.hpp
+++ include/mini/mesh/cgns/shuffler.hpp
@@ -20,7 +20,7 @@ using CSRM = mini::mesh::cgns::CompressedSparseRowMatrix<idx_t>;
 template <typename T>
 void GetNodePartsByConnectivity(const CSRM& cell_csrm, const std::vector<T>& cell_parts,
                                 T n_parts, int n_nodes, std::vector<T>& node_parts) {
-  node_parts.resize(n_nodes, n_parts);
+  node_parts = std::vector<T>(n_nodes, n_parts);
   auto node_pointer = cell_csrm.index.data();
   int n_cells = cell_csrm.pointer.size() - 1;
   auto curr_range_pointer = cell_csrm.pointer.data();
@@ -36,21 +36,9 @@ void GetNodePartsByConnectivity(const CSRM& cell_csrm, const std::vector<T>& cel
 }
 
 template <typename T>
-void SelectByIndices(const std::vector<T>& global_parts,
-                     std::vector<int>& selected_indices,
-                     std::vector<T>& local_parts) {
-  int n = selected_indices.size();
-  local_parts.reserve(n);
-  for (int i = 0; i < n; ++i) {
-    local_parts[i] = global_parts[selected_indices[i]];
-  }                  
-}
-
-template <typename T>
-void ReorderByParts(const std::vector<T>& parts, std::vector<int>& new_order) {
+void ReorderByParts(const std::vector<T>& parts, int* new_order) {
   std::map<int, std::set<int>> collector;
   int n = parts.size();
-  new_order.reserve(n);
   for (int local_id = 0; local_id < n; ++local_id) {
     int part = parts[local_id];
     if (collector.find(part) != collector.end()) {
@@ -63,39 +51,103 @@ void ReorderByParts(const std::vector<T>& parts, std::vector<int>& new_order) {
   int index = 0;
   for (auto& part : collector) {
     for (auto& local_id : part.second) {
-      new_order[index++] = local_id;
+      *new_order++ = local_id;
     }
   }
 }
-
-template <typename T1, typename T2>
-void ShuffleDataArray(const std::vector<T1>& old_array,
-                      const std::vector<T2>& new_order,
-                      std::vector<T1>& new_array) {
- int n = old_array.size();
- new_array.reserve(n);
- for (int i = 0; i < n; ++i) {
-   new_array[i] = old_array[new_order[i]];
- }
-}
-
 template <typename T>
-void ShuffleConnectivity(const std::vector<T>& old_to_new_for_node,
-                         const std::vector<int>& new_cell_order,
-                         const std::vector<T>& old_connectivity,
-                         std::vector<T>& new_connectivity) {
-  int connectivity_size = old_connectivity.size();
+void ShuffleConnectivity(const std::vector<int>& new_cell_order, int npe,
+                         T* connectivity) {
   int n_cells = new_cell_order.size();
-  new_connectivity.reserve(connectivity_size);
-  int n_nodes_per_cell = connectivity_size / n_cells;
-  auto curr_ptr = new_connectivity.data();
+  int connectivity_size = n_cells * npe;
+  std::vector<T> new_connectivity(connectivity_size);
+  auto new_ptr = new_connectivity.data();
   for (int i = 0; i < n_cells; ++i) {
-    int range_min = n_nodes_per_cell * new_cell_order[i];
-    for (int j = range_min; j < range_min + n_nodes_per_cell; ++j) {
-      *curr_ptr++ = old_to_new_for_node[old_connectivity[j]];
+    int range_min = npe * new_cell_order[i];
+    auto old_ptr = connectivity + range_min;
+    for (int j = range_min; j < range_min + npe; ++j) {
+      *new_ptr++ = *old_ptr++;
     }
-  }                        
+  }
+  auto old_ptr = connectivity;
+  for (int i = 0; i < connectivity_size; ++i) {
+    *old_ptr++ = new_connectivity[i];
+  }
 }
+template <typename T>
+void ShuffleDataArray(const std::vector<int>& new_cell_order, T* cell_data) {
+  int n = new_cell_order.size();
+  std::vector<T> new_cell_data(n);
+  for (int i = 0; i < n; ++i) {
+    new_cell_data[i] = *(cell_data + new_cell_order[i]);
+  }
+  auto cell_data_ptr = cell_data;
+  for (int i = 0; i < n; ++i) {
+    *cell_data_ptr++ = new_cell_data[i];
+  }  
+}
+
+template <typename T, class Real>
+class Shuffler {
+ public:
+  using MeshType = mini::mesh::cgns::Tree<Real>;
+  using CSRM = mini::mesh::cgns::CompressedSparseRowMatrix<T>;
+  using ConvertMapType = mini::mesh::cgns::ConvertMap;
+  using SectionType = mini::mesh::cgns::Section<Real>;
+  using SolutionType = mini::mesh::cgns::Solution<Real>;
+  using FieldType = mini::mesh::cgns::Field;
+  Shuffler() = default;
+  void SetNumParts(int n_parts) {
+    n_parts_ = n_parts;
+  }
+  void SetCellParts(std::vector<T>* cell_parts) {
+    cell_parts_ = cell_parts;
+  }
+  void SetMetisMesh(CSRM* cell_csrm) {
+    cell_csrm_ = cell_csrm;
+  }
+  void SetConvertMap(ConvertMapType* convert_map) {
+    convert_map_ = convert_map;
+  }
+  void ShuffleMesh(std::unique_ptr<MeshType>& mesh) {
+    auto& cgns_to_metis_for_cells = convert_map_->cgns_to_metis_for_cells;
+    auto& base = mesh->GetBase(1); int n_zones = base.CountZones();
+    for (int zone_id = 1; zone_id <= n_zones; ++zone_id) {
+      auto& zone = base.GetZone(zone_id);
+      int n_sections = zone.CountSections();
+      for (int section_id = 1; section_id <= n_sections; ++section_id) {
+        auto& section = zone.GetSection(section_id);
+        int n_cells = section.CountCells();
+        std::vector<int> parts(n_cells);
+        for (int local_id = 0; local_id < n_cells; ++local_id) {
+          int global_id = cgns_to_metis_for_cells[zone_id][section_id][local_id];
+          parts[local_id] = (*cell_parts_)[global_id];
+        }
+        int range_min{section.GetOneBasedCellIdMin()-1};
+            /* Shuffle Connectivity */
+        std::vector<int> new_order(n_cells);
+        ReorderByParts<int>(parts, new_order.data());
+        int npe = mini::mesh::cgns::CountNodesByType(section.GetType());
+        cgsize_t* connectivity = section.GetConnectivity();
+        ShuffleConnectivity<cgsize_t>(new_order, npe, connectivity);
+            /* Shuffle CellData */
+        int n_solutions = zone.CountSolutions();
+        for (int solution_id = 1; solution_id <= n_solutions; solution_id++) {
+          auto& solution = zone.GetSolution(solution_id);
+          for (auto& [name, field] : solution.fields) {
+            auto field_ptr = field.data() + range_min;
+            ShuffleDataArray<double>(new_order, field_ptr);
+          }
+        }
+      }
+    }
+  }
+ private:
+  int n_parts_;
+  std::vector<T>* cell_parts_;
+  CSRM* cell_csrm_;
+  ConvertMapType* convert_map_;
+};
 
 }  // namespace metis
 }  // namespace mesh
