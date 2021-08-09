bcf99674a3eb6d3ce91de60a376dfff55cb59ed7
PEI WeiCheng <weicheng.pei@icloud.com>
2021-08-09 20:32:04 +0800

    Rename `node` to `vertex` for graphs.

--- include/mini/mesh/metis/partitioner.hpp
+++ include/mini/mesh/metis/partitioner.hpp
@@ -21,50 +21,50 @@ namespace metis {
  * @tparam Graph sparse graph type
  * @tparam Real real number type
  * 
- * @param[in] n_nodes the number of nodes in the graph
+ * @param[in] n_vertices the number of vertices in the graph
  * @param[in] n_constraints the number of balancing constraints (>= 1)
  * @param[in] graph the graph to be partitioned
- * @param[in] cost_of_each_node the computational cost of each node
- * @param[in] size_of_each_node the communication size of each node
+ * @param[in] cost_of_each_vertex the computational cost of each vertex
+ * @param[in] size_of_each_vertex the communication size of each vertex
  * @param[in] cost_of_each_edge the weight of each edge
  * @param[in] n_parts the number of parts to be partitioned
  * @param[in] weight_of_each_part the weight of each part (sum must be 1.0)
  * @param[in] unbalances the unbalance tolerance for each constraint
  * @param[in] options the array of METIS options
  * @param[out] objective_value the edge cut or the communication volume of the partitioning
- * @param[out] node_parts the part id of each node
+ * @param[out] vertex_parts the part id of each vertex
  */
 template <typename Int, typename Graph, typename Real>
 void PartGraphKway(
-    const Int &n_nodes, const Int &n_constraints,
+    const Int &n_vertices, const Int &n_constraints,
     const Graph &graph,
-    const std::vector<Int> &cost_of_each_node,
-    const std::vector<Int> &size_of_each_node,
+    const std::vector<Int> &cost_of_each_vertex,
+    const std::vector<Int> &size_of_each_vertex,
     const std::vector<Int> &cost_of_each_edge,
     const Int &n_parts,
     const std::vector<Real> &weight_of_each_part,
     const std::vector<Real> &unbalances,
     const std::vector<Int> &options,
     /* input ↑, output ↓ */
-    Int *objective_value, std::vector<Int> *node_parts) {
+    Int *objective_value, std::vector<Int> *vertex_parts) {
   static_assert(std::is_integral_v<Int>, "`Int` must be an integral type.");
   static_assert(std::is_same_v<Int, idx_t>, "`Int` must be `idx_t`.");
   static_assert(std::is_floating_point_v<Real>,
       "`Real` must be a floating-point type.");
   static_assert(std::is_same_v<Real, real_t>, "`Real` must be `real_t`.");
-  assert(node_parts->size() == n_nodes);
+  assert(vertex_parts->size() == n_vertices);
   auto error_code = METIS_PartGraphKway(
-      const_cast<Int*>(&n_nodes), const_cast<Int*>(&n_constraints),
+      const_cast<Int*>(&n_vertices), const_cast<Int*>(&n_constraints),
       const_cast<Int*>(&(graph.range()[0])),
       const_cast<Int*>(&(graph.index()[0])),
-      const_cast<Int*>(cost_of_each_node.data()),
-      const_cast<Int*>(size_of_each_node.data()),
+      const_cast<Int*>(cost_of_each_vertex.data()),
+      const_cast<Int*>(size_of_each_vertex.data()),
       const_cast<Int*>(cost_of_each_edge.data()),
       const_cast<Int*>(&n_parts),
       const_cast<Real*>(weight_of_each_part.data()),
       const_cast<Real*>(unbalances.data()),
       const_cast<Int*>(options.data()),
-      objective_value, node_parts->data());
+      objective_value, vertex_parts->data());
   assert(error_code == METIS_OK);
 }
 /**
