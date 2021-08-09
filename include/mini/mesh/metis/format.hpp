f116f041235b1e6e208706a8d0ad64b0fe819f17
PEI WeiCheng <weicheng.pei@icloud.com>
2021-08-09 20:27:42 +0800

    Let the mesh object provide its size info.

--- include/mini/mesh/metis/format.hpp
+++ include/mini/mesh/metis/format.hpp
@@ -3,7 +3,6 @@
 #ifndef MINI_MESH_METIS_FORMAT_HPP_
 #define MINI_MESH_METIS_FORMAT_HPP_
 
-#include <memory>
 #include <type_traits>
 #include <vector>
 
@@ -14,15 +13,15 @@ namespace mesh {
 namespace metis {
 
 /**
- * @brief The data structure for storing sparse graphs or meshes.
+ * @brief The data structure for representing sparse graphs.
  * 
  * In a real CSR (Compressed Sparse Row) matrix, the column indices for row `i` are stored in `index_[ range_[i], range_[i+1] )` and their corresponding values are stored in `value_[ range_[i], range_[i+1] )`.
- * To represent a graph of mesh, We only care about the positions of non-zero values, so we have ignored the `value_` array.
+ * To represent an unweighted graph, we only care about the positions of non-zero values, so we have ignored the `value_` array.
  * 
  * @tparam Int index type
  */
 template <typename Int>
-struct SparseMatrix {
+class SparseMatrix {
  private:
   static_assert(std::is_integral_v<Int>, "Integral required.");
   std::vector<Int> range_, index_/* , value_ */;
@@ -34,28 +33,68 @@ struct SparseMatrix {
   std::vector<Int> const& index() const {
     return index_;
   }
+  Int CountVertices() const {
+    return range_.size() - 1;
+  }
+  Int CountEdges() const {
+    return index_.size();
+  }
   std::vector<Int>& range() {
     return range_;
   }
   std::vector<Int>& index() {
     return index_;
   }
-  void resize(int n_rows, int n_nonzeros) {
-    range_.resize(n_rows + 1);
-    index_.resize(n_nonzeros);
+  /**
+   * @brief Resize the underling containers.
+   * 
+   * @param n_vertices the number of vertices in this graph
+   * @param n_edges the number of edges in this graph
+   */
+  void resize(Int n_vertices, Int n_edges) {
+    range_.resize(n_vertices + 1);
+    index_.resize(n_edges);
   }
 };
 
 template <typename Int>
-struct Mesh : public SparseMatrix<Int> {
+class Mesh : private SparseMatrix<Int> {
   static_assert(std::is_integral_v<Int>, "Integral required.");
+  using Base = SparseMatrix<Int>;
+  Int n_nodes_;
 
  public:
+  std::vector<Int> const& range() const {
+    return this->Base::range();
+  }
   std::vector<Int> const& nodes() const {
     return this->index();
   }
+  Int CountCells() const {
+    return this->CountVertices();
+  }
+  Int CountNodes() const {
+    return n_nodes_;
+  }
+  std::vector<Int>& range() {
+    return this->Base::range();
+  }
   std::vector<Int>& nodes() {
     return this->index();
+  }
+  /**
+   * @brief Resize the underling containers.
+   * 
+   * @param n_cells the number of cells in this mesh
+   * @param n_nodes_global the number of nodes counted globally (without duplication)
+   * @param n_nodes_local the number of nodes counted locally (with duplication)
+   */
+  void resize(Int n_cells, Int n_nodes_global, Int n_nodes_local) {
+    assert(0 < n_cells);
+    assert(0 < n_nodes_global);
+    assert(0 < n_nodes_local);
+    n_nodes_ = n_nodes_global;
+    Base::resize(n_cells, n_nodes_local);
   }
 };
 
