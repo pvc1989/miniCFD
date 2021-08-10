1ad38d9abdb54988c4bcde377a661bc97bdca216
jyy <jiang.yu.yan@icloud.com>
2021-08-10 21:27:57 +0800

    Simplify data structures.

--- include/mini/mesh/filter/cgns_to_metis.hpp
+++ include/mini/mesh/filter/cgns_to_metis.hpp
@@ -24,16 +24,15 @@ namespace filter {
 static_assert(std::is_same_v<idx_t, cgsize_t>,
     "METIS's `idx_t` is different from CGNS's `cgsize_t`.");
 
+template <class Int = int>
 struct NodeInfo {
-  NodeInfo(int zi, int ni) : zone_id(zi), node_id(ni) {}
-  int zone_id{0};
-  int node_id{0};
+  NodeInfo(Int zi, Int ni) : zone_id(zi), node_id(ni) {}
+  Int zone_id{0}, node_id{0};
 };
+template <class Int = int>
 struct CellInfo {
-  CellInfo(int zi, int si, int ci) : zone_id(zi), section_id(si), cell_id(ci) {}
-  int zone_id{0};
-  int section_id{0};
-  int cell_id{0};
+  CellInfo(Int zi, Int si, Int ci) : zone_id(zi), section_id(si), cell_id(ci) {}
+  Int zone_id{0}, section_id{0}, cell_id{0};
 };
 
 template <class Real = double, class Int = int>
@@ -43,10 +42,14 @@ struct CgnsToMetis {
 
   MetisMesh Filter(const CgnsMesh& mesh);
 
-  std::vector<NodeInfo> metis_to_cgns_for_nodes;
-  std::map<int, std::vector<int>> cgns_to_metis_for_nodes;
-  std::vector<CellInfo> metis_to_cgns_for_cells;
-  std::map<int, std::map<int, std::vector<int>>> cgns_to_metis_for_cells;
+  std::vector<NodeInfo<Int>> metis_to_cgns_for_nodes;
+  std::vector<CellInfo<Int>> metis_to_cgns_for_cells;
+  // metis_node_id =
+  //     cgns_to_metis_for_nodes[zone_id][node_id];
+  std::vector<std::vector<Int>>              cgns_to_metis_for_nodes;
+  // metis_cell_id =
+  //     cgns_to_metis_for_cells[zone_id][sect_id][cell_id - cell_id_min];
+  std::vector<std::vector<std::vector<Int>>> cgns_to_metis_for_cells;
 };
 template <class Real, class Int>
 typename CgnsToMetis<Real, Int>::MetisMesh
@@ -55,59 +58,58 @@ CgnsToMetis<Real, Int>::Filter(const CgnsMesh& cgns_mesh) {
   auto& base = cgns_mesh.GetBase(1);
   auto cell_dim = base.GetCellDim();
   auto n_zones = base.CountZones();
-  int n_nodes_of_curr_base{0};
-  auto cell_ptr = std::vector<int>();
-  auto cell_idx = std::vector<int>();
-  int pointer_value{0};
+  Int n_nodes_in_curr_base{0};
+  auto cell_ptr = std::vector<Int>();  // METIS node range
+  auto cell_idx = std::vector<Int>();  // METIS node index
+  Int pointer_value{0};
   cell_ptr.emplace_back(pointer_value);
-  auto n_nodes_in_prev_zones{0};
-  for (int zone_id = 1; zone_id <= n_zones; ++zone_id) {
+  Int n_nodes_in_prev_zones{0};
+  cgns_to_metis_for_nodes.resize(n_zones+1);
+  cgns_to_metis_for_cells.resize(n_zones+1);
+  for (Int zone_id = 1; zone_id <= n_zones; ++zone_id) {
     auto& zone = base.GetZone(zone_id);
     // read nodes in current zone
-    auto n_nodes_of_curr_zone = zone.CountNodes();
-    cgns_to_metis_for_nodes.emplace(zone_id, std::vector<int>());
+    auto n_nodes_in_curr_zone = zone.CountNodes();
     auto& nodes = cgns_to_metis_for_nodes.at(zone_id);
-    nodes.reserve(n_nodes_of_curr_zone+1);
-    nodes.emplace_back(-1);
+    nodes.reserve(n_nodes_in_curr_zone+1);
+    nodes.emplace_back(-1);  // nodes[0] is invalid
     metis_to_cgns_for_nodes.reserve(metis_to_cgns_for_nodes.size() +
-                                      n_nodes_of_curr_zone);
-    for (int node_id = 1; node_id <= n_nodes_of_curr_zone; ++node_id) {
+                                    n_nodes_in_curr_zone);
+    for (Int node_id = 1; node_id <= n_nodes_in_curr_zone; ++node_id) {
       metis_to_cgns_for_nodes.emplace_back(zone_id, node_id);
-      nodes.emplace_back(n_nodes_of_curr_base++);
+      nodes.emplace_back(n_nodes_in_curr_base++);
     }
     // read cells in current zone
-    cgns_to_metis_for_cells.emplace(zone_id, std::map<int, std::vector<int>>());
     auto n_sections = zone.CountSections();
-    for (int section_id = 1; section_id <= n_sections; ++section_id) {
-      auto [iter, succ] = cgns_to_metis_for_cells[zone_id].emplace(
-          section_id, std::vector<int>());
-      auto& metis_ids_in_section = iter->second;
+    cgns_to_metis_for_cells[zone_id].resize(n_sections + 1);
+    for (Int section_id = 1; section_id <= n_sections; ++section_id) {
       auto& section = zone.GetSection(section_id);
       if (!zone.CheckTypeDim(section.type(), cell_dim))
         continue;
-      auto n_cells_of_curr_sect = section.CountCells();
-
+      auto& metis_ids_in_section = cgns_to_metis_for_cells[zone_id][section_id];
+      auto n_cells_in_curr_sect = section.CountCells();
       auto n_nodes_per_cell = section.CountNodesByType(section.type());
       metis_to_cgns_for_cells.reserve(metis_to_cgns_for_cells.size() +
-                                      n_cells_of_curr_sect);
-      cell_ptr.reserve(cell_ptr.size() + n_cells_of_curr_sect);
-      for (int cell_id = section.CellIdMin();
+                                      n_cells_in_curr_sect);
+      cell_ptr.reserve(cell_ptr.size() + n_cells_in_curr_sect);
+      for (Int cell_id = section.CellIdMin();
            cell_id <= section.CellIdMax(); ++cell_id) {
         metis_ids_in_section.emplace_back(metis_to_cgns_for_cells.size());
         metis_to_cgns_for_cells.emplace_back(zone_id, section_id, cell_id);
-        cell_ptr.emplace_back(pointer_value+=n_nodes_per_cell);
+        cell_ptr.emplace_back(pointer_value += n_nodes_per_cell);
       }
-      auto node_id_list_size = n_nodes_per_cell * n_cells_of_curr_sect;
+      auto node_id_list_size = n_nodes_per_cell * n_cells_in_curr_sect;
       cell_idx.reserve(cell_idx.size() + node_id_list_size);
       auto node_id_list = section.GetNodeIdList();
-      for (int node_id = 0; node_id < node_id_list_size; ++node_id) {
-        auto node_id_global = n_nodes_in_prev_zones + node_id_list[node_id] - 1;
-        cell_idx.emplace_back(node_id_global);
+      for (Int node_id = 0; node_id < node_id_list_size; ++node_id) {
+        auto metis_node_id = n_nodes_in_prev_zones + node_id_list[node_id] - 1;
+        cell_idx.emplace_back(metis_node_id);
       }
     }
     n_nodes_in_prev_zones += zone.CountNodes();
   }
-  return MetisMesh(cell_ptr, cell_idx, metis_to_cgns_for_nodes.size());
+  assert(metis_to_cgns_for_nodes.size() == n_nodes_in_curr_base);
+  return MetisMesh(cell_ptr, cell_idx, n_nodes_in_curr_base);
 }
 
 }  // namespace filter
