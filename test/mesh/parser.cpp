34f7695516a8156160d5d213082cdf9430df132e
PEI WeiCheng <weicheng.pei@icloud.com>
2021-08-27 14:29:47 +0800

    Print time stamp on each process.

--- test/mesh/parser.cpp
+++ test/mesh/parser.cpp
@@ -35,17 +35,24 @@ int main(int argc, char* argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
   cgp_mpi_comm(MPI_COMM_WORLD);
 
-  if (comm_rank == 0)
+  auto time_begin = MPI_Wtime();
+  if (comm_rank == 0) {
+    std::printf("Run ./shuffler on proc[%d/%d] at %f sec\n",
+        comm_rank, comm_size, MPI_Wtime() - time_begin);
     std::system("./shuffler");
+  }
   MPI_Barrier(MPI_COMM_WORLD);
 
-  std::printf("TestProjection on %d/%d\n", comm_rank, comm_size);
+  std::printf("Run Parser() on proc[%d/%d] at %f sec\n",
+      comm_rank, comm_size, MPI_Wtime() - time_begin);
   auto current_binary_dir =
       std::string(PROJECT_BINARY_DIR) + std::string("/test/mesh");
   auto cgns_file = current_binary_dir + "/hexa_new.cgns";
   auto prefix = current_binary_dir + "/hexa_part_";
   auto parser = mini::mesh::cgns::Parser<cgsize_t, double>(
       cgns_file, prefix, comm_rank);
+  std::printf("Run Project() on proc[%d/%d] at %f sec\n",
+      comm_rank, comm_size, MPI_Wtime() - time_begin);
   parser.Project([](auto const& xyz){
     auto r = std::hypot(xyz[0] - 2, xyz[1] - 0.5);
     mini::algebra::Matrix<double, 2, 1> col;
@@ -53,7 +60,10 @@ int main(int argc, char* argv[]) {
     col[1] = 1 - r + (r >= 1);
     return col;
   });
+  std::printf("Run Write() on proc[%d/%d] at %f sec\n",
+      comm_rank, comm_size, MPI_Wtime() - time_begin);
   parser.WriteSolutions();
-
+  std::printf("Run MPI_Finalize() on proc[%d/%d] at %f sec\n",
+      comm_rank, comm_size, MPI_Wtime() - time_begin);
   MPI_Finalize();
 }
