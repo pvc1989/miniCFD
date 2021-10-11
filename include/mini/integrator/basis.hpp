f81045f1e2f923d9a7b2b56978dea05d5d027574
jyy <jiang.yu.yan@icloud.com>
2021-10-11 22:06:17 +0800

    Implement `ProjFunc::GetSmoothness`.

--- include/mini/integrator/basis.hpp
+++ include/mini/integrator/basis.hpp
@@ -200,6 +200,59 @@ class ProjFunc<Scalar, 3, 2, kFunc> {
     return coef_;
   }
 
+  MatKxN GetMpdv(CoordType const& xyz) const {
+    auto x = xyz[0] - center_[0], y = xyz[1] - center_[1],
+         z = xyz[2] - center_[2];
+    MatKxN res;
+    // pdv_x
+    res.col(1) += coef_.col(1);
+    res.col(1) += coef_.col(4) * (2 * x);
+    res.col(1) += coef_.col(5) * y;
+    res.col(1) += coef_.col(6) * z;
+    // pdv_y
+    res.col(2) += coef_.col(2);
+    res.col(2) += coef_.col(5) * x;
+    res.col(2) += coef_.col(7) * (2 * y);
+    res.col(2) += coef_.col(8) * z;
+    // pdv_z
+    res.col(3) += coef_.col(3);
+    res.col(3) += coef_.col(6) * x;
+    res.col(3) += coef_.col(8) * y;
+    res.col(3) += coef_.col(9) * (2 * z);
+    // pdv_xx
+    res.col(4) += coef_.col(4);
+    // pdv_xy
+    res.col(5) += coef_.col(5);
+    // pdv_xz
+    res.col(6) += coef_.col(6);
+    // pdv_yy
+    res.col(7) += coef_.col(7);
+    // pdv_yz
+    res.col(8) += coef_.col(8);
+    // pdv_zz
+    res.col(9) += coef_.col(9);
+    return res;
+  }
+
+  template <typename Element>
+  auto GetSmoothness(Element const& elem) {
+    auto mat_pdv_prod = [&](CoordType const& xyz) {
+      auto mat_pdv = GetMpdv(xyz);
+      mat_pdv = mat_pdv.cwiseProduct(mat_pdv);
+      return mat_pdv;
+    };
+    auto integral = Integrate(mat_pdv_prod, elem);
+    MatKx1 smoothness = integral.col(1);
+    smoothness += integral.col(2);
+    smoothness += integral.col(3);
+    auto volume = Integrate([](CoordType const& xyz){ return 1.0; }, elem);
+    for (int i = 4; i < N; ++i) {
+      smoothness += integral.col(i) * (volume / 4);
+    }
+    return smoothness;
+  }
+
+
   template <typename Callable, typename Element>
   void Reset(Callable&& func, BasisType const& basis, Element const& elem) {
     center_ = basis.GetCenter();
