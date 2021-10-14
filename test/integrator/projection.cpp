820efcdd4aa9ef90e241d47ee80e868789d6873f
PEI WeiCheng <weicheng.pei@icloud.com>
2021-10-14 12:05:10 +0800

    Redefine methods.

--- test/integrator/projection.cpp
+++ test/integrator/projection.cpp
@@ -76,6 +76,36 @@ TEST_F(TestProjection, VectorFunction) {
   EXPECT_NEAR(v_actual[8], v_expect[8], 1e-16);
   EXPECT_NEAR(v_actual[9], v_expect[9], 1e-15);
 }
+TEST_F(TestProjection, PartialDerivatives) {
+  using ProjFunc = mini::integrator::Projection<double, 3, 2, 10>;
+  using RawBasis = mini::integrator::RawBasis<double, 3, 2>;
+  using MatKx1 = typename ProjFunc::MatKx1;
+  auto func = [](Coord const &point) {
+    return RawBasis::CallAt(point);
+  };
+  auto basis = Basis(gauss_);
+  auto proj_func = ProjFunc(func, basis);
+  static_assert(ProjFunc::K == 10);
+  static_assert(ProjFunc::N == 10);
+  auto point = Coord{ 0.3, 0.4, 0.5 };
+  auto pdv_actual = proj_func.GetPdvValue(point);
+  auto coef = ProjFunc::MatKxN(); coef.setIdentity();
+  auto pdv_expect = RawBasis::GetPdvValue(point, coef);
+  ProjFunc::MatKxN diff = pdv_actual - pdv_expect;
+  EXPECT_NEAR(diff.cwiseAbs().maxCoeff(), 0.0, 1e-14);
+  auto s_actual = proj_func.GetSmoothness();
+  std::cout << "s_actual =\n" << s_actual << std::endl;
+  EXPECT_NEAR(s_actual[0], 0.0, 1e-14);
+  EXPECT_NEAR(s_actual[1], 8.0, 1e-13);
+  EXPECT_NEAR(s_actual[2], 8.0, 1e-14);
+  EXPECT_NEAR(s_actual[3], 8.0, 1e-14);
+  EXPECT_NEAR(s_actual[4], 80.0/3, 1e-13);
+  EXPECT_NEAR(s_actual[5], 64.0/3, 1e-13);
+  EXPECT_NEAR(s_actual[6], 64.0/3, 1e-13);
+  EXPECT_NEAR(s_actual[7], 80.0/3, 1e-13);
+  EXPECT_NEAR(s_actual[8], 64.0/3, 1e-13);
+  EXPECT_NEAR(s_actual[9], 80.0/3, 1e-12);
+}
 
 class TestProjFunc : public ::testing::Test {
  protected:
@@ -88,65 +118,12 @@ class TestProjFunc : public ::testing::Test {
   using MatNx1 = algebra::Matrix<double, N, 1>;
   static constexpr int K = 10;
   using MatKxN = algebra::Matrix<double, K, N>;
-  static MatKxN GetMpdv(double x, double y, double z) {
-    MatKxN mat_pdv;
-    mat_pdv.setZero();
-    for (int i = 1; i < N; ++i)
-      mat_pdv(i, i) = 1;
-    mat_pdv(4, 1) = 2 * x;
-    mat_pdv(5, 1) = y;
-    mat_pdv(6, 1) = z;
-    mat_pdv(5, 2) = x;
-    mat_pdv(7, 2) = 2 * y;
-    mat_pdv(8, 2) = z;
-    mat_pdv(6, 3) = x;
-    mat_pdv(8, 3) = y;
-    mat_pdv(9, 3) = 2 * z;
-    return mat_pdv;
-  }
-
   using CgnsMesh = mini::mesh::cgns::File<double>;
   using MetisMesh = mini::mesh::metis::Mesh<idx_t>;
   using MapperType = mini::mesh::mapper::CgnsToMetis<double, idx_t>;
   using FieldType = mini::mesh::cgns::Field<double>;
   std::string const test_data_dir_{TEST_DATA_DIR};
 };
-TEST_F(TestProjFunc, Derivative) {
-  Mat3x1 origin = {0, 0, 0};
-  Mat3x8 xyz_global_i;
-  xyz_global_i.row(0) << -1, +1, +1, -1, -1, +1, +1, -1;
-  xyz_global_i.row(1) << -1, -1, +1, +1, -1, -1, +1, +1;
-  xyz_global_i.row(2) << -1, -1, -1, -1, +1, +1, +1, +1;
-  auto hexa = Hexa4x4x4(xyz_global_i);
-  auto basis = BasisType();
-  OrthoNormalize(&basis, hexa);
-  auto fvector = [](Mat3x1 const& xyz) {
-    auto x = xyz[0], y = xyz[1], z = xyz[2];
-    MatNx1 func(1, x, y, z, x * x, x * y, x * z, y * y, y * z, z * z);
-    return func;
-  };
-  using Pvector = ProjFunc<double, 3, 2, 10>;
-  auto pf_vec = Pvector(fvector, basis, hexa);
-  double x = 0.0, y = 0.0, z = 0.0;
-  auto mat_actual = pf_vec.GetMpdv({x, y, z});
-  std::cout << "mat_actual =\n" << mat_actual << std::endl;
-  auto mat_expect = GetMpdv(x, y, z);
-  std::cout << "mat_expect =\n" << mat_expect << std::endl;
-  MatKxN diff = mat_actual - mat_expect;
-  EXPECT_NEAR(diff.cwiseAbs().maxCoeff(), 0.0, 1e-14);
-  auto s_actual = pf_vec.GetSmoothness(hexa);
-  std::cout << "s_actual =\n" << s_actual << std::endl;
-  EXPECT_NEAR(s_actual(0), 0.0, 1e-14);
-  EXPECT_NEAR(s_actual(1), 8.0, 1e-13);
-  EXPECT_NEAR(s_actual(2), 8.0, 1e-14);
-  EXPECT_NEAR(s_actual(3), 8.0, 1e-14);
-  EXPECT_NEAR(s_actual(4), 80.0/3, 1e-13);
-  EXPECT_NEAR(s_actual(5), 64.0/3, 1e-13);
-  EXPECT_NEAR(s_actual(6), 64.0/3, 1e-13);
-  EXPECT_NEAR(s_actual(7), 80.0/3, 1e-13);
-  EXPECT_NEAR(s_actual(8), 64.0/3, 1e-13);
-  EXPECT_NEAR(s_actual(9), 80.0/3, 1e-12);
-}
 TEST_F(TestProjFunc, Reconstruction) {
   auto case_name = std::string("simple_cube");
   char cmd[1024];
