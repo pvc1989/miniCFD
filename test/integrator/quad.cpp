a5f8366211f94427e008ce12bde3ee054dd71c49
jyy <jiang.yu.yan@icloud.com>
2021-10-16 19:54:01 +0800

    Remove deprecated `ProjFunc`.

--- test/integrator/quad.cpp
+++ test/integrator/quad.cpp
@@ -20,12 +20,10 @@ class TestQuad4x4 : public ::testing::Test {
   using Mat2x1 = algebra::Matrix<double, 2, 1>;
   using Mat3x4 = algebra::Matrix<double, 3, 4>;
   using Mat3x1 = algebra::Matrix<double, 3, 1>;
-  using B = Basis<double, 2, 2>;
-  using Y = typename B::MatNx1;
-  using A = typename B::MatNxN;
-  using Pscalar = ProjFunc<double, 2, 2, 1>;
+  using Basis = OrthoNormalBasis<double, 2, 2>;
+  using Y = typename Basis::MatNx1;
+  using A = typename Basis::MatNxN;
   using Mat1x6 = algebra::Matrix<double, 1, 6>;
-  using Pvector = ProjFunc<double, 2, 2, 7>;
   using Mat7x1 = algebra::Matrix<double, 7, 1>;
   using Mat7x6 = algebra::Matrix<double, 7, 6>;
 };
@@ -82,66 +80,56 @@ TEST_F(TestQuad4x4, In3dSpace) {
 }
 TEST_F(TestQuad4x4, Basis) {
   Mat2x1 origin = {0, 0}, left = {-1, 2}, right = {1, 3};
-  B b; double residual;
-  b = B();
-  EXPECT_EQ(b(origin), Y(1, 0, 0, 0, 0, 0));
-  EXPECT_EQ(b(left), Y(1, -1, 2, 1, -2, 4));
-  EXPECT_EQ(b(right), Y(1, 1, 3, 1, 3, 9));
   Mat2x4 xyz_global_i;
   xyz_global_i.row(0) << -1, 1, 1, -1;
   xyz_global_i.row(1) << -1, -1, 1, 1;
   auto quad = Quad2D4x4(xyz_global_i);
-  OrthoNormalize(&b, quad);
-  residual = (Integrate([&b](const Mat2x1& xy) {
-    auto col = b(xy);
+  auto basis = Basis(quad);
+  double residual = (Integrate([&basis](const Mat2x1& xy) {
+    auto col = basis(xy);
     A prod = col * col.transpose();
     return prod;
   }, quad) - A::Identity()).cwiseAbs().maxCoeff();
   EXPECT_NEAR(residual, 0.0, 1e-15);
-  b = B(left);
-  EXPECT_EQ(b(origin), Y(1, 1, -2, 1, -2, 4));
-  EXPECT_EQ(b(left), Y(1, 0, 0, 0, 0, 0));
-  EXPECT_EQ(b(right), Y(1, 2, 1, 4, 2, 1));
   auto x = left[0], y = left[1];
   xyz_global_i.row(0) << x-1, x+1, x+1, x-1;
   xyz_global_i.row(1) << y-1, y-1, y+1, y+1;
   quad = Quad2D4x4(xyz_global_i);
-  b.OrthoNormalize(quad);
-  residual = (Integrate([&b](Mat2x1 const& xy) {
-    auto col = b(xy);
+  basis = Basis(quad);
+  residual = (Integrate([&basis](Mat2x1 const& xy) {
+    auto col = basis(xy);
     A prod = col * col.transpose();
     return prod;
   }, quad) - A::Identity()).cwiseAbs().maxCoeff();
   EXPECT_NEAR(residual, 0.0, 1e-15);
 }
-TEST_F(TestQuad4x4, ProjFunc) {
+TEST_F(TestQuad4x4, Projection) {
   Mat2x4 xyz_global_i;
   xyz_global_i.row(0) << -1, 1, 1, -1;
   xyz_global_i.row(1) << -1, -1, 1, 1;
   auto quad = Quad2D4x4(xyz_global_i);
-  B b;
-  OrthoNormalize(&b, quad);
-  auto fscalar = [](Mat2x1 const& xy){
+  auto basis = Basis(quad);
+  auto scalar_f = [](Mat2x1 const& xy){
     return xy[0] * xy[1];
   };
-  auto scalar = Pscalar(fscalar, b, quad);
-  double residual = (scalar.GetCoef() - Mat1x6(0, 0, 0, 0, 1, 0))
-      .cwiseAbs().maxCoeff();
+  using ScalarPF = Projection<double, 2, 2, 1>;
+  auto scalar_pf = ScalarPF(scalar_f, basis);
+  double residual = (scalar_pf.GetCoef()
+      - Mat1x6(0, 0, 0, 0, 1, 0)).cwiseAbs().maxCoeff();
   EXPECT_NEAR(residual, 0.0, 1e-15);
-  auto fvector = [](Mat2x1 const& xy) {
+  auto vector_f = [](Mat2x1 const& xy) {
     auto x = xy[0], y = xy[1];
-    Mat7x1 func(0, 1,
-                x, y,
-                x * x, x * y, y * y);
+    Mat7x1 func(0, 1, x, y, x * x, x * y, y * y);
     return func;
   };
-  auto vec = Pvector(fvector, b, quad);
+  using VectorPF = Projection<double, 2, 2, 7>;
+  auto vector_pf = VectorPF(vector_f, basis);
   Mat7x6 exact_vector{
       {0, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0},
       {0, 0, 1, 0, 0, 0}, {0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 1, 0},
       {0, 0, 0, 0, 0, 1}
   };
-  Mat7x6 abs_diff = vec.GetCoef() - exact_vector;
+  Mat7x6 abs_diff = vector_pf.GetCoef() - exact_vector;
   EXPECT_NEAR(abs_diff.cwiseAbs().maxCoeff(), 0.0, 1e-15);
 }
 
