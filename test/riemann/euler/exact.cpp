ab08f471d1d1e1064d90225a3c3ca4651bd837c4
jyy <jiang.yu.yan@icloud.com>
2021-10-22 21:31:49 +0800

    Add tests for 3D problems.

--- test/riemann/euler/exact.cpp
+++ test/riemann/euler/exact.cpp
@@ -166,6 +166,89 @@ TEST_F(TestExact2d, TestVacuumed) {
               solver.GetFlux({0.0, 0.0, v_right, 0.0}));
 }
 
+class TestExact3d : public ::testing::Test {
+ protected:
+  using Solver = Exact<IdealGas<1, 4>, 3>;
+  using State = Solver::State;
+  using Speed = State::Speed;
+  using Flux = Solver::FluxType;
+  Solver solver;
+  Speed v__left{1.5}, v_right{2.5};
+  Speed w__left{1.5}, w_right{0.5};
+  static void ExpectNear(double x, double y, double eps) {
+    if (x == 0) {
+      EXPECT_EQ(y, 0);
+    } else {
+      EXPECT_NEAR(y / x, 1, eps);
+    }
+  }
+  static void CompareFlux(Flux const& lhs, Flux const& rhs) {
+    constexpr double eps = 1e-4;
+    ExpectNear(lhs.mass, rhs.mass, eps);
+    ExpectNear(lhs.energy, rhs.energy, eps);
+    ExpectNear(lhs.momentum[0], rhs.momentum[0], eps);
+    ExpectNear(lhs.momentum[1], rhs.momentum[1], eps);
+    ExpectNear(lhs.momentum[2], rhs.momentum[2], eps);
+  }
+};
+TEST_F(TestExact3d, TestEqualStates) {
+  State  left{1.0, 0.0, v__left, w__left, 1.0};
+  State right{1.0, 0.0, v_right, w_right, 1.0};
+  CompareFlux(solver.GetFlux(left), solver.GetFlux(right));
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+              solver.GetFlux(left));
+  left.u() = +1.0;
+  right.u() = left.u();
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+              solver.GetFlux(left));
+  left.u() = -1.0;
+  right.u() = left.u();
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+              solver.GetFlux(right));
+}
+TEST_F(TestExact3d, TestSod) {
+  State  left{1.000, 0.0, v__left, w__left, 1.0};
+  State right{0.125, 0.0, v_right, w_right, 0.1};
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+      solver.GetFlux({0.426319, +0.927453, v__left, w__left, 0.303130}));
+  CompareFlux(solver.GetFluxOnTimeAxis(right, left),
+      solver.GetFlux({0.426319, -0.927453, v__left, w__left, 0.303130}));
+}
+TEST_F(TestExact3d, TestShockCollision) {
+  State  left{5.99924, 19.59750, v__left, w__left, 460.894};
+  State right{5.99242, -6.19633, v_right, w_right, 46.0950};
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+              solver.GetFlux({5.99924, 19.5975, v__left, w__left, 460.894}));
+}
+TEST_F(TestExact3d, TestBlastFromLeft) {
+  State  left{1.0, 0.0, v__left, w__left, 1e+3};
+  State right{1.0, 0.0, v_right, w_right, 1e-2};
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+              solver.GetFlux({0.575062, 19.59745, v__left, w__left, 460.8938}));
+}
+TEST_F(TestExact3d, TestBlastFromRight) {
+  State  left{1.0, 0.0, v__left, w__left, 1e-2};
+  State right{1.0, 0.0, v_right, w_right, 1e+2};
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+      solver.GetFlux({0.575113, -6.196328, v_right, w_right, 46.09504}));
+}
+TEST_F(TestExact3d, TestAlmostVacuumed) {
+  State  left{1.0, -2.0, v__left, w__left, 0.4};
+  State right{1.0, +2.0, v_right, w_right, 0.4};
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+              solver.GetFlux({0.21852, 0.0, v__left, w__left, 0.001894}));
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+              solver.GetFlux({0.21852, 0.0, v_right, w_right, 0.001894}));
+}
+TEST_F(TestExact3d, TestVacuumed) {
+  State  left{1.0, -4.0, v__left, w__left, 0.4};
+  State right{1.0, +4.0, v_right, w_right, 0.4};
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+              solver.GetFlux({0.0, 0.0, v__left, w__left, 0.0}));
+  CompareFlux(solver.GetFluxOnTimeAxis(left, right),
+              solver.GetFlux({0.0, 0.0, v_right, w_right, 0.0}));
+}
+
 }  // namespace euler
 }  // namespace riemann
 }  // namespace mini
