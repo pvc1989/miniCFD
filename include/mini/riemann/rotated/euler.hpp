bed00bcfcd77577e9c0287d62f99549511c46d0c
Weicheng Pei <weicheng.pei@icloud.com>
2021-11-11 20:10:32 +0800

    Check `Rotate()`.

--- include/mini/riemann/rotated/euler.hpp
+++ include/mini/riemann/rotated/euler.hpp
@@ -18,49 +18,49 @@ class Cartesian;
 
 template <typename Scalar>
 class Cartesian<Scalar, 2> {
+  static constexpr int x{0}, y{1}, n{0}, t{1};
+
  public:
   using Vector = mini::algebra::Column<Scalar, 2>;
 
-  void Rotate(const Vector& normal) {
-    normal_ = normal;
+  void Rotate(const Vector& nu) {
+    nu_ = nu;
   }
-  void Rotate(const Scalar& n_0, const Scalar& n_1) {
-    normal_[0] = n_0;
-    normal_[1] = n_1;
+  void Rotate(const Scalar& nu_x, const Scalar& nu_y) {
+    nu_[x] = nu_x;
+    nu_[y] = nu_y;
   }
   void GlobalToNormal(Vector* v) {
-    auto& n = normal_;
     /* Calculate the normal component: */
-    auto v_n = (*v)[0] * n[0] + (*v)[1] * n[1];
-    /* Calculate the tangential component:
-       auto t = Vector{ -n[1], n[0] };
-       auto v_t = v->Dot(t);
-    */
-    (*v)[1] = n[0] * (*v)[1] - n[1] * (*v)[0];
+    auto v_n = (*v)[x] * nu_[x] + (*v)[y] * nu_[y];
+    /* Calculate the tangential component: */
+    auto t_x = -nu_[y], t_y = nu_[x];
+    (*v)[t] =  t_x * (*v)[x] + t_y * (*v)[y];
     /* Write the normal component: */
-    (*v)[0] = v_n;
+    (*v)[n] = v_n;
   }
   void NormalToGlobal(Vector* v) {
-    auto& n = normal_;
-    auto v_0 = (*v)[0] * n[0] - (*v)[1] * n[1];
-    (*v)[1] = (*v)[0] * n[1] + (*v)[1] * n[0];
-    (*v)[0] = v_0;
+    auto t_x = -nu_[y], t_y = nu_[x];
+    auto v_x = (*v)[n] * nu_[x] + (*v)[t] * t_x;
+    (*v)[y]  = (*v)[n] * nu_[y] + (*v)[t] * t_y;
+    (*v)[x] = v_x;
   }
 
  private:
-  Vector normal_{1.0, 0.0};
+  Vector nu_{1.0, 0.0};
   static_assert(std::is_scalar_v<Scalar>);
 };
 
 template <typename Scalar>
 class Cartesian<Scalar, 3> {
+  static constexpr int x = 0, y = 1, z = 2;
+
  public:
   using Vector = mini::algebra::Column<Scalar, 3>;
 
   void Rotate(const Vector& nu, const Vector& sigma) {
     nu_ = nu; sigma_ = sigma;
     // \vec{\pi} = \vec{\nu} \cross \vec{\sigma}
-    constexpr int x = 0, y = 1, z = 2;
     pi_[x] = nu[y] * sigma[z] - nu[z] * sigma[y];
     pi_[y] = nu[z] * sigma[x] - nu[x] * sigma[z];
     pi_[z] = nu[x] * sigma[y] - nu[y] * sigma[x];
@@ -70,18 +70,17 @@ class Cartesian<Scalar, 3> {
   }
   void Rotate(const mini::algebra::Matrix<Scalar, 3, 3> &frame) {
     auto &nu = frame.col(0), &sigma = frame.col(1), &pi = frame.col(2);
-    constexpr int x = 0, y = 1, z = 2;
     nu_[x] = nu[x]; sigma_[x] = sigma[x]; pi_[x] = pi[x];
     nu_[y] = nu[y]; sigma_[y] = sigma[y]; pi_[y] = pi[y];
     nu_[z] = nu[z]; sigma_[z] = sigma[z]; pi_[z] = pi[z];
   }
   void GlobalToNormal(Vector* v) {
     auto v_nu = v->Dot(nu_), v_sigma = v->Dot(sigma_), v_pi = v->Dot(pi_);
-    (*v)[0] = v_nu; (*v)[1] = v_sigma; (*v)[2] = v_pi;
+    constexpr int nu = 0, sigma = 1, pi = 2;
+    (*v)[nu] = v_nu; (*v)[sigma] = v_sigma; (*v)[pi] = v_pi;
   }
   void NormalToGlobal(Vector* v) {
     constexpr int nu = 0, sigma = 1, pi = 2;
-    constexpr int  x = 0,     y = 1,  z = 2;
     auto v_x = (*v)[nu] * nu_[x] + (*v)[sigma] * sigma_[x] + (*v)[pi] * pi_[x];
     auto v_y = (*v)[nu] * nu_[y] + (*v)[sigma] * sigma_[y] + (*v)[pi] * pi_[y];
     auto v_z = (*v)[nu] * nu_[z] + (*v)[sigma] * sigma_[z] + (*v)[pi] * pi_[z];
@@ -110,9 +109,9 @@ class Euler {
     static_assert(kDim == 2);
     cartesian_.Rotate(normal);
   }
-  void Rotate(const Scalar& n_1, const Scalar& n_2) {
+  void Rotate(const Scalar& n_x, const Scalar& n_y) {
     static_assert(kDim == 2);
-    cartesian_.Rotate(n_1, n_2);
+    cartesian_.Rotate(n_x, n_y);
   }
   void Rotate(const Vector& nu, const Vector& sigma) {
     static_assert(kDim == 3);
