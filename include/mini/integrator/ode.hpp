3f241c0f3082ce16868349a0922f723714cdc2fd
Weicheng Pei <weicheng.pei@icloud.com>
2021-11-20 22:04:36 +0800

    Hide details of ode solvers.

--- include/mini/integrator/ode.hpp
+++ include/mini/integrator/ode.hpp
@@ -3,49 +3,68 @@
 #include <stdexcept>
 #include "mini/mesh/part.hpp"
 
-template <typename Part, int kOrder>
-struct RungeKutta;
-
-template <typename Part>
-struct RungeKutta<Part, 3/* kOrder */> {
-  using MyPart = Part;
-  using MyCell = typename MyPart::CellType;
-  using MyFace = typename MyPart::FaceType;
-  using Projection = typename MyCell::Projection;
+template <typename PartType>
+class RungeKuttaBase {
+ public:
+  using Part = PartType;
+  using Cell = typename Part::CellType;
+  using Face = typename Part::FaceType;
+  using Projection = typename Cell::Projection;
   using Coeff = typename Projection::Coeff;
   using Value = typename Projection::Value;
 
-  std::vector<Coeff> u_old_, u_frac13_, u_frac23_, u_new_;
+ protected:
   std::vector<Coeff> rhs_;
   double dt_;
 
-  explicit RungeKutta(double dt)
+ public:
+  explicit RungeKuttaBase(double dt)
       : dt_(dt) {
     assert(dt > 0.0);
   }
-  RungeKutta(const RungeKutta &) = default;
-  RungeKutta& operator=(const RungeKutta &) = default;
-  RungeKutta(RungeKutta &&) noexcept = default;
-  RungeKutta& operator=(RungeKutta &&) noexcept = default;
-  ~RungeKutta() noexcept = default;
+  RungeKuttaBase(const RungeKuttaBase &) = default;
+  RungeKuttaBase& operator=(const RungeKuttaBase &) = default;
+  RungeKuttaBase(RungeKuttaBase &&) noexcept = default;
+  RungeKuttaBase& operator=(RungeKuttaBase &&) noexcept = default;
+  ~RungeKuttaBase() noexcept = default;
 
-  static void ReadFromLocalCells(const MyPart &part, std::vector<Coeff> *coeffs) {
+ public:
+  static void ReadFromLocalCells(const Part &part, std::vector<Coeff> *coeffs) {
     coeffs->resize(part.CountLocalCells());
     part.ForEachLocalCell([&coeffs](const auto &cell){
       coeffs->at(cell.id())
           = cell.projection_.GetCoeffOnOrthoNormalBasis();
     });
   }
-  static void WriteToLocalCells(const std::vector<Coeff> &coeffs, MyPart *part) {
+  static void WriteToLocalCells(const std::vector<Coeff> &coeffs, Part *part) {
     assert(coeffs.size() == part->CountLocalCells());
     part->ForEachLocalCell([&coeffs](auto &cell){
       Coeff new_coeff = coeffs.at(cell.id()) * cell.basis_.coeff();
       cell.projection_.UpdateCoeffs(new_coeff);
     });
   }
-  static void UpdateLocalRhs(MyPart &part, std::vector<Coeff> *rhs) {
-    assert(rhs->size() == part.CountLocalCells());
-    part.ForEachLocalFace([&rhs](MyFace &face){
+  void InitializeRhs(const Part &part) {
+    rhs_.resize(part.CountLocalCells());
+    for (auto& coeff : rhs_) {
+      coeff.setZero();
+    }
+    part.ForEachLocalCell([this](const Cell &cell){
+      auto& r = this->rhs_.at(cell.id());
+      const auto& gauss = *(cell.gauss_ptr_);
+      for (int q = 0; q < gauss.CountQuadPoints(); ++q) {
+        const auto& xyz = gauss.GetGlobalCoord(q);
+        Value cv = cell.projection_(xyz);
+        auto flux = Face::Riemann::GetFluxMatrix(cv);
+        auto grad = cell.basis_.GetGradValue(xyz);
+        Coeff prod = flux * grad.transpose();
+        prod *= gauss.GetGlobalWeight(q);
+        r += prod;
+      }
+    });
+  }
+  void UpdateLocalRhs(Part &part) {
+    assert(rhs_.size() == part.CountLocalCells());
+    part.ForEachLocalFace([this](Face &face){
       const auto& gauss = *(face.gauss_ptr_);
       const auto& holder = *(face.holder_);
       const auto& sharer = *(face.sharer_);
@@ -56,27 +75,17 @@ struct RungeKutta<Part, 3/* kOrder */> {
         auto& riemann = face.GetRiemann(q);
         Value flux = riemann.GetFluxOnTimeAxis(u_holder, u_sharer);
         flux *= gauss.GetGlobalWeight(q);
-        rhs->at(holder.id()) -= flux * holder.basis_(coord).transpose();
-        rhs->at(sharer.id()) += flux * sharer.basis_(coord).transpose();
+        this->rhs_.at(holder.id()) -= flux * holder.basis_(coord).transpose();
+        this->rhs_.at(sharer.id()) += flux * sharer.basis_(coord).transpose();
       }
     });
   }
-  static void UpdateGhostRhs(MyPart &part, std::vector<Coeff> *rhs) {
-    assert(rhs->size() == part.CountLocalCells());
-    part.ForEachGhostFace([&rhs](MyFace &face){
+  void UpdateGhostRhs(Part &part) {
+    assert(rhs_.size() == part.CountLocalCells());
+    part.ForEachGhostFace([this](Face &face){
       const auto& gauss = *(face.gauss_ptr_);
       const auto& holder = *(face.holder_);
       const auto& sharer = *(face.sharer_);
-      // if (sharer.metis_id == 1856) {
-      //   std::cout << "sharer:\n" << sharer.projection_.coeff() << "\n";
-      // }
-      // if (holder.metis_id == 1856) {
-      //   std::cout << "holder:\n" << holder.projection_.coeff() << "\n";
-      // }
-      // if (holder.projection_.coeff().hasNaN())
-      //   throw std::range_error("holder has NaN");
-      // if (sharer.projection_.coeff().hasNaN())
-      //   throw std::range_error("sharer has NaN");
       for (int q = 0; q < gauss.CountQuadPoints(); ++q) {
         const auto& coord = gauss.GetGlobalCoord(q);
         Value u_holder = holder.projection_(coord);
@@ -85,12 +94,12 @@ struct RungeKutta<Part, 3/* kOrder */> {
         Value flux = riemann.GetFluxOnTimeAxis(u_holder, u_sharer);
         flux *= gauss.GetGlobalWeight(q);
         Coeff temp = flux * holder.basis_(coord).transpose();
-        rhs->at(holder.id()) -= temp;
+        this->rhs_.at(holder.id()) -= temp;
       }
     });
   }
-  static void UpdateBoundaryRhs(MyPart &part, std::vector<Coeff> *rhs) {
-    part.ForEachSolidFace([&rhs](MyFace &face){
+  void UpdateBoundaryRhs(Part &part) {
+    part.ForEachSolidFace([this](Face &face){
       const auto& gauss = *(face.gauss_ptr_);
       assert(face.sharer_ == nullptr);
       const auto& holder = *(face.holder_);
@@ -100,44 +109,133 @@ struct RungeKutta<Part, 3/* kOrder */> {
         Value u_holder = holder.projection_(coord);
         Value flux = riemann.GetFluxOnSolidWall(u_holder);
         flux *= gauss.GetGlobalWeight(q);
-        rhs->at(holder.id()) -= flux * holder.basis_(coord).transpose();
+        this->rhs_.at(holder.id()) -= flux * holder.basis_(coord).transpose();
       }
     });
   }
-  void InitializeRhs(const MyPart &part) {
-    rhs_.resize(part.CountLocalCells());
-    for (auto& coeff : rhs_) {
-      coeff.setZero();
+};
+
+template <typename PartType, int kTemporalAccuracy = 3>
+struct RungeKutta;
+
+template <typename PartType>
+struct RungeKutta<PartType, 1> : public RungeKuttaBase<PartType> {
+ private:
+  using Base = RungeKuttaBase<PartType>;
+
+ public:
+  using Part = PartType;
+  using Cell = typename Base::Cell;
+  using Face = typename Base::Face;
+  using Projection = typename Base::Projection;
+  using Coeff = typename Base::Coeff;
+  using Value = typename Base::Value;
+
+ private:
+  std::vector<Coeff> u_old_, u_new_;
+
+ public:
+  using Base::Base;
+  RungeKutta(const RungeKutta &) = default;
+  RungeKutta& operator=(const RungeKutta &) = default;
+  RungeKutta(RungeKutta &&) noexcept = default;
+  RungeKutta& operator=(RungeKutta &&) noexcept = default;
+  ~RungeKutta() noexcept = default;
+
+ public:
+  template <class Limiter>
+  void Update(Part *part_ptr, Limiter& limiter) {
+    Part &part = *part_ptr;  // TODO: change to const ref
+
+    Base::ReadFromLocalCells(part, &u_old_);
+    part_ptr->ShareGhostCellCoeffs();
+    this->InitializeRhs(part);
+    this->UpdateLocalRhs(part);
+    this->UpdateBoundaryRhs(part);
+    part_ptr->UpdateGhostCellCoeffs();
+    this->UpdateGhostRhs(part);
+    auto n_cells = u_old_.size();
+    assert(n_cells == rhs_.size());
+    u_new_ = this->rhs_;
+    for (int i_cell = 0; i_cell < n_cells; ++i_cell) {
+      u_new_[i_cell] *= this->dt_;
+      u_new_[i_cell] += u_old_[i_cell];
     }
-    part.ForEachLocalCell([&](const MyCell &cell){
-      auto& r = rhs_.at(cell.id());
-      const auto& gauss = *(cell.gauss_ptr_);
-      for (int q = 0; q < gauss.CountQuadPoints(); ++q) {
-        const auto& xyz = gauss.GetGlobalCoord(q);
-        Value cv = cell.projection_(xyz);
-        auto flux = MyFace::Riemann::GetFluxMatrix(cv);
-        auto grad = cell.basis_.GetGradValue(xyz);
-        Coeff prod = flux * grad.transpose();
-        prod *= gauss.GetGlobalWeight(q);
-        r += prod;
-      }
-    });
+    Base::WriteToLocalCells(u_new_, part_ptr);
+    // part_ptr->Reconstruct(limiter);  // only for high order methods
   }
-  void UpdateLocalRhs(MyPart &part) {
-    UpdateLocalRhs(part, &rhs_);
+};
+
+template <typename PartType>
+struct RungeKutta<PartType, 3> : public RungeKuttaBase<PartType> {
+ private:
+  using Base = RungeKuttaBase<PartType>;
+
+ public:
+  using Part = PartType;
+  using Cell = typename Base::Cell;
+  using Face = typename Base::Face;
+  using Projection = typename Base::Projection;
+  using Coeff = typename Base::Coeff;
+  using Value = typename Base::Value;
+
+ private:
+  std::vector<Coeff> u_old_, u_frac13_, u_frac23_, u_new_;
+
+ public:
+  using Base::Base;
+  RungeKutta(const RungeKutta &) = default;
+  RungeKutta& operator=(const RungeKutta &) = default;
+  RungeKutta(RungeKutta &&) noexcept = default;
+  RungeKutta& operator=(RungeKutta &&) noexcept = default;
+  ~RungeKutta() noexcept = default;
+
+ public:
+  template <class Limiter>
+  void Update(Part *part_ptr, Limiter& limiter) {
+    Part &part = *part_ptr;  // TODO: change to const ref
+
+    Base::ReadFromLocalCells(part, &u_old_);
+    part_ptr->ShareGhostCellCoeffs();
+    this->InitializeRhs(part);
+    this->UpdateLocalRhs(part);
+    this->UpdateBoundaryRhs(part);
+    part_ptr->UpdateGhostCellCoeffs();
+    this->UpdateGhostRhs(part);
+    this->SolveFrac13();
+    Base::WriteToLocalCells(u_frac13_, part_ptr);
+    part_ptr->Reconstruct(limiter);
+
+    Base::ReadFromLocalCells(part, &u_frac13_);
+    part_ptr->ShareGhostCellCoeffs();
+    this->InitializeRhs(part);
+    this->UpdateLocalRhs(part);
+    this->UpdateBoundaryRhs(part);
+    part_ptr->UpdateGhostCellCoeffs();
+    this->UpdateGhostRhs(part);
+    this->SolveFrac23();
+    Base::WriteToLocalCells(u_frac23_, part_ptr);
+    part_ptr->Reconstruct(limiter);
+
+    Base::ReadFromLocalCells(part, &u_frac23_);
+    part_ptr->ShareGhostCellCoeffs();
+    this->InitializeRhs(part);
+    this->UpdateLocalRhs(part);
+    this->UpdateBoundaryRhs(part);
+    part_ptr->UpdateGhostCellCoeffs();
+    this->UpdateGhostRhs(part);
+    this->SolveFrac33();
+    Base::WriteToLocalCells(u_new_, part_ptr);
+    part_ptr->Reconstruct(limiter);
   }
-  void UpdateGhostRhs(MyPart &part) {
-    UpdateGhostRhs(part, &rhs_);
-  }
-  void UpdateBoundaryRhs(MyPart &part) {
-    UpdateBoundaryRhs(part, &rhs_);
-  }
+
+ private:
   void SolveFrac13() {
     auto n_cells = u_old_.size();
     assert(n_cells == rhs_.size());
-    u_frac13_ = rhs_;
+    u_frac13_ = this->rhs_;
     for (int i_cell = 0; i_cell < n_cells; ++i_cell) {
-      u_frac13_[i_cell] *= dt_;
+      u_frac13_[i_cell] *= this->dt_;
       u_frac13_[i_cell] += u_old_[i_cell];
     }
   }
@@ -145,9 +243,9 @@ struct RungeKutta<Part, 3/* kOrder */> {
     auto n_cells = u_old_.size();
     assert(n_cells == rhs_.size());
     assert(n_cells == u_frac13_.size());
-    u_frac23_ = rhs_;
+    u_frac23_ = this->rhs_;
     for (int i_cell = 0; i_cell < n_cells; ++i_cell) {
-      u_frac23_[i_cell] *= dt_;
+      u_frac23_[i_cell] *= this->dt_;
       u_frac23_[i_cell] += u_frac13_[i_cell];
       u_frac23_[i_cell] += u_old_[i_cell];
       u_frac23_[i_cell] += u_old_[i_cell];
@@ -160,9 +258,9 @@ struct RungeKutta<Part, 3/* kOrder */> {
     assert(n_cells == rhs_.size());
     assert(n_cells == u_frac13_.size());
     assert(n_cells == u_frac23_.size());
-    u_new_ = rhs_;
+    u_new_ = this->rhs_;
     for (int i_cell = 0; i_cell < n_cells; ++i_cell) {
-      u_new_[i_cell] *= 2 * dt_;
+      u_new_[i_cell] *= 2 * this->dt_;
       u_new_[i_cell] += u_frac23_[i_cell];
       u_new_[i_cell] += u_frac23_[i_cell];
       u_new_[i_cell] += u_old_[i_cell];
