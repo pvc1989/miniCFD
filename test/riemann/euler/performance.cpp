71d4f8b0bd614efd1b8ba9e687a547fba31097b7
JiangYY <jiang.yu.yan@icloud.com>
2021-11-29 21:46:53 +0800

    Compare performance.

---
+++ test/riemann/euler/performance.cpp
@@ -0,0 +1,74 @@
+// Copyright 2021 PEI WeiCheng and JIANG YuYan
+
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "mini/riemann/euler/types.hpp"
+#include "mini/riemann/euler/exact.hpp"
+#include "mini/riemann/euler/ausm.hpp"
+#include "mini/riemann/euler/hllc.hpp"
+
+namespace mini {
+namespace riemann {
+namespace euler {
+
+using Gas = IdealGas<1, 4>;
+
+template <class Solver>
+auto run() {
+  using State = typename Solver::State;
+  using Flux = typename Solver::Flux;
+  Solver solver;
+  Flux flux{};
+  {
+  State left{1.0, 0.0, 1.0}, right{0.125, 0.0, 0.1};
+  flux += solver.GetFluxOnTimeAxis(left, right);
+  flux += solver.GetFluxOnTimeAxis(right, left);
+  } {
+  State left{5.99924, 19.5975, 460.894}, right{5.99242, 6.19633, 46.0950};
+  flux += solver.GetFluxOnTimeAxis(left, right);
+  } {
+  State left{1.0, 0.0, 1000}, right{1.0, 0.0, 0.01};
+  flux += solver.GetFluxOnTimeAxis(left, right);
+  } {
+  State left{1.0, 0.0, 0.01}, right{1.0, 0.0, 100};
+  flux += solver.GetFluxOnTimeAxis(left, right);
+  } {
+  State left{1.0, -2.0, 0.4}, right{1.0, +2.0, 0.4};
+  flux += solver.GetFluxOnTimeAxis(left, right);
+  } {
+  State left{1.0, -4.0, 0.4}, right{1.0, +4.0, 0.4};
+  flux += solver.GetFluxOnTimeAxis(left, right);
+  }
+  return flux;
+}
+
+class TestPerformance : public ::testing::Test {
+ public:
+  int n = 1000000;
+};
+TEST_F(TestPerformance, TestAusm) {
+  for (int i = 0; i < n; ++i) {
+    run<Ausm<Gas>>();
+  }
+}
+TEST_F(TestPerformance, TestHllc) {
+  for (int i = 0; i < n; ++i) {
+    run<Hllc<Gas>>();
+  }
+}
+TEST_F(TestPerformance, TestExact) {
+  for (int i = 0; i < n; ++i) {
+    run<Exact<Gas>>();
+  }
+}
+
+}  // namespace euler
+}  // namespace riemann
+}  // namespace mini
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
