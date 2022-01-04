a7456bae90e701b5b2701126fc79bf31ad6f8891
Weicheng Pei <weicheng.pei@icloud.com>
2022-01-04 21:18:37 +0800

    Test more methods.

--- test/riemann/euler/types.cpp
+++ test/riemann/euler/types.cpp
@@ -10,20 +10,34 @@ namespace mini {
 namespace riemann {
 namespace euler {
 
-class TestIdealGas : public ::testing::Test {
- protected:
-  using Gas = IdealGas<double, 1, 4>;
-  static constexpr auto gamma = Gas::Gamma();
+class TestTypes : public ::testing::Test {
 };
-TEST_F(TestIdealGas, TestConverters) {
+TEST_F(TestTypes, TestTuples) {
+  auto rho{0.1}, u{+0.3}, v{-0.4}, p{0.5};
+  auto primitive = PrimitiveTuple<double, 2>{rho, u, v, p};
+  EXPECT_DOUBLE_EQ(primitive[0], primitive.rho());
+  EXPECT_DOUBLE_EQ(primitive.rho(), primitive.mass());
+  EXPECT_DOUBLE_EQ(primitive[1], primitive.u());
+  EXPECT_DOUBLE_EQ(primitive.u(), primitive.momentumX());
+  EXPECT_DOUBLE_EQ(primitive[2], primitive.v());
+  EXPECT_DOUBLE_EQ(primitive.v(), primitive.momentumY());
+  EXPECT_DOUBLE_EQ(primitive[3], primitive.p());
+  EXPECT_DOUBLE_EQ(primitive.p(), primitive.energy());
+  using Vector = typename PrimitiveTuple<double, 2>::Vector;
+  EXPECT_EQ(primitive.momentum(), Vector(+0.3, -0.4));
+  EXPECT_DOUBLE_EQ(primitive.GetDynamicPressure(), rho * (u*u + v*v) / 2);
+}
+TEST_F(TestTypes, TestConverters) {
   auto rho{0.1}, u{+0.2}, v{-0.2}, p{0.3};
   auto primitive = PrimitiveTuple<double, 2>{rho, u, v, p};
+  using Gas = IdealGas<double, 1, 4>;
+  constexpr auto gamma = Gas::Gamma();
   auto conservative = ConservativeTuple<double, 2>{
     rho, rho*u, rho*v, p/(gamma-1) + 0.5*rho*(u*u + v*v)
   };
   EXPECT_EQ(Gas::PrimitiveToConservative(primitive), conservative);
   auto primitive_copy = Gas::ConservativeToPrimitive(conservative);
-  EXPECT_EQ(primitive_copy.rho(), rho);
+  EXPECT_DOUBLE_EQ(primitive_copy.rho(), rho);
   EXPECT_DOUBLE_EQ(primitive_copy.u(), u);
   EXPECT_DOUBLE_EQ(primitive_copy.v(), v);
   EXPECT_DOUBLE_EQ(primitive_copy.p(), p);
