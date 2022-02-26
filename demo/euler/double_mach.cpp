26c7742cbd0d229d030522c0b10da7e03c836515
Weicheng Pei <weicheng.pei@icloud.com>
2022-02-26 14:57:39 +0800

    Simplify problem definitions.

--- demo/euler/double_mach.cpp
+++ demo/euler/double_mach.cpp
@@ -1,218 +1,55 @@
-//  Copyright 2021 PEI Weicheng and JIANG Yuyan
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
+#include "main.hpp"
 
-#include "mpi.h"
-#include "pcgnslib.h"
+/* Set initial conditions. */
+constexpr double kGammaPlus = 2.4, kGamma = 1.4, kGammaMinus = 0.4;
+double rho_before = 1.4, p_before = 1.0;
+double m_before = 10.0, a_before = 1.0, u_nu = m_before * a_before;
+auto rho_after = rho_before * (m_before * m_before * kGammaPlus / 2.0)
+    / (1.0 + m_before * m_before * kGammaMinus / 2.0);  // 8.0
+auto p_after = p_before * (m_before * m_before * kGamma - kGammaMinus / 2.0)
+    / (kGammaPlus / 2.0);  // 116.5
+auto u_n_after = u_nu * (rho_after - rho_before) / rho_after;  // 8.25
+auto tan_60 = std::sqrt(3.0), cos_30 = tan_60 * 0.5, sin_30 = 0.5;
+auto u_after = u_n_after * cos_30, v_after = u_n_after * (-sin_30);
+auto primitive_after = Primitive(rho_after, u_after, v_after, 0.0, p_after);
+auto primitive_before = Primitive(rho_before, 0.0, 0.0, 0.0, p_before);
+Value value_after = Gas::PrimitiveToConservative(primitive_after);
+Value value_before = Gas::PrimitiveToConservative(primitive_before);
+double x_gap = 1.0 / 6.0;
 
-#include "mini/mesh/shuffler.hpp"
-#include "mini/riemann/euler/types.hpp"
-#include "mini/riemann/euler/eigen.hpp"
-#include "mini/riemann/euler/exact.hpp"
-#include "mini/riemann/rotated/euler.hpp"
-#include "mini/polynomial/limiter.hpp"
-#include "mini/stepping/explicit.hpp"
-#include "rkdg.hpp"
+Value MyIC(const Coord &xyz) {
+  auto x = xyz[0], y = xyz[1];
+  return ((x - x_gap) * tan_60 < y) ? value_after : value_before;
+}
 
-int main(int argc, char* argv[]) {
-  MPI_Init(NULL, NULL);
-  int n_cores, i_core;
-  MPI_Comm_size(MPI_COMM_WORLD, &n_cores);
-  MPI_Comm_rank(MPI_COMM_WORLD, &i_core);
-  cgp_mpi_comm(MPI_COMM_WORLD);
+/* Set boundary conditions. */
+auto u_x = u_nu / cos_30;
+auto moving_shock = [](const Coord& xyz, double t){
+  auto x = xyz[0], y = xyz[1];
+  return ((x - (x_gap + u_x * t)) * tan_60 < y) ? value_after : value_before;
+};
 
-  if (argc < 7) {
-    if (i_core == 0) {
-      std::cout << "usage:\n"
-          << "  mpirun -n <n_cores> " << argv[0] << " <cgns_file> <hexa|tetra>"
-          << " <t_start> <t_stop> <n_steps_per_frame> <n_frames>"
-          << " [<i_frame_start> [n_parts_prev]]\n";
-    }
-    MPI_Finalize();
-    exit(0);
-  }
-  auto old_file_name = std::string(argv[1]);
-  auto suffix = std::string(argv[2]);
-  double t_start = std::atof(argv[3]);
-  double t_stop = std::atof(argv[4]);
-  int n_steps_per_frame = std::atoi(argv[5]);
-  int n_frames = std::atoi(argv[6]);
-  int n_steps = n_frames * n_steps_per_frame;
-  auto dt = (t_stop - t_start) / n_steps;
-  int i_frame = 0;
-  if (argc > 7) {
-    i_frame = std::atoi(argv[7]);
-  }
-  int n_parts_prev = 0;
-  if (argc > 8) {
-    n_parts_prev = std::atoi(argv[8]);
-  }
-
-  std::string case_name = "double_mach_" + suffix;
-
-  auto time_begin = MPI_Wtime();
-
-  /* Define the Euler system. */
-  constexpr int kFunc = 5;
-  constexpr int kDim = 3;
-  using Primitive = mini::riemann::euler::PrimitiveTuple<double, kDim>;
-  using Conservative = mini::riemann::euler::ConservativeTuple<double, kDim>;
-  using Gas = mini::riemann::euler::IdealGas<double, 1, 4>;
-  using Unrotated = mini::riemann::euler::Exact<Gas, kDim>;
-  using Riemann = mini::riemann::rotated::Euler<Unrotated>;
-
-  /* Partition the mesh. */
-  if (i_core == 0 && n_parts_prev != n_cores) {
-    using Shuffler = mini::mesh::Shuffler<idx_t, double>;
-    Shuffler::PartitionAndShuffle(case_name, old_file_name, n_cores);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  constexpr int kOrder = 2;
-  using Part = mini::mesh::cgns::Part<cgsize_t, kOrder, Riemann>;
-  using Cell = typename Part::Cell;
-  using Face = typename Part::Face;
-  using Coord = typename Cell::Coord;
-  using Value = typename Cell::Value;
-  using Coeff = typename Cell::Coeff;
-
-  if (i_core == 0) {
-    std::printf("Create %d `Part`s at %f sec\n",
-        n_cores, MPI_Wtime() - time_begin);
-  }
-  auto part = Part(case_name, i_core);
-  part.SetFieldNames({"Density", "MomentumX", "MomentumY", "MomentumZ",
-      "EnergyStagnationDensity"});
-
-  /* Build a `Limiter` object. */
-  // using Limiter = mini::polynomial::LazyWeno<Cell>;
-  using Limiter = mini::polynomial::EigenWeno<Cell>;
-  auto limiter = Limiter(/* w0 = */0.001, /* eps = */1e-6);
-
-  /* Set initial conditions. */
-  double rho_before = 1.4, p_before = 1.0;
-  double m_before = 10.0, a_before = 1.0, u_gamma = m_before * a_before;
-  double gamma_plus = 2.4, gamma = 1.4, gamma_minus = 0.4;
-  auto rho_after = rho_before * (m_before * m_before * gamma_plus / 2.0)
-      / (1.0 + m_before * m_before * gamma_minus / 2.0);
-  assert(rho_after == 8.0);
-  auto p_after = p_before * (m_before * m_before * gamma - gamma_minus / 2.0)
-      / (gamma_plus / 2.0);
-  // assert(p_after == 116.5);
-  std::cout << "p_after = " << p_after << '\n';
-  auto u_n_after = u_gamma * (rho_after - rho_before) / rho_after;
-  // assert(u_n_after == 8.25);
-  std::cout << "u_n_after = " << u_n_after << '\n';
-  auto tan_60 = std::sqrt(3.0), cos_30 = tan_60 * 0.5, sin_30 = 0.5;
-  auto u_after = u_n_after * cos_30, v_after = u_n_after * (-sin_30);
-  auto primitive_after = Primitive(rho_after, u_after, v_after, 0.0, p_after);
-  auto primitive_before = Primitive(rho_before, 0.0, 0.0, 0.0, p_before);
-  Value value_after = Gas::PrimitiveToConservative(primitive_after);
-  Value value_before = Gas::PrimitiveToConservative(primitive_before);
-  double x_gap = 1.0 / 6.0;
-  auto initial_condition = [&](const Coord& xyz){
-    auto x = xyz[0], y = xyz[1];
-    return ((x - x_gap) * tan_60 < y) ? value_after : value_before;
-  };
-
-  if (argc == 7) {
-    if (i_core == 0) {
-      std::printf("[Start] `Project()` on %d cores at %f sec\n",
-          n_cores, MPI_Wtime() - time_begin);
-    }
-    part.ForEachLocalCell([&](Cell *cell_ptr){
-      cell_ptr->Project(initial_condition);
-    });
-
-    if (i_core == 0) {
-      std::printf("[Start] `Reconstruct()` on %d cores at %f sec\n",
-          n_cores, MPI_Wtime() - time_begin);
-    }
-    if (kOrder > 0) {
-      part.Reconstruct(limiter);
-      if (suffix == "tetra") {
-        part.Reconstruct(limiter);
-      }
-    }
-
-    part.GatherSolutions();
-    if (i_core == 0) {
-      std::printf("[Start] `WriteSolutions(Frame0)` on %d cores at %f sec\n",
-          n_cores, MPI_Wtime() - time_begin);
-    }
-    part.WriteSolutions("Frame0");
-    part.WriteSolutionsOnCellCenters("Frame0");
-  } else {
-    if (i_core == 0) {
-      std::printf("[Start] `ReadSolutions(Frame%d)` on %d cores at %f sec\n",
-          i_frame, n_cores, MPI_Wtime() - time_begin);
-    }
-    part.ReadSolutions("Frame" + std::to_string(i_frame));
-    part.ScatterSolutions();
-  }
-
-  /* Choose the time-stepping scheme. */
-  constexpr int kSteps = std::min(3, kOrder + 1);
-  auto rk = RungeKutta<kSteps, Part, Limiter>(dt, limiter);
-
-  /* Set boundary conditions. */
-  auto u_x = u_gamma / cos_30;
-  auto moving_shock = [&](const Coord& xyz, double t){
-    auto x = xyz[0], y = xyz[1];
-    return ((x - (x_gap + u_x * t)) * tan_60 < y) ? value_after : value_before;
-  };
+void MyBC(const std::string &suffix, Solver *solver) {
   if (suffix == "tetra") {
-    rk.SetPrescribedBC("3_S_27", moving_shock);  // Top
-    rk.SetPrescribedBC("3_S_31", moving_shock);  // Left
-    rk.SetSolidWallBC("3_S_1");   // Back
-    rk.SetSolidWallBC("3_S_32");  // Front
-    rk.SetSolidWallBC("3_S_19");  // Bottom
-    rk.SetFreeOutletBC("3_S_23");  // Right
-    rk.SetFreeOutletBC("3_S_15");  // Gap
+    solver->SetPrescribedBC("3_S_27", moving_shock);  // Top
+    solver->SetPrescribedBC("3_S_31", moving_shock);  // Left
+    solver->SetSolidWallBC("3_S_1");   // Back
+    solver->SetSolidWallBC("3_S_32");  // Front
+    solver->SetSolidWallBC("3_S_19");  // Bottom
+    solver->SetFreeOutletBC("3_S_23");  // Right
+    solver->SetFreeOutletBC("3_S_15");  // Gap
   } else {
     assert(suffix == "hexa");
-    rk.SetPrescribedBC("4_S_27", moving_shock);  // Top
-    rk.SetPrescribedBC("4_S_31", moving_shock);  // Left
-    rk.SetSolidWallBC("4_S_1");   // Back
-    rk.SetSolidWallBC("4_S_32");  // Front
-    rk.SetSolidWallBC("4_S_19");  // Bottom
-    rk.SetFreeOutletBC("4_S_23");  // Right
-    rk.SetFreeOutletBC("4_S_15");  // Gap
+    solver->SetPrescribedBC("4_S_27", moving_shock);  // Top
+    solver->SetPrescribedBC("4_S_31", moving_shock);  // Left
+    solver->SetSolidWallBC("4_S_1");   // Back
+    solver->SetSolidWallBC("4_S_32");  // Front
+    solver->SetSolidWallBC("4_S_19");  // Bottom
+    solver->SetFreeOutletBC("4_S_23");  // Right
+    solver->SetFreeOutletBC("4_S_15");  // Gap
   }
+}
 
-  /* Main Loop */
-  auto wtime_start = MPI_Wtime();
-  for (int i_step = 1; i_step <= n_steps; ++i_step) {
-    double t_curr = t_start + dt * (i_step - 1);
-    rk.Update(&part, t_curr);
-
-    auto wtime_curr = MPI_Wtime() - wtime_start;
-    auto wtime_left = wtime_curr * (n_steps - i_step) / (i_step);
-    if (i_core == 0) {
-      std::printf("[Done] `Update(Step%d/%d)` on %d cores at %fs (%fs to go)\n",
-          i_step, n_steps, n_cores, wtime_curr, wtime_left);
-    }
-
-    if (i_step % n_steps_per_frame == 0) {
-      ++i_frame;
-      part.GatherSolutions();
-      if (i_core == 0) {
-        std::printf("[Start] `WriteSolutions(Frame%d)` on %d cores at %f sec\n",
-            i_frame, n_cores, MPI_Wtime() - wtime_start);
-      }
-      auto frame_name = "Frame" + std::to_string(i_frame);
-      part.WriteSolutions(frame_name);
-      part.WriteSolutionsOnCellCenters(frame_name);
-    }
-  }
-
-  if (i_core == 0) {
-    std::printf("time-range = [%f, %f], frame-range = [%d, %d], dt = %f\n",
-        t_start, t_stop, i_frame - n_frames, i_frame, dt);
-    std::printf("[Start] MPI_Finalize() on %d cores at %f sec\n",
-        n_cores, MPI_Wtime() - time_begin);
-  }
-  MPI_Finalize();
+int main(int argc, char* argv[]) {
+  return Main(argc, argv, MyIC, MyBC);
 }
--- demo/euler/yf17.cpp
+++ demo/euler/yf17.cpp
@@ -1,191 +1,35 @@
-//  Copyright 2021 PEI Weicheng and JIANG Yuyan
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
+#include "main.hpp"
 
-#include "mpi.h"
-#include "pcgnslib.h"
+/* Set initial conditions. */
+auto primitive = Primitive(1.4, 2.0, 0.0, 0.0, 1.0);
+Value given_value = Gas::PrimitiveToConservative(primitive);
 
-#include "mini/mesh/shuffler.hpp"
-#include "mini/riemann/euler/types.hpp"
-#include "mini/riemann/euler/eigen.hpp"
-#include "mini/riemann/euler/exact.hpp"
-#include "mini/riemann/rotated/euler.hpp"
-#include "mini/polynomial/limiter.hpp"
-#include "mini/stepping/explicit.hpp"
-#include "rkdg.hpp"
+Value MyIC(const Coord &xyz) {
+  return given_value;
+}
+
+/* Set boundary conditions. */
+auto given_state = [](const Coord& xyz, double t){
+  return given_value;
+};
+
+void MyBC(const std::string &suffix, Solver *solver) {
+  solver->SetPrescribedBC("upstream", given_state);
+  solver->SetFreeOutletBC("downstream");
+  solver->SetSolidWallBC("intake");
+  solver->SetSolidWallBC("exhaust");
+  solver->SetSolidWallBC("intake ramp");
+  solver->SetSolidWallBC("lower");
+  solver->SetSolidWallBC("upper");
+  solver->SetSolidWallBC("strake");
+  solver->SetSolidWallBC("vertical tail");
+  solver->SetSolidWallBC("horizontal tail");
+  solver->SetSolidWallBC("side");
+  solver->SetSolidWallBC("wing");
+  solver->SetSolidWallBC("fuselage");
+  solver->SetSolidWallBC("symmetry");
+}
 
 int main(int argc, char* argv[]) {
-  MPI_Init(NULL, NULL);
-  int n_cores, i_core;
-  MPI_Comm_size(MPI_COMM_WORLD, &n_cores);
-  MPI_Comm_rank(MPI_COMM_WORLD, &i_core);
-  cgp_mpi_comm(MPI_COMM_WORLD);
-
-  if (argc < 6) {
-    if (i_core == 0) {
-      std::cout << "usage:\n"
-          << "  mpirun -n <n_cores> " << argv[0] << " <cgns_file> <hexa|tetra>"
-          << " <t_start> <t_stop> <n_steps_per_frame> <n_frames>"
-          << " [<i_frame_start> [n_parts_prev]]\n";
-    }
-    MPI_Finalize();
-    exit(0);
-  }
-  auto old_file_name = std::string(argv[1]);
-  auto suffix = std::string(argv[2]);
-  double t_start = std::atof(argv[3]);
-  double t_stop = std::atof(argv[4]);
-  int n_steps_per_frame = std::atoi(argv[5]);
-  int n_frames = std::atoi(argv[6]);
-  int n_steps = n_frames * n_steps_per_frame;
-  auto dt = (t_stop - t_start) / n_steps;
-  int i_frame = 0;
-  if (argc > 7) {
-    i_frame = std::atoi(argv[7]);
-  }
-  int n_parts_prev = 0;
-  if (argc > 8) {
-    n_parts_prev = std::atoi(argv[8]);
-  }
-
-  std::string case_name = "yf17_tetra";
-
-  auto time_begin = MPI_Wtime();
-
-  /* Define the Euler system. */
-  constexpr int kFunc = 5;
-  constexpr int kDim = 3;
-  using Primitive = mini::riemann::euler::PrimitiveTuple<double, kDim>;
-  using Conservative = mini::riemann::euler::ConservativeTuple<double, kDim>;
-  using Gas = mini::riemann::euler::IdealGas<double, 1, 4>;
-  using Unrotated = mini::riemann::euler::Exact<Gas, kDim>;
-  using Riemann = mini::riemann::rotated::Euler<Unrotated>;
-
-  /* Partition the mesh. */
-  if (i_core == 0 && n_parts_prev != n_cores) {
-    using Shuffler = mini::mesh::Shuffler<idx_t, double>;
-    Shuffler::PartitionAndShuffle(case_name, old_file_name, n_cores);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  constexpr int kOrder = 2;
-  using Part = mini::mesh::cgns::Part<cgsize_t, kOrder, Riemann>;
-  using Cell = typename Part::Cell;
-  using Face = typename Part::Face;
-  using Coord = typename Cell::Coord;
-  using Value = typename Cell::Value;
-  using Coeff = typename Cell::Coeff;
-
-  if (i_core == 0) {
-    std::printf("Create %d `Part`s at %f sec\n",
-        n_cores, MPI_Wtime() - time_begin);
-  }
-  auto part = Part(case_name, i_core);
-  part.SetFieldNames({"Density", "MomentumX", "MomentumY", "MomentumZ",
-      "EnergyStagnationDensity"});
-
-  /* Build a `Limiter` object. */
-  // using Limiter = mini::polynomial::LazyWeno<Cell>;
-  using Limiter = mini::polynomial::EigenWeno<Cell>;
-  auto limiter = Limiter(/* w0 = */0.001, /* eps = */1e-6);
-
-  /* Set initial conditions. */
-  auto primitive = Primitive(1.4, 2.0, 0.0, 0.0, 1.0);
-  Value given_value = Gas::PrimitiveToConservative(primitive);
-  auto initial_condition = [&given_value](const Coord& xyz){
-    return given_value;
-  };
-
-  if (argc == 7) {
-    if (i_core == 0) {
-      std::printf("[Start] `Project()` on %d cores at %f sec\n",
-          n_cores, MPI_Wtime() - time_begin);
-    }
-    part.ForEachLocalCell([&](Cell *cell_ptr){
-      cell_ptr->Project(initial_condition);
-    });
-
-    if (i_core == 0) {
-      std::printf("[Start] `Reconstruct()` on %d cores at %f sec\n",
-          n_cores, MPI_Wtime() - time_begin);
-    }
-    if (kOrder > 0) {
-      part.Reconstruct(limiter);
-      part.Reconstruct(limiter);
-    }
-
-    part.GatherSolutions();
-    if (i_core == 0) {
-      std::printf("[Start] `WriteSolutions(Frame0)` on %d cores at %f sec\n",
-          n_cores, MPI_Wtime() - time_begin);
-    }
-    part.WriteSolutions("Frame0");
-    part.WriteSolutionsOnCellCenters("Frame0");
-  } else {
-    if (i_core == 0) {
-      std::printf("[Start] `ReadSolutions(Frame%d)` on %d cores at %f sec\n",
-          i_frame, n_cores, MPI_Wtime() - time_begin);
-    }
-    part.ReadSolutions("Frame" + std::to_string(i_frame));
-    part.ScatterSolutions();
-  }
-
-  /* Choose the time-stepping scheme. */
-  constexpr int kSteps = std::min(3, kOrder + 1);
-  auto rk = RungeKutta<kSteps, Part, Limiter>(dt, limiter);
-
-  /* Set boundary conditions. */
-  auto given_state = [&given_value](const Coord& xyz, double t){
-    return given_value;
-  };
-  rk.SetPrescribedBC("upstream", given_state);
-  rk.SetFreeOutletBC("downstream");
-  rk.SetSolidWallBC("intake");
-  rk.SetSolidWallBC("exhaust");
-  rk.SetSolidWallBC("intake ramp");
-  rk.SetSolidWallBC("lower");
-  rk.SetSolidWallBC("upper");
-  rk.SetSolidWallBC("strake");
-  rk.SetSolidWallBC("vertical tail");
-  rk.SetSolidWallBC("horizontal tail");
-  rk.SetSolidWallBC("side");
-  rk.SetSolidWallBC("wing");
-  rk.SetSolidWallBC("fuselage");
-  rk.SetSolidWallBC("symmetry");
-
-  /* Main Loop */
-  auto wtime_start = MPI_Wtime();
-  for (int i_step = 1; i_step <= n_steps; ++i_step) {
-    double t_curr = t_start + dt * (i_step - 1);
-    rk.Update(&part, t_curr);
-
-    auto wtime_curr = MPI_Wtime() - wtime_start;
-    auto wtime_left = wtime_curr * (n_steps - i_step) / (i_step);
-    if (i_core == 0) {
-      std::printf("[Done] `Update(Step%d/%d)` on %d cores at %fs (%fs to go)\n",
-          i_step, n_steps, n_cores, wtime_curr, wtime_left);
-    }
-
-    if (i_step % n_steps_per_frame == 0) {
-      ++i_frame;
-      part.GatherSolutions();
-      if (i_core == 0) {
-        std::printf("[Start] `WriteSolutions(Frame%d)` on %d cores at %f sec\n",
-            i_frame, n_cores, MPI_Wtime() - wtime_start);
-      }
-      auto frame_name = "Frame" + std::to_string(i_frame);
-      part.WriteSolutions(frame_name);
-      part.WriteSolutionsOnCellCenters(frame_name);
-    }
-  }
-
-  if (i_core == 0) {
-    std::printf("time-range = [%f, %f], frame-range = [%d, %d], dt = %f\n",
-        t_start, t_stop, i_frame - n_frames, i_frame, dt);
-    std::printf("[Start] MPI_Finalize() on %d cores at %f sec\n",
-        n_cores, MPI_Wtime() - time_begin);
-  }
-  MPI_Finalize();
+  return Main(argc, argv, MyIC, MyBC);
 }
