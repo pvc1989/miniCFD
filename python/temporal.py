da10efe06b03ce48d33a761a651928697f25c4c9
Weicheng Pei <weicheng.pei@icloud.com>
2022-12-14 14:42:19 +0800

    Implement the explicit Euler and use it in RK.

--- python/temporal.py
+++ python/temporal.py
@@ -10,11 +10,26 @@ class TemporalScheme(abc.ABC):
     pass
 
 
+class ExplicitEuler(TemporalScheme):
+
+  def __init__(self):
+    pass
+
+
+  def update(self, semi_discrete_system, delta_t):
+    u_curr = semi_discrete_system.get_unknown()
+    residual = semi_discrete_system.get_redisual()
+    u_next = u_curr
+    u_next += residual * delta_t
+    semi_discrete_system.set_unknown(u_next)
+
+
 class SspRungeKutta(TemporalScheme):
 
   def __init__(self, order):
     assert isinstance(order, int) and 1 <= order <= 3
     self._order = order
+    self._euler = ExplicitEuler()
 
 
   def update(self, semi_discrete_system, delta_t):
@@ -29,38 +44,39 @@ class SspRungeKutta(TemporalScheme):
 
 
   def _rk1_update(self, semi_discrete_system, delta_t):
-    u_curr = semi_discrete_system.get_unknown()
-    residual = semi_discrete_system.get_redisual(u_curr)
-    u_next = u_curr
-    u_next += residual * delta_t
-    semi_discrete_system.set_unknown(u_next)
+    self._euler.update(semi_discrete_system, delta_t)
 
 
   def _rk2_update(self, semi_discrete_system, delta_t):
     u_curr = semi_discrete_system.get_unknown()  # u_curr == U_{n}
-    residual = semi_discrete_system.get_redisual(u_curr)
-    u_next = u_curr + residual * delta_t  # u_next == U_{n + 1/2}
-    residual = semi_discrete_system.get_redisual(u_next)
-    u_next += residual * delta_t  # u_next == U_{n + 1/2} + R_{n + 1/2} * dt
-    u_next += u_curr  # u_next == U_{n} + (U_{n + 1/2} + R_{n + 1/2} * dt)
+    self._euler.update(semi_discrete_system, delta_t)
+    # Now, semi_discrete_system holds U_{n + 1/2}
+    self._euler.update(semi_discrete_system, delta_t)
+    u_next = semi_discrete_system.get_unknown()
+    # Now, u_next == U_{n + 1/2} + R_{n + 1/2} * delta_t
+    u_next += u_curr
     u_next /= 2
+    # Now, u_next == U_{n + 1}
     semi_discrete_system.set_unknown(u_next)
 
 
   def _rk3_update(self, semi_discrete_system, delta_t):
     u_curr = semi_discrete_system.get_unknown()  # u_curr == U_{n}
-    residual = semi_discrete_system.get_redisual(u_curr)
-    u_next = u_curr + residual * delta_t
-    # Now, u_next == U_{n + 1/3}.
-    residual = semi_discrete_system.get_redisual(u_next)
-    u_next += residual * delta_t  # u_next == U_{n + 1/3} + R_{n + 1/3} * dt
-    u_next *= 3/4
-    u_next += u_curr / 4  # u_next == (3/4)U_{n} + (1/4)(U_{n + 1/3} + R_{n + 1/3} * dt)
+    self._euler.update(semi_discrete_system, delta_t)
+    # Now, semi_discrete_system holds U_{n + 1/3}
+    self._euler.update(semi_discrete_system, delta_t)
+    u_next = semi_discrete_system.get_unknown()
+    # Now, u_next == U_{n + 1/3} + R_{n + 1/3} * dt
+    u_next += u_curr * 3
+    u_next /= 4
     # Now, u_next == U_{n + 2/3}.
-    residual = semi_discrete_system.get_redisual(u_next)
-    u_next += residual * delta_t  # u_next == U_{n + 2/3} + R_{n + 2/3} * dt
-    u_next *= 2/3
-    u_next += u_curr / 3
+    semi_discrete_system.set_unknown(u_next)
+    self._euler.update(semi_discrete_system, delta_t)
+    u_next = semi_discrete_system.get_unknown()
+    # Now, u_next == U_{n + 2/3} + R_{n + 2/3} * dt
+    u_next *= 2
+    u_next += u_curr
+    u_next /= 3
     # Now, u_next == U_{n + 1}.
     semi_discrete_system.set_unknown(u_next)
 
