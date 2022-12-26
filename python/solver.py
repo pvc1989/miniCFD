21f8ff1b070f864f8dffffeeea6f8fe9f1256966
Weicheng Pei <weicheng.pei@icloud.com>
2022-12-26 21:37:56 +0800

    Test high-order schemes.

--- python/solver.py
+++ python/solver.py
@@ -11,12 +11,12 @@ class RungeKuttaFluxReconstruction:
 
     def __init__(self) -> None:
         self._n_element = 5
-        self._a_const = np.pi
+        self._a_const = 10.0
         self._spatial = spatial.FluxReconstruction(
             equation.LinearAdvection(self._a_const),
             riemann.LinearAdvection(self._a_const),
-            degree=0, n_element=self._n_element,
-            x_min=0.0, x_max=np.pi*2)
+            degree=2, n_element=self._n_element,
+            x_min=0.0, x_max=10.0)
         self._temporal = temporal.SspRungeKutta(3)
         self._delta_x = self._spatial.length() / self._n_element
 
@@ -34,6 +34,9 @@ class RungeKuttaFluxReconstruction:
         plt.ylim([-1.1, 1.1])
         plt.title('t = {0:.2f}'.format(t_curr))
         plt.legend()
+        plt.xticks(np.linspace(self._spatial.x_left(), self._spatial.x_right(),
+            self._n_element + 1))
+        plt.grid()
         plt.show()
 
     def run(self):
@@ -45,10 +48,11 @@ class RungeKuttaFluxReconstruction:
         n_step = int((t_stop - t_start) / delta_t) + 1
         delta_t = (t_stop - t_start) / n_step
         print(f"delta_t == {delta_t}")
-        function = np.sin
-        self._spatial.initialize(function)
+        def u_init(x_global):
+            return np.sin(x_global * np.pi * 2 / self._spatial.length())
+        self._spatial.initialize(u_init)
         plt.figure()
-        self.plot(exact=function, t_curr=0.0, n_point=101)
+        self.plot(exact=u_init, t_curr=0.0, n_point=101)
         plot_steps = range(0, n_step+1, n_step//4)
         for i_step in range(1, n_step + 1):
             t_curr = t_start + i_step * delta_t
@@ -56,9 +60,9 @@ class RungeKuttaFluxReconstruction:
             if i_step not in plot_steps:
                 continue
             print(f'step {i_step}, t = {t_curr}')
-            def exact(point):
-                return function(point - self._a_const * t_curr)
-            self.plot(exact, t_curr, 101)
+            def u_exact(point):
+                return u_init(point - self._a_const * t_curr)
+            self.plot(u_exact, t_curr, 101)
 
 
 if __name__ == '__main__':
