Fix Jacobian of Lagrange.

python/element.py
@@ -15,9 +15,12 @@ class FluxReconstruction(Element):
         self._equation = equation
         self._degree = degree
         self._boundaries = (x_min, x_max)
-        self._solution_points = np.linspace(x_min, x_max, degree + 1)
+        # Sample points strictly inside the element to avoid continuous at boundaries.
+        delta = (self.x_right() - self.x_left()) / 10
+        self._solution_points = np.linspace(x_min + delta, x_max - delta, degree + 1)
         self._solution_lagrange = Lagrange(self._solution_points)
         self._flux_lagrange = Lagrange(self._solution_points)
+        self._plot_lagrange = Lagrange(np.linspace(x_min, x_max, degree + 1))
         self._radau = Radau(degree + 1)
 
     def x_left(self):
@@ -28,19 +31,10 @@ class FluxReconstruction(Element):
 
     def approximate(self, function):
         n_point = self._degree + 1
-        # Sample points strictly inside the element to avoid continuous at boundaries.
-        delta = (self.x_right() - self.x_left()) / 10
-        lagrange = Lagrange(np.linspace(self.x_left() + delta,
-            self.x_right() - delta, n_point))
-        lagrange.approximate(function)
-        solution_values = np.zeros(n_point)
-        flux_values = np.zeros(n_point)
+        coeff = np.ndarray(n_point)
         for i in range(n_point):
-            solution_values[i] = lagrange.get_function_value(
-                self._solution_points[i])
-            flux_values[i] = self._equation.F(solution_values[i])
-        self._solution_lagrange.set_coeff(solution_values)
-        self._flux_lagrange.set_coeff(flux_values)
+            coeff[i] = function(self._solution_points[i])
+        self.set_solution_coeff(coeff)
 
     def set_solution_coeff(self, coeff):
         self._solution_lagrange.set_coeff(coeff)
@@ -67,7 +61,7 @@ class FluxReconstruction(Element):
         """Get the value of the reconstructed continuous flux at a given point.
         """
         flux = self.get_discontinuous_flux(x_global)
-        x_local = self._solution_lagrange.global_to_local(x_global)
+        x_local = self._plot_lagrange.global_to_local(x_global)
         radau_left, radau_right = self._radau.get_function_value(x_local)
         flux += radau_right * (upwind_flux_left
             - self.get_discontinuous_flux(self.x_left()))
@@ -79,10 +73,10 @@ class FluxReconstruction(Element):
         """Get the gradient value of the reconstructed continuous flux at a given point.
         """
         gradient = self._flux_lagrange.get_gradient_value(x_global)
-        x_local = self._solution_lagrange.global_to_local(x_global)
+        x_local = self._plot_lagrange.global_to_local(x_global)
         radau_left, radau_right = self._radau.get_gradient_value(x_local)
-        radau_left /= self._solution_lagrange.jacobian(x_global)
-        radau_right /= self._solution_lagrange.jacobian(x_global)
+        radau_left /= self._plot_lagrange.jacobian(x_global)
+        radau_right /= self._plot_lagrange.jacobian(x_global)
         gradient += radau_right * (upwind_flux_left
             - self.get_discontinuous_flux(self.x_left()))
         gradient += radau_left * (upwind_flux_right