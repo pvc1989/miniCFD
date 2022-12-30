b5f2ca0afce5d59db0e1f489dad52d926a7b81ab
Weicheng Pei <weicheng.pei@icloud.com>
2022-12-30 21:45:34 +0800

    Avoid using misleading variable names.

--- python/temporal_test.py
+++ python/temporal_test.py
@@ -40,14 +40,14 @@ class TestExplicitEuler(unittest.TestCase):
         """Test the update() method.
         """
         n_row = 10
-        df_du = np.random.rand(n_row, n_row)
-        ode_system = Constant(df_du)
+        a_const = np.random.rand(n_row, n_row)
+        ode_system = Constant(a_const)
         u_curr = np.random.rand(n_row)
         ode_system.set_solution_column(u_curr)
         scheme = ExplicitEuler()
         delta_t = 0.01
         scheme.update(ode_system, delta_t)
-        u_expect = u_curr + df_du.dot(u_curr) * delta_t
+        u_expect = u_curr + a_const.dot(u_curr) * delta_t
         u_actual = ode_system.get_solution_column()
         self.assertFalse(np.linalg.norm(u_expect - u_actual))
 
@@ -56,57 +56,57 @@ class TestSspRungeKutta(unittest.TestCase):
     """Test the SspRungeKutta class.
     """
 
-    def euler(self, solution, df_du, delta_t):
+    def euler(self, solution, a_const, delta_t):
         """Wrap the computation of the explicit Euler method.
         """
-        return solution + df_du.dot(solution) * delta_t
+        return solution + a_const.dot(solution) * delta_t
 
     def test_rk1_update(self):
         """Test RK1's update() method.
         """
         n_row = 10
-        df_du = np.random.rand(n_row, n_row)
-        ode_system = Constant(df_du)
+        a_const = np.random.rand(n_row, n_row)
+        ode_system = Constant(a_const)
         u_curr = np.random.rand(n_row)
         ode_system.set_solution_column(u_curr)
         scheme = SspRungeKutta(order=1)
         delta_t = 0.01
         scheme.update(ode_system, delta_t)
         u_actual = ode_system.get_solution_column()
-        u_expect = self.euler(u_curr, df_du, delta_t)
+        u_expect = self.euler(u_curr, a_const, delta_t)
         self.assertFalse(np.linalg.norm(u_expect - u_actual))
 
     def test_rk2_update(self):
         """Test RK2's update() method.
         """
         n_row = 10
-        df_du = np.random.rand(n_row, n_row)
-        ode_system = Constant(df_du)
+        a_const = np.random.rand(n_row, n_row)
+        ode_system = Constant(a_const)
         u_curr = np.random.rand(n_row)
         ode_system.set_solution_column(u_curr)
         scheme = SspRungeKutta(order=2)
         delta_t = 0.01
         scheme.update(ode_system, delta_t)
         u_actual = ode_system.get_solution_column()
-        u_frac12 = self.euler(u_curr, df_du, delta_t)
-        u_expect = (u_curr + self.euler(u_frac12, df_du, delta_t)) / 2
+        u_frac12 = self.euler(u_curr, a_const, delta_t)
+        u_expect = (u_curr + self.euler(u_frac12, a_const, delta_t)) / 2
         self.assertFalse(np.linalg.norm(u_expect - u_actual))
 
     def test_rk3_update(self):
         """Test RK3's update() method.
         """
         n_row = 10
-        df_du = np.random.rand(n_row, n_row)
-        ode_system = Constant(df_du)
+        a_const = np.random.rand(n_row, n_row)
+        ode_system = Constant(a_const)
         u_curr = np.random.rand(n_row)
         ode_system.set_solution_column(u_curr)
         scheme = SspRungeKutta(order=3)
         delta_t = 0.01
         scheme.update(ode_system, delta_t)
         u_actual = ode_system.get_solution_column()
-        u_frac13 = self.euler(u_curr, df_du, delta_t)
-        u_frac23 = (u_curr * 3 + self.euler(u_frac13, df_du, delta_t)) / 4
-        u_expect = (u_curr + self.euler(u_frac23, df_du, delta_t) * 2) / 3
+        u_frac13 = self.euler(u_curr, a_const, delta_t)
+        u_frac23 = (u_curr * 3 + self.euler(u_frac13, a_const, delta_t)) / 4
+        u_expect = (u_curr + self.euler(u_frac23, a_const, delta_t) * 2) / 3
         self.assertFalse(np.linalg.norm(u_expect - u_actual))
 
 
