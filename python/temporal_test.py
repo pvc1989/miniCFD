b7ae35d38e150483f949524f482a5f624c28649f
Weicheng Pei <weicheng.pei@icloud.com>
2023-01-16 20:15:25 +0800

    Add visual test for RK schemes.

--- python/temporal_test.py
+++ python/temporal_test.py
@@ -2,6 +2,7 @@
 """
 import unittest
 import numpy as np
+from matplotlib import pyplot as plt
 
 from concept import OdeSystem
 from temporal import ExplicitEuler, RungeKutta
@@ -27,6 +28,36 @@ class Constant(OdeSystem):
     def get_residual_column(self):
         residual = self._a.dot(self._u)
         return residual
+
+
+class Pendulum(OdeSystem):
+    """A simple ODE system obtained from u'' + u = 0.
+    """
+
+    def __init__(self, u_init, v_init):
+        self._u_init = u_init
+        self._v_init = v_init
+        self._solution = np.array((u_init, v_init))
+
+    def reset(self):
+        self._solution = np.array((self._u_init, self._v_init))
+
+    def get_exact_solution(self, time):
+        u = self._u_init * np.cos(time)
+        v = self._v_init * np.sin(time)
+        return u + v
+
+    def set_solution_column(self, column):
+        self._solution[0] = column[0]
+        self._solution[1] = column[1]
+
+    def get_solution_column(self):
+        return self._solution.copy()
+
+    def get_residual_column(self):
+        u = self._solution[0]
+        v = self._solution[1]
+        return np.array((v, -u))
 
 
 class TestExplicitEuler(unittest.TestCase):
@@ -105,6 +136,29 @@ class TestRungeKutta(unittest.TestCase):
         u_frac23 = (u_curr * 3 + self.euler(u_frac13, a_const, delta_t)) / 4
         u_expect = (u_curr + self.euler(u_frac23, a_const, delta_t) * 2) / 3
         self.assertFalse(np.linalg.norm(u_expect - u_actual))
+
+    def test_plot(self):
+        ode_system = Pendulum(u_init=-1.0, v_init=1.0)
+        t_start = 0.0
+        t_stop = 10 * np.pi
+        n_step = 1001
+        time_values = np.linspace(t_start, t_stop, n_step)
+        delta_t = time_values[1] - time_values[0]
+        exact_values = ode_system.get_exact_solution(time_values)
+        markers = ('.', '1', '2', '3', '4')
+        for order in (1, 2, 3, 4):
+            ode_system.reset()
+            scheme = RungeKutta(order)
+            rk_values = np.ndarray(n_step)
+            rk_values[0] = ode_system.get_solution_column()[0]
+            for i_step in range(1, n_step):
+                scheme.update(ode_system, delta_t)
+                rk_values[i_step] = ode_system.get_solution_column()[0]
+            plt.plot(time_values, rk_values, markers[order], label=f'RK{order}')
+        plt.plot(time_values, exact_values, label='Exact')
+        plt.legend()
+        # plt.show()
+        plt.savefig('rk_compare.pdf')
 
 
 if __name__ == '__main__':
