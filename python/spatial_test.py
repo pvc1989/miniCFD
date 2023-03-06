9a87a81f85a4f69c09bba027784a3d1fc9579908
Weicheng Pei <weicheng.pei@icloud.com>
2023-03-06 22:47:52 +0800

    Plot ADR for DG/FR/DGFR.

--- python/spatial_test.py
+++ python/spatial_test.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 from spatial import LagrangeFR, LagrangeDG, DGwithLagrangeFR
 import equation
 import riemann
+import temporal
 
 class TestLagrangeFR(unittest.TestCase):
     """Test the element for implement flux reconstruction schemes.
@@ -60,47 +61,65 @@ class PlotModifiedWavenumbers(unittest.TestCase):
 
     def __init__(self, method_name: str = ...) -> None:
         super().__init__(method_name)
+        self._a_const = 1.0
         self._spatial = LagrangeFR(
-            equation.LinearAdvection(1.0),
-            riemann.LinearAdvection(1.0),
-            degree=5, n_element=50,
-            x_left=0.0, x_right=10.0, value_type=complex)
-        self._n_point = self._spatial.n_element() + 1
+            equation.LinearAdvection(self._a_const),
+            riemann.LinearAdvection(self._a_const),
+            degree=2, n_element=10,
+            x_left=0.0, x_right=10.0, value_type=float)
 
     def test_plot(self):
         """.
         """
+        a = self._a_const
+        tau = 0.0001
         l = self._spatial.length() / 2
-        i = 1j
-        n = self._n_point - 1
+        i = 1.0j
+        npe = 5
+        n = self._spatial.n_element() * npe
         w = np.exp(i * 2 * np.pi / n)
-        h = self._spatial.length() / n
-        kappa_h = np.ndarray(self._n_point // 2)
-        kappa_tilde_h = np.ndarray(self._n_point // 2, complex)
-        for k in range(1, 1 + self._n_point // 2):
+        h = self._spatial.length() / self._spatial.n_element()
+        eps = h / 2 / npe
+        points = np.linspace(eps, self._spatial.length()-eps, n)
+        assert len(points) == n
+        kappa_h = np.ndarray(n)
+        kappa_tilde_h = np.ndarray(n, complex)
+        ode_solver = temporal.RungeKutta(3)
+        for k in range(1, 1 + n):
             kappa = k * np.pi / l
             kappa_h[k-1] = kappa * h
             def u_init(x):
                 return np.exp(i * kappa * x)
-            self._spatial.initialize(u_init)
-            gradients = np.ndarray(self._spatial.n_element(), complex)
-            v_k = 0.0
-            for r in range(self._spatial.n_element()):
-                x_r = self._spatial.x_left() + r * h + h * 0.5
-                # finite-difference approximation
-                dx = 0.0001
-                gradients[r] = (self._spatial.get_continuous_flux(x_r+dx)
-                    - self._spatial.get_continuous_flux(x_r-dx)) / (2*dx)
-                v_k += gradients[r] * w**(-r * k)
-            kappa_tilde_h[k-1] = v_k * h / (i * n)
+            # DFT for u_init
+            u_k_0 = 0.0 + 0.0j
+            for j in range(n):
+                x_j = points[j]
+                u_k_0 += u_init(x_j) * w**(-j*k)
+            # solve the real part
+            u_k_tau_re = 0.0
+            self._spatial.initialize(lambda x: u_init(x).real)
+            ode_solver.update(self._spatial, delta_t=tau)
+            for j in range(n):
+                x_j = points[j]
+                u_k_tau_re += self._spatial.get_solution_value(x_j) * w**(-j*k)
+            # solve the imag part
+            u_k_tau_im = 0.0
+            self._spatial.initialize(lambda x: u_init(x).imag)
+            ode_solver.update(self._spatial, delta_t=tau)
+            for j in range(n):
+                x_j = points[j]
+                u_k_tau_im += self._spatial.get_solution_value(x_j) * w**(-j*k)
+            # put together
+            u_k_tau = u_k_tau_re + i * u_k_tau_im
+            kappa_tilde_h[k-1] = i * h / (a * tau) * np.log(u_k_tau / u_k_0)
         plt.subplot(2,1,1)
         plt.plot(kappa_h, kappa_tilde_h.real, 'b+')
         plt.plot([0, np.pi], [0, np.pi], 'r-')
         plt.subplot(2,1,2)
         plt.plot(kappa_h, kappa_tilde_h.imag, 'b+')
         plt.plot([0, np.pi], [0, 0], 'r-')
-        # plt.show()
-        plt.savefig('modified_wave_numbers.pdf')
+        plt.show()
+        # plt.savefig('modified_wave_numbers.pdf')
 
 
 if __name__ == '__main__':
