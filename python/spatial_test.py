a7a78cd6acee3d9326fdc35261e4195f4ee0f52e
Weicheng Pei <weicheng.pei@icloud.com>
2023-03-07 23:50:00 +0800

    Compare the ADR for various FR schemes.

--- python/spatial_test.py
+++ python/spatial_test.py
@@ -4,7 +4,8 @@ import unittest
 import numpy as np
 from matplotlib import pyplot as plt
 
-from spatial import LagrangeFR, LagrangeDG, DGwithLagrangeFR
+import concept
+import spatial
 import equation
 import riemann
 import temporal
@@ -20,7 +21,7 @@ class TestLagrangeFR(unittest.TestCase):
         self._n_element = 3
         self._degree = 1
         a_const = np.pi
-        self._spatial = LagrangeFR(
+        self._spatial = spatial.LagrangeFR(
             equation.LinearAdvection(a_const),
             riemann.LinearAdvection(a_const),
             self._degree, self._n_element, self._x_left, self._x_right)
@@ -61,63 +62,100 @@ class PlotModifiedWavenumbers(unittest.TestCase):
 
     def __init__(self, method_name: str = ...) -> None:
         super().__init__(method_name)
-        self._a_const = 1.0
-        self._spatial = LagrangeFR(
-            equation.LinearAdvection(self._a_const),
-            riemann.LinearAdvection(self._a_const),
-            degree=2, n_element=10,
-            x_left=0.0, x_right=10.0, value_type=float)
+        self._a = 1.0
+        self._equation = equation.LinearAdvection(self._a)
+        self._riemann = riemann.LinearAdvection(self._a)
+        self._tau = 0.0001
+        self._n_element = 20
+        delta_x = 100.0
+        self._x_left = 0.0
+        self._x_right = delta_x * self._n_element
+        n_sample_per_element = 10
+        n_sample = self._n_element * n_sample_per_element
+        self._w = np.exp(1.0j * 2 * np.pi / n_sample)
+        half_sample_gap = (delta_x / n_sample_per_element) / 2
+        self._sample_points = np.linspace(self._x_left+half_sample_gap,
+            self._x_right-half_sample_gap, n_sample)
+
+    def get_kth_fourier_coeff(self, k, u):
+        """Get the kth fourier coefficient of a scalar-valued function u(x).
+        """
+        n_sample = len(self._sample_points)
+        kth_fourier_coeff = 0.0
+        for j in range(n_sample):
+            x_j = self._sample_points[j]
+            u_j = u(x_j)
+            kth_fourier_coeff += u_j * self._w**(-j*k)
+        return kth_fourier_coeff / n_sample
+
+    def get_wavenumbers(self, spatial_scheme: spatial.PiecewiseContinuous):
+        """Get the reduced and modified wavenumbers of a PiecewiseContinuous scheme.
+        """
+        k_max = spatial_scheme.n_element() // 2
+        k_max *= spatial_scheme.get_element(0.0).n_term()
+        reduced_wavenumbers = np.ndarray(k_max)
+        modified_wavenumbers = np.ndarray(k_max, complex)
+        ode_solver = temporal.RungeKutta(3)
+        for k in range(1, 1 + k_max):
+            kappa = k * np.pi / (spatial_scheme.length() / 2)
+            reduced_wavenumbers[k-1] = kappa * spatial_scheme.delta_x()
+            def u_init(x):
+                return np.exp(1.0j * kappa * x)
+            kth_fourier_of_u_init = 0.0j
+            kth_fourier_of_u_tau = 0.0j
+            # solve the real part
+            spatial_scheme.initialize(lambda x: u_init(x).real)
+            kth_fourier_of_u_init += self.get_kth_fourier_coeff(k,
+                lambda x: spatial_scheme.get_solution_value(x))
+            ode_solver.update(spatial_scheme, delta_t=self._tau)
+            kth_fourier_of_u_tau += self.get_kth_fourier_coeff(k,
+                lambda x: spatial_scheme.get_solution_value(x))
+            # solve the imag part
+            spatial_scheme.initialize(lambda x: u_init(x).imag)
+            kth_fourier_of_u_init += 1j * self.get_kth_fourier_coeff(k,
+                lambda x: spatial_scheme.get_solution_value(x))
+            ode_solver.update(spatial_scheme, delta_t=self._tau)
+            kth_fourier_of_u_tau += 1j * self.get_kth_fourier_coeff(k,
+                lambda x: spatial_scheme.get_solution_value(x))
+            # put together
+            modified_wavenumbers[k-1] = (1.0j * spatial_scheme.delta_x()
+                / (self._a * self._tau)
+                * np.log(kth_fourier_of_u_tau / kth_fourier_of_u_init))
+        return reduced_wavenumbers, modified_wavenumbers
 
     def test_plot(self):
-        """.
-        """
-        a = self._a_const
-        tau = 0.0001
-        l = self._spatial.length() / 2
-        i = 1.0j
-        npe = self._spatial.degree() + 1
-        n = self._spatial.n_element() * npe
-        w = np.exp(i * 2 * np.pi / n)
-        h = self._spatial.length() / self._spatial.n_element()
-        eps = h / 2 / npe
-        points = np.linspace(eps, self._spatial.length()-eps, n)
-        assert len(points) == n
-        kappa_h = np.ndarray(n)
-        kappa_tilde_h = np.ndarray(n, complex)
-        ode_solver = temporal.RungeKutta(3)
-        for k in range(1, 1 + n):
-            kappa = k * np.pi / l
-            kappa_h[k-1] = kappa * h
-            def u_init(x):
-                return np.exp(i * kappa * x)
-            # DFT for u_init
-            u_k_0 = 0.0 + 0.0j
-            for j in range(n):
-                x_j = points[j]
-                u_k_0 += u_init(x_j) * w**(-j*k)
-            # solve the real part
-            u_k_tau_re = 0.0
-            self._spatial.initialize(lambda x: u_init(x).real)
-            ode_solver.update(self._spatial, delta_t=tau)
-            for j in range(n):
-                x_j = points[j]
-                u_k_tau_re += self._spatial.get_solution_value(x_j) * w**(-j*k)
-            # solve the imag part
-            u_k_tau_im = 0.0
-            self._spatial.initialize(lambda x: u_init(x).imag)
-            ode_solver.update(self._spatial, delta_t=tau)
-            for j in range(n):
-                x_j = points[j]
-                u_k_tau_im += self._spatial.get_solution_value(x_j) * w**(-j*k)
-            # put together
-            u_k_tau = u_k_tau_re + i * u_k_tau_im
-            kappa_tilde_h[k-1] = i * h / (a * tau) * np.log(u_k_tau / u_k_0)
+        spatials = [
+            spatial.LagrangeFR(self._equation, self._riemann,
+                0, self._n_element, self._x_left, self._x_right),
+            spatial.LagrangeFR(self._equation, self._riemann,
+                1, self._n_element, self._x_left, self._x_right),
+            spatial.LagrangeFR(self._equation, self._riemann,
+                2, self._n_element, self._x_left, self._x_right),
+            spatial.LagrangeFR(self._equation, self._riemann,
+                3, self._n_element, self._x_left, self._x_right),
+        ]
+        markers = ['1', '2', '3', '4']
+        labels = ['FR1', 'FR2', 'FR3', 'FR4']
+        kh_max = 4 * np.pi
         plt.subplot(2,1,1)
-        plt.plot(kappa_h, kappa_tilde_h.real, 'b+')
-        plt.plot([0, np.pi], [0, np.pi], 'r-')
+        plt.plot([0, kh_max, kh_max], [0, kh_max, 0], '--')
+        plt.ylabel(r'$\Re(\Omega)$')
         plt.subplot(2,1,2)
-        plt.plot(kappa_h, kappa_tilde_h.imag, 'b+')
-        plt.plot([0, np.pi], [0, 0], 'r-')
+        plt.ylabel(r'$\Im(\Omega)$')
+        plt.xlabel(r'$\kappa h$')
+        plt.plot([0, kh_max], [0, 0], '--')
+        for i in range(len(labels)):
+            reduced, modified = self.get_wavenumbers(spatials[i])
+            # reduced /= (i + 2)
+            # modified /= (i + 2)
+            plt.subplot(2,1,1)
+            plt.plot(reduced, modified.real, marker=markers[i], label=labels[i])
+            plt.subplot(2,1,2)
+            plt.plot(reduced, modified.imag, marker=markers[i], label=labels[i])
+        plt.subplot(2,1,1)
+        plt.legend()
+        plt.subplot(2,1,2)
+        plt.legend()
         plt.show()
         # plt.savefig('modified_wave_numbers.pdf')
 
