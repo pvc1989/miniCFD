cfe3fb91e97a779872e971c548907b6614d00176
Weicheng Pei <weicheng.pei@icloud.com>
2023-03-09 17:14:01 +0800

    Implement expansion on Legendre basis.

--- python/expansion.py
+++ python/expansion.py
@@ -1,6 +1,7 @@
 """Implement some polynomial approximations for general functions.
 """
 import numpy as np
+from scipy import special, integrate
 
 from concept import Expansion
 import polynomial
@@ -88,6 +89,97 @@ class Lagrange(Expansion):
     def get_gradient_value(self, x_global):
         basis_grad = self.get_basis_gradients(x_global)
         value = basis_grad.dot(self._sample_values)
+        return value
+
+
+class Legendre(Expansion):
+    """Approximate a general function based on Legendre polynomials.
+    """
+
+    def __init__(self, degree: int, x_left: float, x_right: float,
+            value_type=float) -> None:
+        super().__init__()
+        self._n_term = degree + 1
+        # linear coordinate transform:
+        self._x_left = x_left
+        self._jacobian = (x_right - x_left) / 2.0
+        self._mode_coeffs = np.ndarray(self._n_term, value_type)
+        self._mode_weights = np.ndarray(self._n_term, float)
+        for k in range(self._n_term):
+            self._mode_weights[k] = self._jacobian * integrate.fixed_quad(
+                lambda x: (special.eval_legendre(k, x))**2, -1.0, 1.0, n=k+1)[0]
+
+    def get_mode_weight(self, k):
+        """Get the inner-product of the kth basis with itself.
+        """
+        return self._mode_weights[k]
+
+    def n_term(self):
+        return self._n_term
+
+    def degree(self):
+        return self._n_term + 1
+
+    def jacobian(self, x_global: float):
+        """Get the Jacobian value at a given point.
+        """
+        return self._jacobian
+
+    def local_to_global(self, x_local):
+        """Coordinate transform from local to global.
+        """
+        return self._x_left + (self._jacobian * (x_local + 1.0))
+
+    def global_to_local(self, x_global):
+        """Coordinate transform from global to local.
+        """
+        return (x_global - self._x_left) / self._jacobian - 1.0
+
+    def set_coeff(self, coeffs):
+        """Set coefficient for each mode.
+        """
+        assert len(coeffs) == self.n_term()
+        for i in range(len(coeffs)):
+            self._mode_coeffs[i] = coeffs[i]
+
+    def get_coeff(self):
+        return self._mode_coeffs
+
+    def approximate(self, function):
+        for k in range(self.n_term()):
+            def integrand(x_local):
+                value = special.eval_legendre(k, x_local)
+                value *= function(self.local_to_global(x_local))
+                return value
+            self._mode_coeffs[k] = self._jacobian * integrate.fixed_quad(
+                integrand, -1.0, 1.0, n=self.n_term())[0]
+            self._mode_coeffs[k] /= self._mode_weights[k]
+
+    def get_basis_values(self, x_global):
+        x_local = self.global_to_local(x_global)
+        values = np.ndarray(self.n_term())
+        for k in range(self.n_term()):
+            values[k] = special.eval_legendre(k, x_local)
+        return values
+
+    def get_basis_gradients(self, x_global):
+        x_local = self.global_to_local(x_global)
+        values = np.ndarray(self.n_term())
+        values[0] = 0.0
+        for k in range(1, self.n_term()):
+            values[k] = (k * special.eval_legendre(k-1, x_local)
+                + x_local * values[k-1])
+        values /= self._jacobian
+        return values
+
+    def get_function_value(self, x_global):
+        values = self.get_basis_values(x_global)
+        value = values.dot(self._mode_coeffs)
+        return value
+
+    def get_gradient_value(self, x_global):
+        basis_grad = self.get_basis_gradients(x_global)
+        value = basis_grad.dot(self._mode_coeffs)
         return value
 
 
--- python/expansion_test.py
+++ python/expansion_test.py
@@ -2,73 +2,73 @@
 """
 import unittest
 import numpy as np
+from scipy import integrate
 from matplotlib import pyplot as plt
 
-from expansion import Lagrange
+import expansion
 
 
 class TestLagrange(unittest.TestCase):
-    """Test the Lagrange class.
+    """Test the expansion.Lagrange class.
     """
 
     def __init__(self, method_name: str = "") -> None:
         super().__init__(method_name)
-        x_left = 0.0
-        x_right = 10.0
-        points = np.linspace(x_left + 0.5, x_right - 0.5, 5)
-        self._lagrange = Lagrange(points, x_left, x_right)
+        self._x_left = 0.0
+        self._x_right = 10.0
+        points = np.linspace(self._x_left + 0.5, self._x_right - 0.5, 5)
+        self._expansion = expansion.Lagrange(points,
+            self._x_left, self._x_right)
 
     def test_coordinate_transforms(self):
         """Test coordinate transforms.
         """
-        points = np.linspace(0.0, 10.0, 20)
+        points = np.linspace(self._x_left, self._x_right, num=201)
         for x_global in points:
-            x_local = self._lagrange.global_to_local(x_global)
+            x_local = self._expansion.global_to_local(x_global)
             self.assertAlmostEqual(x_global,
-                self._lagrange.local_to_global(x_local))
+                self._expansion.local_to_global(x_local))
 
     def test_plot(self):
         """Plot the curves of a function and its approximations."""
-        x_left = 0.0
-        x_right = np.pi
-        n_point = 201
-        points = np.linspace(x_left, x_right, n_point)
+        points = np.linspace(self._x_left, self._x_right, num=201)
         def my_function(point):
             return np.sin(point)
         exact_values = my_function(points)
+        plt.figure()
         plt.plot(points, exact_values, '^', label='Exact')
-        for degree in range(5):
-            sample_points = np.linspace(x_left, x_right, degree+1)
-            lagrange = Lagrange(sample_points, x_left, x_right)
-            lagrange.approximate(my_function)
-            approx_values = np.ndarray(n_point)
-            for i in range(n_point):
-                approx_values[i] = lagrange.get_function_value(points[i])
-            plt.plot(points, approx_values, label= f'{degree}-degree Lagrange')
+        for degree in range(8):
+            sample_points = np.linspace(self._x_left, self._x_right, degree+1)
+            my_expansion = expansion.Lagrange(sample_points,
+                self._x_left, self._x_right)
+            my_expansion.approximate(my_function)
+            approx_values = np.ndarray(len(points))
+            for i in range(len(points)):
+                approx_values[i] = my_expansion.get_function_value(points[i])
+            plt.plot(points, approx_values, label= f'$p={degree}$')
         plt.legend()
         # plt.show()
-        plt.savefig("lagrange_expansion.pdf")
+        plt.savefig("expansion_on_lagrange.pdf")
 
     def test_values_at_sample_points(self):
         """Test values at sample points.
         """
         my_function = np.sin
-        self._lagrange.approximate(my_function)
-        for point in self._lagrange.get_sample_points():
-            self.assertEqual(my_function(point), self._lagrange.get_function_value(point))
+        self._expansion.approximate(my_function)
+        for point in self._expansion.get_sample_points():
+            self.assertEqual(my_function(point), self._expansion.get_function_value(point))
 
     def test_get_basis_values_and_gradients(self):
         """Test methods for getting values and gradients of basis.
         """
-        n_point = 20
-        points = np.linspace(0.0, 10.0, n_point)
+        points = np.linspace(self._x_left, self._x_right, num=201)
         for point in points:
             # 2nd-order finite difference approximation
             delta = 0.0001
-            values_right = self._lagrange.get_basis_values(point + delta)
-            values_left = self._lagrange.get_basis_values(point - delta)
+            values_right = self._expansion.get_basis_values(point + delta)
+            values_left = self._expansion.get_basis_values(point - delta)
             gradients_approx = (values_right - values_left) / (delta * 2)
-            gradients_actual = self._lagrange.get_basis_gradients(point)
+            gradients_actual = self._expansion.get_basis_gradients(point)
             norm = np.linalg.norm(gradients_actual - gradients_approx)
             self.assertAlmostEqual(norm, 0.0)
 
@@ -79,13 +79,83 @@ class TestLagrange(unittest.TestCase):
             return (point-5.0)**4 + (point-4.0)**3
         def my_function_gradient(point):
             return 4 * (point-5.0)**3 + 3 * (point-4.0)**2
-        self._lagrange.approximate(my_function)
+        self._expansion.approximate(my_function)
         n_point = 20
         points = np.linspace(0.0, 10.0, n_point)
         for point in points:
             self.assertAlmostEqual(my_function_gradient(point),
-                self._lagrange.get_gradient_value(point))
+                self._expansion.get_gradient_value(point))
 
+
+class TestLegendre(unittest.TestCase):
+    """Test the Legendre class.
+    """
+
+    def __init__(self, method_name: str = "") -> None:
+        super().__init__(method_name)
+        self._x_left = 0.0
+        self._x_right = 10.0
+        self._expansion = expansion.Legendre(5, self._x_left, self._x_right)
+
+    def test_plot(self):
+        """Plot the curves of a function and its approximations."""
+        points = np.linspace(self._x_left, self._x_right, num=201)
+        def my_function(point):
+            return np.sin(point)
+        exact_values = my_function(points)
+        plt.figure()
+        plt.plot(points, exact_values, '^', label='Exact')
+        for degree in range(8):
+            my_expansion = expansion.Legendre(degree,
+                self._x_left, self._x_right)
+            my_expansion.approximate(my_function)
+            approx_values = np.ndarray(len(points))
+            for i in range(len(points)):
+                approx_values[i] = my_expansion.get_function_value(points[i])
+            plt.plot(points, approx_values, label= f'$p={degree}$')
+        plt.legend()
+        # plt.show()
+        plt.savefig("expansion_on_legendre.pdf")
+
+    def test_get_basis_values_and_gradients(self):
+        """Test methods for getting values and gradients of basis.
+        """
+        points = np.linspace(self._x_left, self._x_right, num=201)
+        for point in points:
+            # 2nd-order finite difference approximation
+            delta = 0.0001
+            values_right = self._expansion.get_basis_values(point + delta)
+            values_left = self._expansion.get_basis_values(point - delta)
+            gradients_approx = (values_right - values_left) / (delta * 2)
+            gradients_actual = self._expansion.get_basis_gradients(point)
+            norm = np.linalg.norm(gradients_actual - gradients_approx)
+            self.assertAlmostEqual(norm, 0.0)
+
+    def test_get_gradient_value(self):
+        """Test the method for getting gradient values.
+        """
+        def my_function(point):
+            return (point-5.0)**4 + (point-4.0)**3
+        def my_function_gradient(point):
+            return 4 * (point-5.0)**3 + 3 * (point-4.0)**2
+        self._expansion.approximate(my_function)
+        points = np.linspace(self._x_left, self._x_right, num=201)
+        for point in points:
+            self.assertAlmostEqual(my_function_gradient(point),
+                self._expansion.get_gradient_value(point))
+
+    def test_orthogonality(self):
+        weight_matrix = np.eye(self._expansion.n_term())
+        for k in range(self._expansion.n_term()):
+            weight_matrix[k][k] = self._expansion.get_mode_weight(k)
+        for k in range(self._expansion.n_term()):
+            for l in range(self._expansion.n_term()):
+                  def integrand(x_global):
+                      values = self._expansion.get_basis_values(x_global)
+                      return values[k] * values[l]
+                  integral, _ = integrate.quad(integrand,
+                      self._x_left, self._x_right)
+                  self.assertAlmostEqual(integral, weight_matrix[k][l])
 
 if __name__ == '__main__':
     unittest.main()
