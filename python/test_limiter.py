cc05bb656e737d5ced87832923899bec210887fb
Weicheng Pei <weicheng.pei@icloud.com>
2023-03-24 15:50:05 +0800

    Add sub-region plot.

--- python/test_limiter.py
+++ python/test_limiter.py
@@ -49,7 +49,12 @@ class TestLimiters(unittest.TestCase):
             limiter.SimpleWENO(),
         ]
         markers = ['1', '2', '3']
-        plt.figure()
+        fig, ax = plt.subplots(figsize=[6, 5])
+        # inset axes....
+        axins = ax.inset_axes([0.7, 0.10, 0.25, 0.15])
+        # sub region of the original image
+        axins.set_xlim(-0.5, 0.5)
+        axins.set_ylim(-3.5, -2)
         points = np.linspace(self._x_left, self._x_right, self._n_element * 10)
         u_approx = np.ndarray(len(points))
         u_exact = np.ndarray(len(points))
@@ -57,6 +62,7 @@ class TestLimiters(unittest.TestCase):
             u_approx[i] = scheme.get_solution_value(points[i])
             u_exact[i] = u_init(points[i])
         plt.plot(points, u_approx, '--', label=r'$p=4$, no limiter')
+        axins.plot(points, u_approx, '--')
         for i in range(len(limiters)):
             indices = detectors[1].get_smoothness_values(scheme) > 1
             limiters[i].reconstruct(scheme, indices)
@@ -64,7 +70,10 @@ class TestLimiters(unittest.TestCase):
                 u_approx[k] = scheme.get_solution_value(points[k])
             plt.plot(points, u_approx, marker=markers[i],
                 label=r'$p=4$, '+f'{limiters[i].name()}')
+            axins.plot(points, u_approx, marker=markers[i])
         plt.plot(points, u_exact, 'r-', label=r'$p=\infty$, i.e. Exact')
+        axins.plot(points, u_exact, 'r-')
+        ax.indicate_inset_zoom(axins, edgecolor="gray")
         plt.xlabel(r'$x$')
         plt.ylabel(r'$u^h$')
         plt.legend()
