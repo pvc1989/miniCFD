faee25c016f8a5dcb982beb4ff03a2f08556d3cb
Weicheng Pei <weicheng.pei@icloud.com>
2023-06-13 19:08:02 +0800

    Improve profiler statistics printing.

--- python/print_stats.py
+++ python/print_stats.py
@@ -1,5 +1,11 @@
-import pstats
-from pstats import SortKey
-p = pstats.Stats('stats')
-p.strip_dirs().sort_stats(SortKey.CUMULATIVE).print_stats(15)
-p.strip_dirs().sort_stats(SortKey.TIME).print_stats(15)
+import sys
+from pstats import Stats, SortKey
+
+if __name__ == '__main__':
+    pass
+    p = Stats(sys.argv[1])
+    n = 10
+    if len(sys.argv) > 2:
+        n = int(sys.argv[2])
+    p.strip_dirs().sort_stats(SortKey.TIME).print_stats(n)
+    p.strip_dirs().sort_stats(SortKey.CUMULATIVE).print_stats(n)
