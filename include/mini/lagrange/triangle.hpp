6b0a0e031cf2842e15cef8d9aed3415cec414714
Weicheng <weicheng.pei@icloud.com>
2023-08-19 17:33:01 +0800

    Implement and test quadratic triangles.

--- include/mini/lagrange/triangle.hpp
+++ include/mini/lagrange/triangle.hpp
@@ -20,6 +20,7 @@ namespace lagrange {
  * @brief Abstract coordinate map on triangular elements.
  * 
  * @tparam Scalar  Type of scalar variables.
+ * @tparam kPhysDim  Dimension of the underlying physical space.
  */
 template <std::floating_point Scalar, int kPhysDim>
 class Triangle : public Face<Scalar, kPhysDim> {
@@ -50,6 +51,7 @@ class Triangle : public Face<Scalar, kPhysDim> {
  * @brief Coordinate map on 3-node triangular elements.
  * 
  * @tparam Scalar  Type of scalar variables.
+ * @tparam kPhysDim  Dimension of the underlying physical space.
  */
 template <std::floating_point Scalar, int kPhysDim>
 class Triangle3 : public Triangle<Scalar, kPhysDim> {
@@ -117,6 +119,108 @@ const std::array<typename Triangle3<Scalar, kPhysDim>::Local, 3>
 Triangle3<Scalar, kPhysDim>::local_coords_{
   Triangle3::Local(1, 0), Triangle3::Local(0, 1),
   Triangle3::Local(0, 0)
+};
+
+/**
+ * @brief Coordinate map on 6-node triangular elements.
+ * 
+ * @tparam Scalar  Type of scalar variables.
+ * @tparam kPhysDim  Dimension of the underlying physical space.
+ */
+template <std::floating_point Scalar, int kPhysDim>
+class Triangle6 : public Triangle<Scalar, kPhysDim> {
+  using Base = Triangle<Scalar, kPhysDim>;
+
+ public:
+  using typename Base::Real;
+  using typename Base::Local;
+  using typename Base::Global;
+  using typename Base::Jacobian;
+
+  static constexpr int kNodes = 6;
+
+ private:
+  std::array<Global, kNodes> global_coords_;
+  static const std::array<Local, kNodes> local_coords_;
+
+ public:
+  int CountNodes() const final {
+    return kNodes;
+  }
+
+ public:
+  std::vector<Scalar> LocalToShapeFunctions(Scalar x_local, Scalar y_local)
+      const final {
+    auto shapes = std::vector<Scalar>(kNodes);
+    auto z_local = 1.0 - x_local - y_local;
+    shapes[0] = x_local * (x_local - 0.5) * 2;
+    shapes[1] = y_local * (y_local - 0.5) * 2;
+    shapes[2] = z_local * (z_local - 0.5) * 2;
+    shapes[3] = x_local * y_local * 4;
+    shapes[4] = y_local * z_local * 4;
+    shapes[5] = z_local * x_local * 4;
+    return shapes;
+  }
+  std::vector<Local> LocalToShapeGradients(Scalar x_local, Scalar y_local)
+      const final {
+    auto grads = std::vector<Local>(kNodes);
+    auto factor_x = 4 * x_local;
+    auto factor_y = 4 * y_local;
+    auto factor_z = 4 - factor_x - factor_y;
+    // shapes[0] = x_local * (x_local - 0.5) * 2;
+    grads[0][X] = factor_x - 1;
+    grads[0][Y] = 0;
+    // shapes[1] = y_local * (y_local - 0.5) * 2;
+    grads[1][X] = 0;
+    grads[1][Y] = factor_y - 1;
+    // shapes[2] = z_local * (z_local - 0.5) * 2;
+    grads[2][X] = grads[2][Y] = 1 - factor_z;
+    // shapes[3] = x_local * y_local * 4;
+    grads[3][X] = factor_y;
+    grads[3][Y] = factor_x;
+    // shapes[4] = y_local * (1 - x_local - y_local) * 4;
+    grads[4][X] = -factor_y;
+    grads[4][Y] = -factor_y + factor_z;
+    // shapes[5] = (1 - x_local - y_local) * x_local * 4;
+    grads[5][X] = -factor_x + factor_z;
+    grads[5][Y] = -factor_x;
+    return grads;
+  }
+
+ public:
+  Global const &GetGlobalCoord(int i) const final {
+    assert(0 <= i && i < CountNodes());
+    return global_coords_[i];
+  }
+  Local const &GetLocalCoord(int i) const final {
+    assert(0 <= i && i < CountNodes());
+    return local_coords_[i];
+  }
+
+ public:
+  Triangle6(
+      Global const &p0, Global const &p1,
+      Global const &p2, Global const &p3,
+      Global const &p4, Global const &p5) {
+    global_coords_[0] = p0; global_coords_[1] = p1;
+    global_coords_[2] = p2; global_coords_[3] = p3;
+    global_coords_[4] = p4; global_coords_[5] = p5;
+    this->_BuildCenter();
+  }
+
+  friend void lagrange::_Build(Triangle6 *,
+      std::initializer_list<Global>);
+  Triangle6(std::initializer_list<Global> il) {
+    lagrange::_Build(this, il);
+  }
+};
+// initialization of static const members:
+template <std::floating_point Scalar, int kPhysDim>
+const std::array<typename Triangle6<Scalar, kPhysDim>::Local, 6>
+Triangle6<Scalar, kPhysDim>::local_coords_{
+  Triangle6::Local(1, 0), Triangle6::Local(0, 1),
+  Triangle6::Local(0, 0), Triangle6::Local(0.5, 0.5),
+  Triangle6::Local(0, 0.5), Triangle6::Local(0.5, 0)
 };
 
 }  // namespace lagrange
--- test/gauss/triangle.cpp
+++ test/gauss/triangle.cpp
@@ -72,6 +72,44 @@ TEST_F(TestGaussTriangle, OnMappedElementInThreeDimensionalSpace) {
     EXPECT_NEAR((pi - nu.cross(sigma)).norm(), 0.0, 1e-15);
   }
 }
+TEST_F(TestGaussTriangle, OnQuadraticElementInThreeDimensionalSpace) {
+  using Gauss = mini::gauss::Triangle<double, 3, 16>;
+  using Lagrange = mini::lagrange::Triangle6<double, 3>;
+  using Local = typename Lagrange::Local;
+  using Global = typename Lagrange::Global;
+  auto lagrange = Lagrange(
+    Global(0, 0, 2), Global(2, 0, 2), Global(2, 2, 2),
+    Global(1, 0, 2), Global(2, 1, 2), Global(1, 1, 2)
+  );
+  auto gauss = Gauss(lagrange);
+  static_assert(gauss.CellDim() == 2);
+  static_assert(gauss.PhysDim() == 3);
+  EXPECT_DOUBLE_EQ(gauss.area(), 2.0);
+  EXPECT_NEAR((gauss.center() - Global(4./3, 2./3, 2.)).norm(), 0, 1e-15);
+  EXPECT_EQ(gauss.LocalToGlobal(1, 0), Global(0, 0, 2));
+  EXPECT_EQ(gauss.LocalToGlobal(0, 1), Global(2, 0, 2));
+  EXPECT_EQ(gauss.LocalToGlobal(0, 0), Global(2, 2, 2));
+  EXPECT_DOUBLE_EQ(
+      Quadrature([](Local const&){ return 2.0; }, gauss), 1.0);
+  EXPECT_DOUBLE_EQ(
+      Integrate([](Global const&){ return 2.0; }, gauss), 4.0);
+  auto f = [](Global const& xyz){ return xyz[0]; };
+  auto g = [](Global const& xyz){ return xyz[1]; };
+  auto h = [](Global const& xyz){ return xyz[0] * xyz[1]; };
+  EXPECT_DOUBLE_EQ(Innerprod(f, g, gauss), Integrate(h, gauss));
+  EXPECT_DOUBLE_EQ(Norm(f, gauss), std::sqrt(Innerprod(f, f, gauss)));
+  EXPECT_DOUBLE_EQ(Norm(g, gauss), std::sqrt(Innerprod(g, g, gauss)));
+  // test normal frames
+  Global normal = Global(0, 0, 1).normalized();
+  for (int q = 0; q < gauss.CountPoints(); ++q) {
+    auto &frame = gauss.GetNormalFrame(q);
+    auto &nu = frame[0], &sigma = frame[1], &pi = frame[2];
+    EXPECT_NEAR((nu - normal).norm(), 0.0, 1e-15);
+    EXPECT_NEAR((nu - sigma.cross(pi)).norm(), 0.0, 1e-15);
+    EXPECT_NEAR((sigma - pi.cross(nu)).norm(), 0.0, 1e-15);
+    EXPECT_NEAR((pi - nu.cross(sigma)).norm(), 0.0, 1e-15);
+  }
+}
 
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
