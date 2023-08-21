f9c2154358cbed2dd07b85abd6f4b3676eeb749d
Weicheng <weicheng.pei@icloud.com>
2023-08-21 15:29:34 +0800

    Implement and test `lagrange::Wedge15`.

--- include/mini/lagrange/wedge.hpp
+++ include/mini/lagrange/wedge.hpp
@@ -77,19 +77,19 @@ class Wedge6 : public Wedge<Scalar> {
       int face_n_node) const final {
     if (face_n_node == 3) {
       int i_face = 0;
-      for (int f = 0; f < 3; ++f) {
+      for (int f = 0; f < face_n_node; ++f) {
         if (cell_nodes[4] == face_nodes[f]) {
           i_face = 1;
           break;
         }
       }
-      for (int i = 0; i < 3; ++i) {
-        face_nodes[i] = cell_nodes[triangles_[i_face][i]];
+      for (int f = 0; f < face_n_node; ++f) {
+        face_nodes[f] = cell_nodes[triangles_[i_face][f]];
       }
     } else {
       assert(face_n_node == 4);
       int c_sum = 0;  // sum of c's in (0, 1, 2)
-      for (int f = 0; f < 4; ++f) {
+      for (int f = 0; f < face_n_node; ++f) {
         for (int c = 0; c < 3; ++c) {
           if (cell_nodes[c] == face_nodes[f]) {
             c_sum += c;
@@ -104,16 +104,15 @@ class Wedge6 : public Wedge<Scalar> {
       case 3: i_face = 1; break;
       default: assert(false);
       }
-      for (int i = 0; i < 4; ++i) {
-        face_nodes[i] = cell_nodes[quadrangles_[i_face][i]];
+      for (int f = 0; f < face_n_node; ++f) {
+        face_nodes[f] = cell_nodes[quadrangles_[i_face][f]];
       }
     }
   }
 
  public:
-  std::vector<Scalar> LocalToShapeFunctions(
-      Scalar a_local, Scalar b_local, Scalar z_local) const final {
-    auto shapes = std::vector<Scalar>(kNodes);
+  static void LocalToShapeFunctions(Scalar a_local, Scalar b_local,
+      Scalar z_local, Scalar *shapes) {
     auto c_local = 1.0 - a_local - b_local;
     auto factor_z = (1 - z_local) / 2;
     shapes[0] = a_local * factor_z;
@@ -123,12 +122,15 @@ class Wedge6 : public Wedge<Scalar> {
     shapes[3] = a_local * factor_z;
     shapes[4] = b_local * factor_z;
     shapes[5] = c_local * factor_z;
+  }
+  std::vector<Scalar> LocalToShapeFunctions(
+      Scalar a_local, Scalar b_local, Scalar z_local) const final {
+    auto shapes = std::vector<Scalar>(kNodes);
+    LocalToShapeFunctions(a_local, b_local, z_local, shapes.data());
     return shapes;
   }
-  std::vector<Local> LocalToShapeGradients(
-      Scalar a_local, Scalar b_local, Scalar z_local) const final {
-    auto grads = std::vector<Local>(kNodes);
-    constexpr int A{0}, B{1};
+  static void LocalToShapeGradients(Scalar a_local, Scalar b_local,
+      Scalar z_local, Local *grads) {
     auto c_local = 1.0 - a_local - b_local;
     auto factor_z = (1 - z_local) / 2;
     // shapes[0] = a_local * factor_z
@@ -156,6 +158,11 @@ class Wedge6 : public Wedge<Scalar> {
     grads[5][A] = -factor_z;
     grads[5][B] = -factor_z;
     grads[5][Z] = +0.5 * c_local;
+  }
+  std::vector<Local> LocalToShapeGradients(
+      Scalar a_local, Scalar b_local, Scalar z_local) const final {
+    auto grads = std::vector<Local>(kNodes);
+    LocalToShapeGradients(a_local, b_local, z_local, grads.data());
     return grads;
   }
 
@@ -173,9 +180,8 @@ class Wedge6 : public Wedge<Scalar> {
   Wedge6(
       Global const &p0, Global const &p1, Global const &p2, Global const &p3,
       Global const &p4, Global const &p5) {
-    global_coords_[0] = p0; global_coords_[1] = p1;
-    global_coords_[2] = p2; global_coords_[3] = p3;
-    global_coords_[4] = p4; global_coords_[5] = p5;
+    global_coords_[0] = p0; global_coords_[1] = p1; global_coords_[2] = p2;
+    global_coords_[3] = p3; global_coords_[4] = p4; global_coords_[5] = p5;
     this->_BuildCenter();
   }
 
@@ -192,7 +198,6 @@ Wedge6<Scalar>::local_coords_{
   Wedge6::Local(1, 0, -1), Wedge6::Local(0, 1, -1), Wedge6::Local(0, 0, -1),
   Wedge6::Local(1, 0, +1), Wedge6::Local(0, 1, +1), Wedge6::Local(0, 0, +1)
 };
-
 /* See http://cgns.github.io/CGNS_docs_current/sids/conv.figs/penta_6.png for node numbering.
 */
 template <std::floating_point Scalar>
@@ -208,6 +213,261 @@ Wedge6<Scalar>::quadrangles_{
   1, 2, 5, 4,
   0, 3, 5, 2,
 };
+
+/**
+ * @brief Coordinate map on 15-node wedge elements.
+ * 
+ * @tparam Scalar  Type of scalar variables.
+ */
+template <std::floating_point Scalar>
+class Wedge15 : public Wedge<Scalar> {
+  using Base = Wedge<Scalar>;
+
+ public:
+  using typename Base::Real;
+  using typename Base::Local;
+  using typename Base::Global;
+  using typename Base::Jacobian;
+
+  static constexpr int kNodes = 15;
+
+ private:
+  std::array<Global, kNodes> global_coords_;
+  static const std::array<Local, kNodes> local_coords_;
+  static const std::array<std::array<int, 6>, 2> triangles_;
+  static const std::array<std::array<int, 8>, 3> quadrangles_;
+
+ public:
+  int CountNodes() const final {
+    return kNodes;
+  }
+  void SortNodesOnFace(const size_t *cell_nodes, size_t *face_nodes,
+      int face_n_node) const final {
+    if (face_n_node == 6) {
+      int i_face = 0;
+      for (int f = 0; f < face_n_node; ++f) {
+        if (cell_nodes[4] == face_nodes[f]) {
+          i_face = 1;
+          break;
+        }
+      }
+      for (int f = 0; f < face_n_node; ++f) {
+        face_nodes[f] = cell_nodes[triangles_[i_face][f]];
+      }
+    } else {
+      assert(face_n_node == 8);
+      int c_sum = 0;  // sum of c's in (0, 1, 2)
+      for (int f = 0; f < face_n_node; ++f) {
+        for (int c = 0; c < 3; ++c) {
+          if (cell_nodes[c] == face_nodes[f]) {
+            c_sum += c;
+            break;
+          }
+        }
+      }
+      int i_face;
+      switch (c_sum) {
+      case 1: i_face = 0; break;
+      case 2: i_face = 2; break;
+      case 3: i_face = 1; break;
+      default: assert(false);
+      }
+      for (int f = 0; f < face_n_node; ++f) {
+        face_nodes[f] = cell_nodes[quadrangles_[i_face][f]];
+      }
+    }
+  }
+
+ private:
+  static void LocalToNewShapeFunctions(Scalar a_local, Scalar b_local,
+      Scalar z_local, Scalar *shapes) {
+    auto c_local = 1.0 - a_local - b_local;
+    auto factor_ab = a_local * b_local * 4;
+    auto factor_bc = b_local * c_local * 4;
+    auto factor_ca = c_local * a_local * 4;
+    // local_coords_[i][Z] = -1
+    auto factor_z = (1 - z_local) / 2;
+    shapes[6] = factor_ab * factor_z;
+    shapes[7] = factor_bc * factor_z;
+    shapes[8] = factor_ca * factor_z;
+    // local_coords_[i][Z] = 0
+    factor_z = 1 - z_local * z_local;
+    shapes[9] = a_local * factor_z;
+    shapes[10] = b_local * factor_z;
+    shapes[11] = c_local * factor_z;
+    // local_coords_[i][Z] = +1
+    factor_z = (1 + z_local) / 2;
+    shapes[12] = factor_ab * factor_z;
+    shapes[13] = factor_bc * factor_z;
+    shapes[14] = factor_ca * factor_z;
+  }
+  static void LocalToNewShapeGradients(Scalar a_local, Scalar b_local,
+      Scalar z_local, Local *grads) {
+    auto c_local = 1.0 - a_local - b_local;
+    auto factor_a = 4 * a_local;
+    auto factor_b = 4 * b_local;
+    auto factor_c = 4 * c_local;
+    auto factor_ab = factor_a * b_local;
+    auto factor_bc = factor_b * c_local;
+    auto factor_ca = factor_c * a_local;
+    // local_coords_[i][Z] = -1
+    auto factor_z = (1 - z_local) / 2;
+    auto grad_z = -0.5;
+    // shape[6] = 4 * a * b * factor_z
+    grads[6][A] = factor_b * factor_z;
+    grads[6][B] = factor_a * factor_z;
+    grads[6][Z] = factor_ab * grad_z;
+    // shape[7] = 4 * b * c * factor_z
+    grads[7][A] = -factor_b * factor_z;
+    grads[7][B] = (factor_c - factor_b) * factor_z;
+    grads[7][Z] = factor_bc * grad_z;
+    // shape[8] = 4 * c * a * factor_z
+    grads[8][A] = (factor_c - factor_a) * factor_z;
+    grads[8][B] = -factor_a * factor_z;
+    grads[8][Z] = factor_ca * grad_z;
+    // local_coords_[i][Z] = 0
+    factor_z = (1 - z_local * z_local);
+    grad_z = -2 * z_local;
+    // shape[9] = a_local * factor_z;
+    grads[9][A] = factor_z;
+    grads[9][B] = 0;
+    grads[9][Z] = a_local * grad_z;
+    // shape[10] = b_local * factor_z;
+    grads[10][A] = 0;
+    grads[10][B] = factor_z;
+    grads[10][Z] = b_local * grad_z;
+    // shape[11] = c_local * factor_z;
+    grads[11][A] = -factor_z;
+    grads[11][B] = -factor_z;
+    grads[11][Z] = c_local * grad_z;
+    // local_coords_[i][Z] = +1
+    factor_z = (1 + z_local) / 2;
+    grad_z = +0.5;
+    // shape[12] = 4 * a * b * factor_z
+    grads[12][A] = factor_b * factor_z;
+    grads[12][B] = factor_a * factor_z;
+    grads[12][Z] = factor_ab * grad_z;
+    // shape[13] = 4 * b * c * factor_z
+    grads[13][A] = -factor_b * factor_z;
+    grads[13][B] = (factor_c - factor_b) * factor_z;
+    grads[13][Z] = factor_bc * grad_z;
+    // shape[14] = 4 * c * a * factor_z
+    grads[14][A] = (factor_c - factor_a) * factor_z;
+    grads[14][B] = -factor_a * factor_z;
+    grads[14][Z] = factor_ca * grad_z;
+  }
+
+ public:
+  static void LocalToShapeFunctions(Scalar a_local, Scalar b_local,
+      Scalar z_local, Scalar *shapes) {
+    Wedge6<Scalar>::LocalToShapeFunctions(a_local, b_local, z_local, shapes);
+    LocalToNewShapeFunctions(a_local, b_local, z_local, shapes);
+    for (int j = 6; j < 15; ++j) {
+      Scalar a_j = local_coords_[j][A];
+      Scalar b_j = local_coords_[j][B];
+      Scalar z_j = local_coords_[j][Z];
+      Scalar old_shapes_on_new_nodes[6];
+      Wedge6<Scalar>::LocalToShapeFunctions(
+          a_j, b_j, z_j, old_shapes_on_new_nodes);
+      for (int i = 0; i < 6; ++i) {
+        shapes[i] -= old_shapes_on_new_nodes[i] * shapes[j];
+      }
+    }
+  }
+  std::vector<Scalar> LocalToShapeFunctions(
+      Scalar a_local, Scalar b_local, Scalar z_local) const final {
+    auto shapes = std::vector<Scalar>(kNodes);
+    LocalToShapeFunctions(a_local, b_local, z_local, shapes.data());
+    return shapes;
+  }
+  static void LocalToShapeGradients(Scalar a_local, Scalar b_local,
+      Scalar z_local, Local *grads) {
+    Wedge6<Scalar>::LocalToShapeGradients(a_local, b_local, z_local, grads);
+    LocalToNewShapeGradients(a_local, b_local, z_local, grads);
+    for (int j = 6; j < 15; ++j) {
+      Scalar a_j = local_coords_[j][A];
+      Scalar b_j = local_coords_[j][B];
+      Scalar z_j = local_coords_[j][Z];
+      Scalar old_shapes_on_new_nodes[6];
+      Wedge6<Scalar>::LocalToShapeFunctions(
+          a_j, b_j, z_j, old_shapes_on_new_nodes);
+      for (int i = 0; i < 6; ++i) {
+        grads[i] -= old_shapes_on_new_nodes[i] * grads[j];
+      }
+    }
+  }
+  std::vector<Local> LocalToShapeGradients(
+      Scalar a_local, Scalar b_local, Scalar z_local) const final {
+    auto grads = std::vector<Local>(kNodes);
+    LocalToShapeGradients(a_local, b_local, z_local, grads.data());
+    return grads;
+  }
+
+ public:
+  Global const &GetGlobalCoord(int i) const final {
+    assert(0 <= i && i < CountNodes());
+    return global_coords_[i];
+  }
+  Local const &GetLocalCoord(int i) const final {
+    assert(0 <= i && i < CountNodes());
+    return local_coords_[i];
+  }
+
+ public:
+  Wedge15(
+      Global const &p0, Global const &p1, Global const &p2, Global const &p3,
+      Global const &p4, Global const &p5, Global const &p6, Global const &p7,
+      Global const &p8, Global const &p9, Global const &p10, Global const &p11,
+      Global const &p12, Global const &p13, Global const &p14) {
+    global_coords_[0] = p0; global_coords_[1] = p1; global_coords_[2] = p2;
+    global_coords_[3] = p3; global_coords_[4] = p4; global_coords_[5] = p5;
+    global_coords_[6] = p6; global_coords_[7] = p7; global_coords_[8] = p8;
+    global_coords_[9] = p9; global_coords_[10] = p10; global_coords_[11] = p11;
+    global_coords_[12] = p12; global_coords_[13] = p13;
+    global_coords_[14] = p14;
+    this->_BuildCenter();
+  }
+
+  friend void lagrange::_Build(Wedge15 *,
+      std::initializer_list<Global>);
+  Wedge15(std::initializer_list<Global> il) {
+    lagrange::_Build(this, il);
+  }
+};
+// initialization of static const members:
+template <std::floating_point Scalar>
+const std::array<typename Wedge15<Scalar>::Local, 15>
+Wedge15<Scalar>::local_coords_{
+  // corner nodes on the bottom face
+  Wedge15::Local(1, 0, -1), Wedge15::Local(0, 1, -1), Wedge15::Local(0, 0, -1),
+  // corner nodes on the top face
+  Wedge15::Local(1, 0, +1), Wedge15::Local(0, 1, +1), Wedge15::Local(0, 0, +1),
+  // mid-edge nodes on the bottom face
+  Wedge15::Local(0.5, 0.5, -1), Wedge15::Local(0, 0.5, -1),
+  Wedge15::Local(0.5, 0, -1),
+  // mid-edge nodes on vertical edges
+  Wedge15::Local(1, 0, 0), Wedge15::Local(0, 1, 0), Wedge15::Local(0, 0, 0),
+  // mid-edge nodes on the top face
+  Wedge15::Local(0.5, 0.5, +1), Wedge15::Local(0, 0.5, +1),
+  Wedge15::Local(0.5, 0, +1),
+};
+
+/* See http://cgns.github.io/CGNS_docs_current/sids/conv.figs/penta_15.png for node numbering.
+*/
+template <std::floating_point Scalar>
+const std::array<std::array<int, 6>, 2>
+Wedge15<Scalar>::triangles_{
+  0, 2, 1, 8, 7, 6,
+  3, 4, 5, 12, 13, 14,
+};
+template <std::floating_point Scalar>
+const std::array<std::array<int, 8>, 3>
+Wedge15<Scalar>::quadrangles_{
+  0, 1, 4, 3, 6, 10, 12, 9,
+  1, 2, 5, 4, 7, 11, 13, 10,
+  0, 3, 5, 2, 9, 14, 11, 8,
+};
+
 
 }  // namespace lagrange
 }  // namespace mini
--- test/gauss/wedge.cpp
+++ test/gauss/wedge.cpp
@@ -10,13 +10,44 @@
 
 class TestGaussWedge : public ::testing::Test {
 };
-TEST_F(TestGaussWedge, OnStandardElement) {
+TEST_F(TestGaussWedge, OnLinearElement) {
   using Gauss = mini::gauss::Wedge<double, 16, 4>;
   using Lagrange = mini::lagrange::Wedge6<double>;
   using Coord = typename Lagrange::Global;
   auto lagrange = Lagrange {
     Coord(-1, -1, -1), Coord(+1, -1, -1), Coord(0, +1, -1),
     Coord(-1, -1, +1), Coord(+1, -1, +1), Coord(0, +1, +1)
+  };
+  auto gauss = Gauss(lagrange);
+  static_assert(gauss.CellDim() == 3);
+  static_assert(gauss.PhysDim() == 3);
+  EXPECT_NEAR(gauss.volume(), 4.0, 1e-14);
+  EXPECT_EQ(gauss.CountPoints(), 64);
+  auto local_weight_sum = 0.0;
+  for (int i = 0; i < gauss.CountPoints(); ++i) {
+    local_weight_sum += gauss.GetLocalWeight(i);
+  }
+  EXPECT_NEAR(Quadrature([](Coord const&){ return 2.0; }, gauss),
+      local_weight_sum * 2, 1e-15);
+  EXPECT_NEAR(Integrate([](Coord const&){ return 2.0; }, gauss),
+      gauss.volume() * 2, 1e-15);
+  auto f = [](Coord const& xyz){ return xyz[0]; };
+  auto g = [](Coord const& xyz){ return xyz[1]; };
+  auto h = [](Coord const& xyz){ return xyz[0] * xyz[1]; };
+  EXPECT_DOUBLE_EQ(Innerprod(f, g, gauss), Integrate(h, gauss));
+  EXPECT_DOUBLE_EQ(Norm(f, gauss), std::sqrt(Innerprod(f, f, gauss)));
+  EXPECT_DOUBLE_EQ(Norm(g, gauss), std::sqrt(Innerprod(g, g, gauss)));
+}
+TEST_F(TestGaussWedge, OnQuadraticElement) {
+  using Gauss = mini::gauss::Wedge<double, 16, 4>;
+  using Lagrange = mini::lagrange::Wedge15<double>;
+  using Coord = typename Lagrange::Global;
+  auto lagrange = Lagrange {
+    Coord(-1, -1, -1), Coord(+1, -1, -1), Coord(0, +1, -1),
+    Coord(-1, -1, +1), Coord(+1, -1, +1), Coord(0, +1, +1),
+    Coord(0, -1, -1), Coord(0.5, 0, -1), Coord(-0.5, 0, -1),
+    Coord(-1, -1, 0), Coord(+1, -1, 0), Coord(0, +1, 0),
+    Coord(0, -1, +1), Coord(0.5, 0, +1), Coord(-0.5, 0, +1),
   };
   auto gauss = Gauss(lagrange);
   static_assert(gauss.CellDim() == 3);
