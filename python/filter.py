276754449121d3b9322a2462bae932c94a2c41a9
Weicheng <weicheng.pei@icloud.com>
2023-10-11 22:23:20 +0800

    Plot the damping function of the filter.

--- python/filter.py
+++ python/filter.py
@@ -1,11 +1,9 @@
 import numpy as np
 
 
-def Selective(old_array: np.ndarray, periodic: bool):
-    new_array = old_array.copy()
-    width = 5
-    sigma = 1.0
-    coeff = np.array([
+class Selective:
+
+    coeff_ = np.array([
         +0.001446093078167,
         -0.012396449873964,
         +0.049303775636020,
@@ -18,23 +16,39 @@ def Selective(old_array: np.ndarray, periodic: bool):
         +0.012396449873964,
         -0.001446093078167,
     ])
-    for i in range(1, 1 + width):
-        assert coeff[width - i] == -coeff[width + i]
-    for i in range(width, len(old_array) - width):
-        d_i = coeff.dot(old_array[i - width : i + width + 1])
-        new_array[i] -= sigma * d_i
-    if periodic:
-        for i in range(width):
-            d_i = old_array[i] * coeff[width]
-            for j in range(1, 1 + width):
-                d_i += old_array[i + j] * coeff[width + j]
-                d_i += old_array[i - j] * coeff[width - j]
-            new_array[i] -= sigma * d_i
-        for i in range(len(old_array) - width, len(old_array)):
-            d_i = old_array[i] * coeff[width]
-            for j in range(1, 1 + width):
-                d_i += old_array[(i + j) % len(old_array)] * coeff[width + j]
-                d_i += old_array[i - j] * coeff[width - j]
-            new_array[i] -= sigma * d_i
-    return new_array
 
+    def __init__(self, periodic) -> None:
+        self._periodic = periodic
+        width = 5
+        for i in range(1, 1 + width):
+            assert self.coeff_[width - i] == -self.coeff_[width + i]
+
+    def filter(self, old_array: np.ndarray, sigma) -> np.ndarray:
+        new_array = old_array.copy()
+        width = 5
+        n_point = len(old_array)
+        for i in range(width, n_point - width):
+            d_i = self.coeff_.dot(old_array[i - width : i + width + 1])
+            new_array[i] -= sigma * d_i
+        if self._periodic:
+            for i in range(width):
+                d_i = old_array[i] * self.coeff_[width]
+                for j in range(1, 1 + width):
+                    d_i += old_array[i + j] * self.coeff_[width + j]
+                    d_i += old_array[i - j] * self.coeff_[width - j]
+                new_array[i] -= sigma * d_i
+            for i in range(n_point - width, n_point):
+                d_i = old_array[i] * self.coeff_[width]
+                for j in range(1, 1 + width):
+                    k = (i + j) % n_point
+                    d_i += old_array[k] * self.coeff_[width + j]
+                    d_i += old_array[i - j] * self.coeff_[width - j]
+                new_array[i] -= sigma * d_i
+        return new_array
+
+    def damping(self, theta):
+        width = 5
+        damp = self.coeff_[width] * np.cos(0 * theta)
+        for j in range(1, 1 + width):
+            damp += 2 * self.coeff_[width + j] * np.cos(j * theta)
+        return damp
--- python/test_filter.py
+++ python/test_filter.py
@@ -46,9 +46,17 @@ if __name__ == '__main__':
     input_file = sys.argv[2]
     output_file = sys.argv[3]
     xdata, old_array = read(scalar_name, input_file)
-    new_array = filter.Selective(old_array, True)
+    f = filter.Selective(True)
+    new_array = f.filter(old_array, 1.0)
     write(xdata, new_array, scalar_name, output_file)
+    fig = plt.figure()
     plt.plot(xdata, old_array, 'r-', label='Old')
     plt.plot(xdata, new_array, 'b-', label='New')
     plt.legend()
     plt.savefig('temp.svg')
+
+    fig = plt.figure()
+    theta = np.linspace(np.pi / 16, np.pi, 201)
+    plt.plot(theta, f.damping(theta))
+    plt.loglog()
+    plt.savefig('damping.svg')
