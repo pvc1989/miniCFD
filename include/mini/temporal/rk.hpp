9daae6ff33b78e623bc1bb10642ffca78d6cd199
Weicheng <weicheng.pei@icloud.com>
2023-11-10 14:46:09 +0800

    Add more Runge-Kutta cases.

--- include/mini/temporal/rk.hpp
+++ include/mini/temporal/rk.hpp
@@ -14,12 +14,60 @@ template <typename Scalar>
 struct RungeKutta<1, Scalar> : public Solver<Scalar> {
  private:
   using Base = Solver<Scalar>;
+  Euler<Scalar> euler_;
 
  public:
   using Column = typename Base::Column;
 
   void Update(System<Scalar> *system, double t_curr, double dt) final {
-    auto u_next = Euler<Scalar>::NextSolution(system, t_curr, dt);
+    euler_.Update(system, t_curr, dt);
+  }
+};
+
+template <typename Scalar>
+struct RungeKutta<2, Scalar> : public Solver<Scalar> {
+ private:
+  using Base = Solver<Scalar>;
+  Euler<Scalar> euler_;
+
+ public:
+  using Column = typename Base::Column;
+
+  void Update(System<Scalar> *system, double t_curr, double dt) final {
+    auto u_curr = system->GetSolutionColumn();
+    euler_.Update(system, t_curr, dt);
+    auto u_next = euler_.NextSolution(system, t_curr + dt, dt);
+    u_next += u_curr;
+    u_next *= 0.5;
+    system->SetSolutionColumn(u_next);
+  }
+};
+
+template <typename Scalar>
+struct RungeKutta<3, Scalar> : public Solver<Scalar> {
+ private:
+  using Base = Solver<Scalar>;
+  Euler<Scalar> euler_;
+
+ public:
+  using Column = typename Base::Column;
+
+  void Update(System<Scalar> *system, double t_curr, double dt) final {
+    auto u_curr = system->GetSolutionColumn();
+    euler_.Update(system, t_curr, dt);
+    // Now, system->GetSolutionColumn() == U_1st == U_old + R_old * dt
+    auto u_next = u_curr;
+    u_next *= 3;
+    u_next += euler_.NextSolution(system, t_curr + dt, dt);
+    u_next /= 4;
+    // Now, u_next == U_2nd == ((U_1st + R_1st * dt) + U_old * 3) / 4
+    system->SetSolutionColumn(u_next);
+    u_next = euler_.NextSolution(system, t_curr + dt / 2, dt);
+    // Now, u_next == U_2nd + R_2nd * dt
+    u_next *= 2;
+    u_next += u_curr;
+    u_next /= 3;
+    // Now, u_next == U_3rd == ((U_2nd + R_2nd * dt) * 2 + U_old) / 3
     system->SetSolutionColumn(u_next);
   }
 };
--- test/temporal/rk.cpp
+++ test/temporal/rk.cpp
@@ -33,6 +33,41 @@ TEST_F(TestTemporalConstant, OneStepRungeKutta) {
   auto u_new = system.GetSolutionColumn();
   EXPECT_EQ(u_new, u_old + a * u_old * dt);
 }
+TEST_F(TestTemporalConstant, TwoStepRungeKutta) {
+  using Solver = mini::temporal::RungeKutta<2, Scalar>;
+  int n = 10;
+  std::srand(31415926);
+  Matrix a = Matrix::Random(n, n);
+  auto system = System(a);
+  Column u_old = Column::Random(n);
+  system.SetSolutionColumn(u_old);
+  auto solver = Solver();
+  auto t_curr = rand_f();
+  auto dt = rand_f();
+  solver.Update(&system, t_curr, dt);
+  auto u_new = system.GetSolutionColumn();
+  auto u_1st = u_old + a * u_old * dt;
+  auto u_2nd = ((u_1st + a * u_1st * dt) + u_old) / 2;
+  EXPECT_EQ(u_new, u_2nd);
+}
+TEST_F(TestTemporalConstant, ThreeStepRungeKutta) {
+  using Solver = mini::temporal::RungeKutta<3, Scalar>;
+  int n = 10;
+  std::srand(31415926);
+  Matrix a = Matrix::Random(n, n);
+  auto system = System(a);
+  Column u_old = Column::Random(n);
+  system.SetSolutionColumn(u_old);
+  auto solver = Solver();
+  auto t_curr = rand_f();
+  auto dt = rand_f();
+  solver.Update(&system, t_curr, dt);
+  auto u_new = system.GetSolutionColumn();
+  auto u_1st = u_old + a * u_old * dt;
+  auto u_2nd = ((u_1st + a * u_1st * dt) + u_old * 3) / 4;
+  auto u_3rd = ((u_2nd + a * u_2nd * dt) * 2 + u_old) / 3;
+  EXPECT_EQ(u_new, u_3rd);
+}
 
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
