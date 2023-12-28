505dc195f97c374382e62b650c7a89a3a4270ac7
Weicheng <weicheng.pei@icloud.com>
2023-12-28 21:25:08 +0800

    Implement viscous fluxes.

--- include/mini/riemann/diffusive/navier_stokes.hpp
+++ include/mini/riemann/diffusive/navier_stokes.hpp
@@ -2,6 +2,8 @@
 #ifndef MINI_RIEMANN_DIFFUSIVE_NAVIER_STOKES_HPP_
 #define MINI_RIEMANN_DIFFUSIVE_NAVIER_STOKES_HPP_
 
+#include <utility>
+
 #include "mini/algebra/eigen.hpp"
 #include "mini/riemann/euler.hpp"
 #include "mini/constant/index.hpp"
@@ -9,6 +11,8 @@
 namespace mini {
 namespace riemann {
 namespace diffusive {
+
+using namespace mini::constant::index;
 
 template <typename G>
 class NavierStokes {
@@ -23,19 +27,25 @@ class NavierStokes {
   using Conservative = euler::Conservatives<Scalar, kComponents>;
   using Primitive = euler::Primitive<Scalar, kComponents>;
   using Vector = typename Primitive::Vector;
+  using Tensor = algebra::Vector<Scalar, 6>;
 
-  static void SetProperty(Scalar R) {
+  static void SetProperty(Scalar R, Scalar mu, Scalar kappa) {
     R_ = R;
+    mu_ = mu;
+    zeta_ = -2.0 / 3 * mu;  // Stokes' hypothesis
+    kappa_ = kappa;
   }
 
  protected:
   static Scalar R_;
+  static Scalar mu_;
+  static Scalar zeta_;
+  static Scalar kappa_;
 
-  static Gradient ConservativeGradientToPrimitiveGradient(
+  static std::pair<Primitive, Gradient> ConservativeToPrimitive(
       Conservative const &c_val, Gradient const &c_grad) {
-    auto p_val = ConservativeToPrimitive(c_val);
+    auto p_val = Gas::ConservativeToPrimitive(c_val);
     Gradient p_grad;
-    using namespace mini::constant::index;
     auto &grad_u = p_grad.col(X);
     auto &grad_v = p_grad.col(Y);
     auto &grad_w = p_grad.col(Z);
@@ -61,15 +71,63 @@ class NavierStokes {
     return p_grad;
   }
 
- public:
-  static void ModifyFluxMatrix(Conservative const &value, Gradient const &gradient,
-      FluxMatrix *flux) {
-    using namespace mini::constant::index;
-    
+  static Tensor GetViscousStressTensor(Gradient const &p_grad) {
+    Tensor tau;
+    const auto &grad_u = p_grad.col(X);
+    const auto &grad_v = p_grad.col(Y);
+    const auto &grad_w = p_grad.col(Z);
+    auto div_uvw = grad_u[X] + grad_v[Y] + grad_w[Z];
+    tau[XX] = 2 * mu_ * grad_u[X] + zeta_ * div_uvw;
+    tau[YY] = 2 * mu_ * grad_v[Y] + zeta_ * div_uvw;
+    tau[ZZ] = 2 * mu_ * grad_w[Z] + zeta_ * div_uvw;
+    tau[XY] = mu_ * (grad_u[Y] + grad_v[X])
+    tau[YZ] = mu_ * (grad_v[Z] + grad_w[Y])
+    tau[ZX] = mu_ * (grad_w[X] + grad_u[Z])
+    return tau;
   }
 
-  static void ModifyCommonFlux(Conservative const &value, Gradient const &gradient,
+  static Scalar Dot(Scalar x,  Scalar y, Scalar z, Vector const &v) {
+    return v[X] * x + v[Y] * y + v[Z] * z;
+  }
+
+ public:
+  static void ModifyFluxMatrix(Conservative const &c_val, Gradient const &c_grad,
+      FluxMatrix *flux) {
+    auto [p_val, p_grad] = ConservativeToPrimitive(c_val, c_grad);
+    Tensor tau = GetViscousStressTensor(p_grad);
+    auto const &uvw = p_val.momentum();
+    auto const &grad_T = p_grad.col(4);
+    auto &flux_x = flux->col(X);
+    flux_x[1] -= tau[XX];
+    flux_x[2] -= tau[XY];
+    flux_x[3] -= tau[XZ];
+    flux_z[4] -= Dot(tau[XX], tau[XY], tau[XZ], uvw) + kappa_ * grad_T[X];
+    auto &flux_y = flux->col(Y);
+    flux_y[1] -= tau[YX];
+    flux_y[2] -= tau[YY];
+    flux_y[3] -= tau[YZ];
+    flux_y[4] -= Dot(tau[YX], tau[YY], tau[YZ], uvw) + kappa_ * grad_T[Y];
+    auto &flux_z = flux->col(Z);
+    flux_z[1] -= tau[ZX];
+    flux_z[2] -= tau[ZY];
+    flux_z[3] -= tau[ZZ];
+    flux_z[4] -= Dot(tau[ZX], tau[ZY], tau[ZZ], uvw) + kappa_ * grad_T[Z];
+  }
+
+  static void ModifyCommonFlux(Conservative const &c_val, Gradient const &c_grad,
       Vector const &normal, Flux *flux) {
+    auto [p_val, p_grad] = ConservativeToPrimitive(c_val, c_grad);
+    Tensor tau = GetViscousStressTensor(p_grad);
+    auto &flux_momentum = flux->momentum();
+    flux_momentum[X] -= Dot(tau[XX], tau[XY], tau[XZ], normal);
+    flux_momentum[Y] -= Dot(tau[YX], tau[YY], tau[YZ], normal);
+    flux_momentum[Z] -= Dot(tau[ZX], tau[ZY], tau[ZZ], normal);
+    auto const &grad_T = p_grad.col(4);
+    auto const &uvw = p_val.momentum();
+    auto work_x = Dot(tau[XX], tau[XY], tau[XZ], uvw) + kappa_ * grad_T[X];
+    auto work_y = Dot(tau[YX], tau[YY], tau[YZ], uvw) + kappa_ * grad_T[Y];
+    auto work_z = Dot(tau[ZX], tau[ZY], tau[ZZ], uvw) + kappa_ * grad_T[Z];
+    flux->energy() -= Dot(work_x, work_y, work_z, normal);
   }
 };
 
